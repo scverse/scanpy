a38412b955d163fc6ef93efc4cc3d90644c72e12
falexwolf <f.alex.wolf@gmx.de>
2017-02-03 00:54:12 +0100

    renamed scripts

--- scanpy.py
+++ scanpy.py
@@ -9,7 +9,7 @@ Written in Python 3 (compatible with 2).
 from sys import path
 
 # scanpy, first try loading it locally
-path.insert(0, '.')
+#path.insert(0, '.')
 from scanpy.__main__ import main
 
 main()
