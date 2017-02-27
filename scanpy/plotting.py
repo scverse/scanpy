83c7a510c1f10dd233d1276704533aef0ad956e4
falexwolf <f.alex.wolf@gmx.de>
2017-02-27 12:54:06 +0100

    fixed color palettes for larger number of categories

--- scanpy/plotting.py
+++ scanpy/plotting.py
@@ -1,4 +1,5 @@
-# Copyright 2016-2017 F. Alexander Wolf (http://falexwolf.de).
+# Authors: F. Alex Wolf <http://falexwolf.de>
+#          P. Angerer        
 """
 Plotting
 """
@@ -854,6 +855,17 @@ def init_fig_params():
 
     # resolution of png output
     rcParams['savefig.dpi'] = 200
+
+    # color palette
+    # see 'category20' on 
+    # https://github.com/vega/vega/wiki/Scales#scale-range-literals
+    rcParams['axes.prop_cycle'] = cycler(color=
+                                         ['#1f77b4', '#ff7f0e', '#2ca02c', '#d62728',
+                                          '#9467bd', '#8c564b', '#e377c2', #'#7f7f7f', remove grey
+                                          '#bcbd22', '#17becf',
+                                          '#aec7e8', '#ffbb78', '#98df8a', '#ff9896',
+                                          '#c5b0d5', '#c49c94', '#f7b6d2', #'#c7c7c7', remove grey
+                                          '#dbdb8d', '#9edae5'])
 
 init_fig_params()
 
