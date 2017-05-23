48721bf8d593b377cd6508bade49153548d4c971
falexwolf <f.alex.wolf@gmx.de>
2017-05-23 11:19:13 +0200

    added api_without_examples

---
+++ scanpy/examples/api_without_examples.py
@@ -0,0 +1,20 @@
+# Author: F. Alex Wolf (http://falexwolf.de)
+"""Just a namespace to be used within examples.
+"""
+
+from .. import settings
+sett = settings  # abbreviation
+"""Settings"""
+from .. import tools
+tl = tools       # abbreviation
+"""Tools"""
+from .. import plotting
+pl = plotting    # abbreviation
+"""Plotting functions"""
+from .. import preprocessing
+pp = preprocessing  # abbreviation
+"""Preprocessing functions"""
+from ..readwrite import read, read_10x_h5, write, read_params, write_params
+"""Reading and writing."""
+from ..data_structs import AnnData
+"""Main class for storing an annotated data matrix."""
