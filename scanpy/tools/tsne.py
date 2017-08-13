54bfbe5f4d666241d55059cb25de0c89506a2d4c
falexwolf <f.alex.wolf@gmx.de>
2017-08-13 10:51:26 +0200

    set default learning_rate in all tSNE implementations to 1000

--- scanpy/tools/tsne.py
+++ scanpy/tools/tsne.py
@@ -12,12 +12,13 @@ This module automatically choose from three t-SNE versions from
 
 import numpy as np
 from ..tools.pca import pca
-from .. import settings as sett
+from .. import settings
 from .. import logging as logg
 
 
-def tsne(adata, random_state=0, n_pcs=50, perplexity=30, learning_rate=None,
-         recompute_pca=False, use_fast_tsne=True, n_jobs=None, copy=False):
+def tsne(adata, random_state=0, n_pcs=50, perplexity=30, early_exaggeration=12,
+         learning_rate=1000, recompute_pca=False, use_fast_tsne=True,
+         n_jobs=None, copy=False):
     """tSNE
 
     Parameters
@@ -25,10 +26,9 @@ def tsne(adata, random_state=0, n_pcs=50, perplexity=30, learning_rate=None,
     adata : AnnData
         Annotated data matrix, optionally with adata.smp['X_pca'], which is
         written when running sc.pca(adata). Is directly used for tSNE if `n_pcs` > 0.
-    random_state : unsigned int or -1, optional (default: 0)
-        Change to use different intial states for the optimization, if -1, use
-        default behavior of implementation (sklearn uses np.random.seed,
-        Multicore-TSNE produces a new plot at every call).
+    random_state : int or None, optional (default: 0)
+        Change this to use different intial states for the optimization. If `None`,
+        the initial state is not reproducible.
     n_pcs : int, optional (default: 50)
         Number of principal components in preprocessing PCA.
     perplexity : float, optional (default: 30)
@@ -37,17 +37,24 @@ def tsne(adata, random_state=0, n_pcs=50, perplexity=30, learning_rate=None,
         usually require a larger perplexity. Consider selecting a value
         between 5 and 50. The choice is not extremely critical since t-SNE
         is quite insensitive to this parameter.
+    early_exaggeration : float, optional (default: 12.0)
+        Controls how tight natural clusters in the original space are in the
+        embedded space and how much space will be between them. For larger
+        values, the space between natural clusters will be larger in the
+        embedded space. Again, the choice of this parameter is not very
+        critical. If the cost function increases during initial optimization,
+        the early exaggeration factor or the learning rate might be too high.
     learning_rate : float, optional (default: 1000)
+        Note that the R-package "Rtsne" uses a default of 200.
         The learning rate can be a critical parameter. It should be
         between 100 and 1000. If the cost function increases during initial
         optimization, the early exaggeration factor or the learning rate
         might be too high. If the cost function gets stuck in a bad local
         minimum increasing the learning rate helps sometimes.
     use_fast_tsne : bool, optional (default: True)
-        Use the MulticoreTSNE package by D. Ulyanov if available.
+        Use the MulticoreTSNE package by D. Ulyanov if it is installed.
     n_jobs : int or None (default: None)
-        Use the multicore implementation, if it is installed. Defaults to
-        sett.n_jobs.
+        Defaults to `sc.settings.n_jobs`.
 
     Returns
     -------
@@ -83,33 +90,32 @@ def tsne(adata, random_state=0, n_pcs=50, perplexity=30, learning_rate=None,
             logg.info('    using data matrix X directly (no PCA)')
     # params for sklearn
     params_sklearn = {'perplexity': perplexity,
-                      'random_state': None if random_state == -1 else random_state,
-                      'verbose': max(0, sett.verbosity-3),
-                      'early_exaggeration': 12,
+                      'random_state': random_state,
+                      'verbose': max(0, settings.verbosity-3),
+                      'early_exaggeration': early_exaggeration,
+                      'learning_rate': learning_rate,
                       }
-    n_jobs = sett.n_jobs if n_jobs is None else n_jobs
+    n_jobs = settings.n_jobs if n_jobs is None else n_jobs
     # deal with different tSNE implementations
     multicore_failed = True
     if n_jobs >= 1 and use_fast_tsne:
         try:
             from MulticoreTSNE import MulticoreTSNE as TSNE
-            params_sklearn['learning_rate'] = 200 if learning_rate is None else learning_rate
             tsne = TSNE(n_jobs=n_jobs, **params_sklearn)
             logg.info('    using the "MulticoreTSNE" package by Ulyanov (2017)')
             X_tsne = tsne.fit_transform(X.astype(np.float64))
             multicore_failed = False
         except ImportError:
+            logg.warn('Consider installing the package MulticoreTSNE '
+                      '(https://github.com/DmitryUlyanov/Multicore-TSNE). '
+                      'Even for `n_jobs=1` this speeds up the computation considerably and might yield better converged results.')
             pass
     if multicore_failed:
         from sklearn.manifold import TSNE
         from . import _tsne_fix  # fix by D. DeTomaso for sklearn < 0.19
-        # unfortunately, we cannot set a minimum number of iterations for barnes-hut
-        params_sklearn['learning_rate'] = 1000 if learning_rate is None else learning_rate
+        # unfortunately, sklearn does not allow to set a minimum number of iterations for barnes-hut tSNE
         tsne = TSNE(**params_sklearn)
-        logg.info('    using sklearn.manifold.TSNE')
-        logg.warn('Consider installing the package MulticoreTSNE '
-                  '(https://github.com/DmitryUlyanov/Multicore-TSNE). '
-                  'Even for `n_jobs=1` this speeds up the computation considerably and might yield better converged results.')
+        logg.info('    using sklearn.manifold.TSNE with a fix by D. DeTomaso')
         X_tsne = tsne.fit_transform(X)
     # update AnnData instance
     adata.smp['X_tsne'] = X_tsne  # annotate samples with tSNE coordinates
