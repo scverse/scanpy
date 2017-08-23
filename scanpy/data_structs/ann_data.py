8f744864452de26afed3729692e377b28c335c6c
falexwolf <f.alex.wolf@gmx.de>
2017-08-23 10:09:37 +0200

    import pandas from within function to enable building docs

--- scanpy/data_structs/ann_data.py
+++ scanpy/data_structs/ann_data.py
@@ -7,7 +7,6 @@ from enum import Enum
 import numpy as np
 from numpy import ma
 from numpy.lib.recfunctions import append_fields, rec_drop_fields
-import pandas as pd
 from scipy import sparse as sp
 from scipy.sparse.sputils import IndexMixin
 
@@ -294,6 +293,7 @@ class BoundStructArray(np.ndarray):
 
     def __setitem__(self, keys, values):
         """Either a single one- or multi-column or mulitiple one-colum items."""
+        import pandas as pd
         if isinstance(values, pd.Series):
             values = values.values
             if values.dtype.char == 'O' or 'int' in values.dtype.name:
