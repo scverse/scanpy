2f871f52f005fe9d8923e421192a804dc11e3412
falexwolf <f.alex.wolf@gmx.de>
2017-08-24 17:16:54 +0200

    avoided duplicating exports in api.pl and api.pp

--- scanpy/api/pl.py
+++ scanpy/api/pl.py
@@ -1,22 +1,6 @@
-# TODO: should not be duplicated from plotting.__init__
-from ..plotting.ann_data import scatter, violin
-from ..plotting.ann_data import ranking
-
-from ..plotting.preprocessing import filter_genes_dispersion
-
-from ..plotting.tools import pca, pca_loadings, pca_scatter, pca_variance_ratio
-from ..plotting.tools import diffmap
-from ..plotting.tools import draw_graph
-from ..plotting.tools import tsne
-from ..plotting.tools import aga, aga_attachedness, aga_graph, aga_path, aga_scatter
-from ..plotting.tools import dpt, dpt_scatter, dpt_groups_pseudotime, dpt_timeseries
-from ..plotting.tools import louvain
-from ..plotting.tools import rank_genes_groups, rank_genes_groups_violin
-from ..plotting.tools import sim
-
-from ..plotting.rcmod import reset_rcParams
-from ..plotting import palettes
-
-from ..plotting.utils import matrix
-from ..plotting.utils import timeseries, timeseries_subplot, timeseries_as_heatmap
-
+# we need this dummy module as otherwise, sphinx cannot produce the overview of
+# the api module
+#
+# importing all ("*") is ok, as scanpy.plotting.__init__ is carefully maintained
+# to only contain the functions of the api
+from ..plotting import *
--- scanpy/api/pp.py
+++ scanpy/api/pp.py
@@ -1,4 +1,6 @@
-# TODO: this duplicates scanpy.preprocessing.__init__
-from ..preprocessing.recipes import recipe_zheng17, recipe_weinreb16
-from ..preprocessing.simple import filter_cells, filter_genes, filter_genes_dispersion
-from ..preprocessing.simple import log1p, pca, normalize_per_cell, regress_out, scale, subsample
+# we need this dummy module as otherwise, sphinx cannot produce the overview of
+# the api module
+#
+# importing all ("*") is ok, as scanpy.preprocessing.__init__ is carefully maintained
+# to only contain the functions of the api
+from ..preprocessing import *
