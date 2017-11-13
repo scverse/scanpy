5dbd2df12877d2bca1538c31e35d15fb31c434e9
falexwolf <f.alex.wolf@gmx.de>
2017-11-13 10:56:18 +0100

    bug fix

--- scanpy/api/tl.py
+++ scanpy/api/tl.py
@@ -4,7 +4,6 @@ from ..tools.diffmap import diffmap
 from ..tools.draw_graph import draw_graph
 
 from ..tools.aga import aga, aga_contract_graph, aga_degrees, aga_expression_entropies, aga_compare_paths
-from ..tools.dbscan import dbscan
 from ..tools.rank_genes_groups import rank_genes_groups
 from ..tools.dpt import dpt
 from ..tools.louvain import louvain
