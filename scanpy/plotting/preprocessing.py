57feb9abfa337c582245f694d07de95f6cb8ec8a
Joshua Orvis <jorvis@gmail.com>
2018-02-08 09:38:12 -0600

    Cleanup suggested by @flying-sheep

--- scanpy/plotting/preprocessing.py
+++ scanpy/plotting/preprocessing.py
@@ -42,11 +42,8 @@ def filter_genes_dispersion(result, log=False, save=None, show=None):
         pl.legend()
         pl.xlabel(('$log_{10}$ ' if False else '') + 'mean expression of gene')
 
-        if id == 0:
-            pl.ylabel(('$log_{10}$ ' if False else '') + 'dispersion of gene'
-                  + (' (normalized)'))
-            utils.savefig_or_show('filter_genes_dispersion_normalized', show=show, save=save)
-        else:
-            pl.ylabel(('$log_{10}$ ' if False else '') + 'dispersion of gene'
-                  + (' (not normalized)'))
-            utils.savefig_or_show('filter_genes_dispersion', show=show, save=save)
+        file_name = 'filter_genes_dispersion{}'.format('_normalized' if id == 0 else '')
+        pl.ylabel(('$log_{10}$ ' if False else '') + 'dispersion of gene'
+                  + (' (normalized)' if id == 0 else ' (not normalized)'))
+        utils.savefig_or_show(file_name, show=show, save=save)
+        
