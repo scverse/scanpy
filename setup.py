f068937591369b21207deb0628c1db8dd1f47cc1
falexwolf <f.alex.wolf@gmx.de>
2018-03-21 03:04:36 +0100

    removed cython, cleaned up setup.py

--- setup.py
+++ setup.py
@@ -1,65 +1,32 @@
-import sys
 from setuptools import setup, find_packages
-from distutils.extension import Extension
 from pathlib import Path
 import versioneer
-
-# Pip calls setup.py egg_info to get static dependency information,
-# then installs them, and finally it calls setup.py develop/bdist.
-# We only need numpy when really installing/building the package,
-# and we only rebuild the .pyx extension when cython is installed.
-cmd_class = {}
-ext_modules = []
-include_dirs = []
-if 'egg_info' not in sys.argv:
-    import numpy
-
-    include_dirs.append(numpy.get_include())
-
-    try:
-        from Cython.Distutils import build_ext
-    except ImportError:
-        suffix = 'c'
-    else:
-        cmd_class['build_ext'] = build_ext
-        suffix = 'pyx'
-
-    ext_modules.append(Extension(
-        "scanpy.cython.utils_cy",
-        ["scanpy/cython/utils_cy." + suffix],
-        include_dirs=include_dirs,
-    ))
 
 package_name = 'scanpy'
 
 req_path = Path('requires.txt')
 if not req_path.is_file():
     req_path = Path(package_name + '.egg-info') / req_path
+    
 with req_path.open() as requirements:
     requires = [l.strip() for l in requirements]
 
 with open('README.rst', encoding='utf-8') as readme_f:
     readme = readme_f.read()
 
-author = 'Alex Wolf, Philipp Angerer, Tobias Callies, Davide Cittaro, Gokcen Eraslan'
+author = 'Alex Wolf, Philipp Angerer, Davide Cittaro, Gokcen Eraslan, Tobias Callies'
     
 setup(
     name=package_name,
     version=versioneer.get_version(),
-    cmdclass=versioneer.get_cmdclass(cmd_class),
     description='Single-Cell Analysis in Python.',
     long_description=readme,
     url='http://github.com/theislab/scanpy',
     author=author,
     author_email='alex.wolf@helmholtz-muenchen.de',
-    license='BSD-3-Clause',
-    entry_points={
-        'console_scripts': [
-            'scanpy = scanpy.__main__:main',
-        ],
-    },
+    license='BSD',
     install_requires=requires,
-    packages=find_packages(),  # + ['scanpy.sim_models'], might need to include sim_models
+    packages=find_packages(),
     include_dirs=include_dirs,
     # `package_data` does NOT work for source distributions!!!
     # you also need MANIFTEST.in
