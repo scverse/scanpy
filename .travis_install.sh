b33aee86fee6877e9bc7505b8cd342355bd961b8
falexwolf <f.alex.wolf@gmx.de>
2018-03-21 02:58:06 +0100

    added conda to travis ci

---
+++ .travis_install.sh
@@ -0,0 +1,30 @@
+# Deactivate the travis-provided virtual environment and setup a
+# conda-based environment instead
+deactivate
+
+pushd .
+cd
+mkdir -p download
+cd download
+echo "Cached in $HOME/download :"
+ls -l
+echo
+if [[ ! -f miniconda.sh ]]
+   then
+   wget http://repo.continuum.io/miniconda/Miniconda-3.6.0-Linux-x86_64.sh \
+       -O miniconda.sh
+   fi
+chmod +x miniconda.sh && ./miniconda.sh -b
+cd ..
+export PATH=/home/travis/miniconda/bin:$PATH
+conda update --yes conda
+popd
+
+conda create -n testenv --yes python=$PYTHON_VERSION pip pytest \
+      numpy=$NUMPY_VERSION scipy=$SCIPY_VERSION numba scikit-learn
+
+source activate testenv
+
+pip install -r requires.txt
+pip install docutils
+python setup.py develop
