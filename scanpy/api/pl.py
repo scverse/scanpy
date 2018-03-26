cfef837cf5476ef31c5712376e6f3fe175df3821
falexwolf <f.alex.wolf@gmx.de>
2018-03-26 16:37:23 +0200

    almost fixed tests

--- scanpy/api/pl.py
+++ scanpy/api/pl.py
@@ -1,21 +1,21 @@
-from .anndata import scatter, violin, ranking, clustermap
+from ..plotting.anndata import scatter, violin, ranking, clustermap
 
-from .preprocessing import filter_genes_dispersion
+from ..plotting.preprocessing import filter_genes_dispersion
 
-from .tools import pca, pca_loadings, pca_scatter, pca_variance_ratio
-from .tools import diffmap
-from .tools import draw_graph
-from .tools import tsne
-from .tools import umap
-from .tools import aga, aga_connectivity, aga_graph, aga_path, aga_scatter
-from .tools.paga import paga, paga_adjacency, paga_compare, paga_path, paga_scatter
-from .tools import dpt, dpt_scatter, dpt_groups_pseudotime, dpt_timeseries
-from .tools import louvain
-from .tools import rank_genes_groups, rank_genes_groups_violin
-from .tools import sim
+from ..plotting.tools import pca, pca_loadings, pca_scatter, pca_variance_ratio
+from ..plotting.tools import diffmap
+from ..plotting.tools import draw_graph
+from ..plotting.tools import tsne
+from ..plotting.tools import umap
+from ..plotting.tools import aga, aga_connectivity, aga_graph, aga_path, aga_scatter
+from ..plotting.tools.paga import paga, paga_adjacency, paga_compare, paga_path, paga_scatter
+from ..plotting.tools import dpt, dpt_scatter, dpt_groups_pseudotime, dpt_timeseries
+from ..plotting.tools import louvain
+from ..plotting.tools import rank_genes_groups, rank_genes_groups_violin
+from ..plotting.tools import sim
 
-from .rcmod import set_rcParams_scanpy, set_rcParams_defaults
-from . import palettes
+from ..plotting.rcmod import set_rcParams_scanpy, set_rcParams_defaults
+from ..plotting import palettes
 
-from .utils import matrix
-from .utils import timeseries, timeseries_subplot, timeseries_as_heatmap
+from ..plotting.utils import matrix
+from ..plotting.utils import timeseries, timeseries_subplot, timeseries_as_heatmap
