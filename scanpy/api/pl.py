2e4b342316bda829460f2dfbbeb62e6c131041e6
falexwolf <f.alex.wolf@gmx.de>
2018-03-26 16:25:17 +0200

    moved all rexports to api module

--- scanpy/api/pl.py
+++ scanpy/api/pl.py
@@ -1,6 +1,21 @@
-# we need this dummy module as otherwise, sphinx cannot produce the overview of
-# the api module
-#
-# importing all ("*") is ok, as scanpy.plotting.__init__ is carefully maintained
-# to only contain the functions of the api
-from ..plotting import *
+from .anndata import scatter, violin, ranking, clustermap
+
+from .preprocessing import filter_genes_dispersion
+
+from .tools import pca, pca_loadings, pca_scatter, pca_variance_ratio
+from .tools import diffmap
+from .tools import draw_graph
+from .tools import tsne
+from .tools import umap
+from .tools import aga, aga_connectivity, aga_graph, aga_path, aga_scatter
+from .tools.paga import paga, paga_adjacency, paga_compare, paga_path, paga_scatter
+from .tools import dpt, dpt_scatter, dpt_groups_pseudotime, dpt_timeseries
+from .tools import louvain
+from .tools import rank_genes_groups, rank_genes_groups_violin
+from .tools import sim
+
+from .rcmod import set_rcParams_scanpy, set_rcParams_defaults
+from . import palettes
+
+from .utils import matrix
+from .utils import timeseries, timeseries_subplot, timeseries_as_heatmap
