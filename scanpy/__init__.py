338073db0aff71bcb4cb7c197ef839ef423825e6
falexwolf <f.alex.wolf@gmx.de>
2018-03-27 14:12:38 +0200

    updated requirements

--- scanpy/__init__.py
+++ scanpy/__init__.py
@@ -25,7 +25,7 @@ import anndata
 # NOTE: pytest does not correctly retrieve anndata's version? why?
 #       use the following hack...
 if anndata.__version__ != '0+unknown':
-    if anndata.__version__ < LooseVersion('0.5'):
-        raise ImportError('Scanpy {} needs anndata version >=0.5, not {}.\n'
+    if anndata.__version__ < LooseVersion('0.5.7'):
+        raise ImportError('Scanpy {} needs anndata version >=0.5.7, not {}.\n'
                           'Run `pip install anndata --upgrade`.'
                           .format(__version__, anndata.__version__))
