410e53fc1f188b64e799afe9ac939b53c30430f8
falexwolf <f.alex.wolf@gmx.de>
2018-04-10 10:20:55 +0200

    added more extensive explanation on diffusion maps

--- scanpy/tools/diffmap.py
+++ scanpy/tools/diffmap.py
@@ -10,6 +10,14 @@ def diffmap(adata, n_comps=15, copy=False):
     data by [Haghverdi15]_. The tool uses the adapted Gaussian kernel suggested
     by [Haghverdi16]_ in the implementation of [Wolf17]_.
 
+    The width ("sigma") of the connectivity kernel is implicitly determined by
+    the number of neighbors used to compute the single-cell graph in
+    :func:`~scanpy.api.neighbors`. To reproduce the original implementation
+    using a Gaussian kernel, use `method=='gauss'` in
+    :func:`~scanpy.api.neighbors`. To use an exponential kernel, use the default
+    `method=='umap'`. Differences between these options shouldn't usually be
+    dramatic.
+
     Parameters
     ----------
     adata : :class:`~scanpy.api.AnnData`
