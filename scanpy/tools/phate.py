b9c882c5fa148dfe7b2bf2cd6fc326d3e7e2070f
falexwolf <f.alex.wolf@gmx.de>
2018-05-03 12:59:17 +0200

    cleaned up phate

--- scanpy/tools/phate.py
+++ scanpy/tools/phate.py
@@ -1,9 +1,5 @@
 """Embed high-dimensional data using PHATE
 """
-try:
-    import phate as ph
-except ImportError:
-    pass
 
 from .. import settings
 from .. import logging as logg
@@ -32,61 +28,49 @@ def phate(
     embeds high dimensional single-cell data into two or three dimensions for
     visualization of biological progressions. [Moon17]_
 
-    For more information and access to the object-oriented interface,
-    read the `PHATE Documentation <https://phate.readthedocs.io/>`_.
-    For help, tutorials, bug reports, and R/MATLAB implementations,
-    visit the `PHATE GitHub <https://github.com/KrishnaswamyLab/PHATE/>`_.
+    For more information and access to the object-oriented interface, read the
+    `PHATE documentation <https://phate.readthedocs.io/>`_.  For help,
+    tutorials, bug reports, and R/MATLAB implementations, visit the `PHATE
+    GitHub page <https://github.com/KrishnaswamyLab/PHATE/>`_.
 
     Parameters
     ----------
     adata : :class:`~scanpy.api.AnnData`
         Annotated data matrix.
-
     n_components : `int`, optional (default: 2)
         number of dimensions in which the data will be embedded
-
     k : `int`, optional (default: 15)
         number of nearest neighbors on which to build kernel
-
     a : `int`, optional (default: `None`)
         sets decay rate of kernel tails.
         If None, alpha decaying kernel is not used
-
     alpha_decay : `bool` or `None` (default: `None`)
         forces the use of alpha decaying kernel
         If None, alpha decaying kernel is used for small inputs
         (n_samples < n_landmark) and not used otherwise
-
     n_landmark : `int`, optional (default: 2000)
         number of landmarks to use in fast PHATE
-
     t : `int` or 'auto', optional (default: 'auto')
         power to which the diffusion operator is powered
         sets the level of diffusion
-
     potential_method : {'log', 'sqrt'}, optional (default: 'log')
         Selects which transformation of the diffusional operator is used
         to compute the diffusion potential
-
     n_pca : `int`, optional (default: 100)
         Number of principal components to use for calculating
         neighborhoods. For extremely large datasets, using
         n_pca < 20 allows neighborhoods to be calculated in
         log(n_samples) time.
-
     knn_dist : string, optional (default: 'euclidean')
         recommended values: 'euclidean' and 'cosine'
         Any metric from `scipy.spatial.distance` can be used
         distance metric for building kNN graph
-
     mds_dist : string, optional (default: 'euclidean')
         recommended values: 'euclidean' and 'cosine'
         Any metric from `scipy.spatial.distance` can be used
         distance metric for MDS
-
     mds : {'classic', 'metric', 'nonmetric'}, optional (default: 'metric')
         Selects which MDS algorithm is used for dimensionality reduction
-
     n_jobs : `int` or `None`, optional (default: `sc.settings.n_jobs`)
         The number of jobs to use for the computation.
         If `None`, `sc.settings.n_jobs` is used.
@@ -94,14 +78,11 @@ def phate(
         used at all, which is useful for debugging.
         For n_jobs below -1, (n_cpus + 1 + n_jobs) are used. Thus for
         n_jobs = -2, all CPUs but one are used
-
     random_state : `int`, `numpy.RandomState` or `None`, optional (default: `None`)
         Random seed. Defaults to the global `numpy` random number generator
-
     verbose : `bool`, `int` or `None`, optional (default: `sc.settings.verbosity`)
         If `True` or an integer `>= 2`, print status messages.
         If `None`, `sc.settings.verbosity` is used.
-
     copy : `bool` (default: `False`)
         Return a copy instead of writing to `adata`.
 
@@ -134,26 +115,27 @@ def phate(
         verbose = verbose >= 2
     n_jobs = settings.n_jobs if n_jobs is None else n_jobs
     try:
-        X_phate = ph.PHATE(
-            n_components=n_components,
-            k=k,
-            a=a,
-            alpha_decay=alpha_decay,
-            n_landmark=n_landmark,
-            t=t,
-            potential_method=potential_method,
-            n_pca=n_pca,
-            knn_dist=knn_dist,
-            mds_dist=mds_dist,
-            mds=mds,
-            n_jobs=n_jobs,
-            random_state=random_state,
-            verbose=verbose,
-        ).fit_transform(adata)
-    except NameError:
+        import phate
+    except ImportError:
         raise ImportError(
             'You need to install the package `phate`: please run `pip install '
             '--user phate` in a terminal.')
+    X_phate = phate.PHATE(
+        n_components=n_components,
+        k=k,
+        a=a,
+        alpha_decay=alpha_decay,
+        n_landmark=n_landmark,
+        t=t,
+        potential_method=potential_method,
+        n_pca=n_pca,
+        knn_dist=knn_dist,
+        mds_dist=mds_dist,
+        mds=mds,
+        n_jobs=n_jobs,
+        random_state=random_state,
+        verbose=verbose,
+    ).fit_transform(adata)
     logg.info('    finished', time=True,
               end=' ' if settings.verbosity > 2 else '\n')
     # update AnnData instance
