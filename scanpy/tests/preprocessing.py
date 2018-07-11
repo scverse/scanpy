7b6bc67abb60ff6e2f5acb05e57632a301d86416
falexwolf <f.alex.wolf@gmx.de>
2018-07-11 13:51:56 +0200

    added test for log1p chunked

--- scanpy/tests/preprocessing.py
+++ scanpy/tests/preprocessing.py
@@ -1,11 +1,20 @@
 import numpy as np
-from numpy import ma
 from scipy import sparse as sp
-# we don’t need this in requirements.txt, as it’s only needed for testing
-from pytest import mark
 import scanpy.api as sc
-
 from anndata import AnnData
+
+
+def test_log1p_chunked():
+    A = np.random.rand(200, 10)
+    ad = AnnData(A)
+    ad2 = AnnData(A)
+    ad3 = AnnData(A)
+    ad3.filename = 'test.h5ad'
+    sc.pp.log1p(ad)
+    sc.pp.log1p(ad2, chunked=True)
+    assert np.allclose(ad2.X, ad.X)
+    sc.pp.log1p(ad3, chunked=True)
+    assert np.allclose(ad3.X, ad.X)
 
 
 def test_normalize_per_cell():
@@ -43,16 +52,18 @@ def test_recipe_plotting():
 
 def test_regress_out_ordinal():
     from scipy.sparse import random
-    adata = AnnData(random(5000, 2000, density=0.6, format='csr'))
+    adata = AnnData(random(1000, 100, density=0.6, format='csr'))
     adata.obs['percent_mito'] = np.random.rand(adata.X.shape[0])
     adata.obs['n_counts'] = adata.X.sum(axis=1)
 
     # results using only one processor
-    single = sc.pp.regress_out(adata, keys=['n_counts', 'percent_mito'], n_jobs=1, copy=True)
+    single = sc.pp.regress_out(
+        adata, keys=['n_counts', 'percent_mito'], n_jobs=1, copy=True)
     assert adata.X.shape == single.X.shape
 
     # results using 8 processors
-    multi = sc.pp.regress_out(adata, keys=['n_counts', 'percent_mito'], n_jobs=8, copy=True)
+    multi = sc.pp.regress_out(
+        adata, keys=['n_counts', 'percent_mito'], n_jobs=8, copy=True)
 
     np.testing.assert_array_equal(single.X, multi.X)
 
@@ -60,9 +71,10 @@ def test_regress_out_ordinal():
 def test_regress_out_categorical():
     from scipy.sparse import random
     import pandas as pd
-    adata = AnnData(random(2500, 1000, density=0.6, format='csr'))
+    adata = AnnData(random(1000, 100, density=0.6, format='csr'))
     # create a categorical column
-    adata.obs['batch'] = pd.Categorical(np.random.randint(1, 10, size=adata.X.shape[0]))
+    adata.obs['batch'] = pd.Categorical(
+        np.random.randint(1, 4, size=adata.X.shape[0]))
 
     multi = sc.pp.regress_out(adata, keys='batch', n_jobs=8, copy=True)
     assert adata.X.shape == multi.X.shape
