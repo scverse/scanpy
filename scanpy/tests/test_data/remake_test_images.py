0b78c758e5d9cc1b94756615f2250c4dbfcb4337
Fidel Ramirez <fidel.ramirez@gmail.com>
2018-07-19 11:55:50 +0200

    modifications to see if travis tests pass

--- scanpy/tests/test_data/remake_test_images.py
+++ scanpy/tests/test_data/remake_test_images.py
@@ -9,20 +9,20 @@ adata = sc.datasets.krumsiek11()
 
 # make heatmap
 sc.pl.heatmap(adata, adata.var_names, 'cell_type', use_raw=False)
-pl.savefig('master_heatmap.png', dpi=80, bbox_inches='tight')
+pl.savefig('master_heatmap.png', dpi=80)
 
 # make heatmap with continues data
 adata.obs['Gata2'] = adata.X[:, 0]
 sc.pl.heatmap(adata, adata.var_names, 'Gata2', use_raw=False,
               num_categories=4, figsize=(4.5, 5))
-pl.savefig('master_heatmap2.png', dpi=80, bbox_inches='tight')
+pl.savefig('master_heatmap2.png', dpi=80)
 
 # make dotplot
 sc.pl.dotplot(adata, adata.var_names, 'cell_type', use_raw=False)
-pl.savefig('master_dotplot.png', dpi=80, bbox_inches='tight')
+pl.savefig('master_dotplot.png', dpi=80)
 
 # make dotplot with continuous data
 sc.pl.dotplot(adata, adata.var_names, 'Gata2', use_raw=False,
               num_categories=7, figsize=(7, 2.5))
-pl.savefig('master_dotplot2.png', dpi=80, bbox_inches='tight')
+pl.savefig('master_dotplot2.png', dpi=80)
 
