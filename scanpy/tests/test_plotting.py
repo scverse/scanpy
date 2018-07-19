aa618716c4d7e3f9bcd1d6ae8c1bd19ef226bc30
Fidel Ramirez <fidel.ramirez@gmail.com>
2018-07-19 11:55:50 +0200

    modifications to see if travis tests pass

--- scanpy/tests/test_plotting.py
+++ scanpy/tests/test_plotting.py
@@ -18,7 +18,7 @@ def test_heatmap():
     adata = sc.datasets.krumsiek11()
     outfile = NamedTemporaryFile(suffix='.png', prefix='scanpy_test_heatmap_', delete=False)
     sc.pl.heatmap(adata, adata.var_names, 'cell_type', use_raw=False)
-    pl.savefig(outfile.name, dpi=80, bbox_inches='tight')
+    pl.savefig(outfile.name, dpi=80)
     res = compare_images(ROOT + '/master_heatmap.png', outfile.name, tolerance)
     assert res is None, res
 
@@ -32,7 +32,7 @@ def test_heatmap_numeric_column():
     outfile = NamedTemporaryFile(suffix='.png', prefix='scanpy_test_heatmap2_', delete=False)
     sc.pl.heatmap(adata, adata.var_names, 'Gata2', use_raw=False,
                   num_categories=4, figsize=(4.5, 5))
-    pl.savefig(outfile.name, dpi=80, bbox_inches='tight')
+    pl.savefig(outfile.name, dpi=80)
     res = compare_images(ROOT + '/master_heatmap2.png', outfile.name, tolerance)
     assert res is None, res
 
@@ -42,7 +42,7 @@ def test_dotplot():
     adata = sc.datasets.krumsiek11()
     outfile = NamedTemporaryFile(suffix='.png', prefix='scanpy_test_dotplot_', delete=False)
     sc.pl.dotplot(adata, adata.var_names, 'cell_type', use_raw=False)
-    pl.savefig(outfile.name, dpi=80, bbox_inches='tight')
+    pl.savefig(outfile.name, dpi=80)
     res = compare_images(ROOT + '/master_dotplot.png', outfile.name, tolerance)
 
     assert res is None, res
@@ -56,7 +56,7 @@ def test_dotplot_numeric_column():
     adata.obs['Gata2'] = adata.X[:, 0]
     sc.pl.dotplot(adata, adata.var_names, 'Gata2', use_raw=False,
                   num_categories=7, figsize=(7, 2.5))
-    pl.savefig(outfile.name, dpi=80, bbox_inches='tight')
+    pl.savefig(outfile.name, dpi=80)
     res = compare_images(ROOT + '/master_dotplot2.png', outfile.name, tolerance)
 
     assert res is None, res
