b578c0dfe2241a50cc8c05ea7d3d51137a74e038
falexwolf <f.alex.wolf@gmx.de>
2018-08-13 23:07:28 +0200

    changed behavior of pl.pca to pl.pca_scatter, added pl.pca_overview

--- scanpy/api/pl.py
+++ scanpy/api/pl.py
@@ -2,7 +2,7 @@ from ..plotting.anndata import scatter, violin, ranking, clustermap, stacked_vio
 
 from ..plotting.preprocessing import filter_genes_dispersion
 
-from ..plotting.tools import pca, pca_loadings, pca_scatter, pca_variance_ratio
+from ..plotting.tools import pca, pca_loadings, pca_scatter, pca_overview, pca_variance_ratio
 from ..plotting.tools.embedding import diffmap, draw_graph, tsne, phate, umap
 from ..plotting.tools.paga import paga, paga_adjacency, paga_compare, paga_path
 from ..plotting.tools import dpt_timeseries, dpt_groups_pseudotime
