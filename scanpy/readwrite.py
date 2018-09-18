c59995b88b3a4915e365336a1c332485f4b36b33
falexwolf <f.alex.wolf@gmx.de>
2018-09-18 20:27:27 +0000

    fixed bug in read_10x_mtx

--- scanpy/readwrite.py
+++ scanpy/readwrite.py
@@ -161,13 +161,13 @@ def read_10x_mtx(path, var_names='gene_symbols', make_unique=True, cache=False):
     genes = pd.read_csv(path + 'genes.tsv', header=None, sep='\t')
     if var_names == 'gene_symbols':
         var_names = genes[1]
-        adata.var['gene_ids'] = genes[0]
         if make_unique:
             var_names = anndata.utils.make_index_unique(pd.Index(var_names))
         adata.var_names = var_names
+        adata.var['gene_ids'] = genes[0].values
     elif var_names == 'gene_ids':
         adata.var_names = genes[0]
-        adata.var['gene_symbols'] = genes[1]
+        adata.var['gene_symbols'] = genes[1].values
     else:
         raise ValueError('`var_names` needs to be \'gene_symbols\' or \'gene_ids\'')
     adata.obs_names = pd.read_csv(path + 'barcodes.tsv', header=None)[0]
