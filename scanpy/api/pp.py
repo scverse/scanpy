a21e1d503e3245a85dae4265e931d5d4d6567710
Isaac Virshup <ivirshup@gmail.com>
2018-10-22 15:44:02 +1100

    Add function `calculate_qc_metrics`
    Initial commit for adding `calculate_qc_metrics` based on functionality
    from `scater`.

--- scanpy/api/pp.py
+++ scanpy/api/pp.py
@@ -1,6 +1,7 @@
 from ..preprocessing.recipes import recipe_zheng17, recipe_weinreb17, recipe_seurat
 from ..preprocessing.simple import filter_cells, filter_genes, filter_genes_dispersion
 from ..preprocessing.simple import log1p, sqrt, pca, normalize_per_cell, regress_out, scale, subsample, downsample_counts
+from ..preprocessing.simple import calculate_qc_metrics
 from ..preprocessing.mnn_correct import mnn_correct
 from ..preprocessing.dca import dca
 from ..preprocessing.magic import magic
