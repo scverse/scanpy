1767eeb0ff7ae96d5e0252ca6c546b906d18af5b
Isaac Virshup <ivirshup@gmail.com>
2018-10-24 16:51:23 +1100

    qc_metrics COO support and features.
    Probably should be two commits, but the code was kinda intertwinned by
    the time I got there.
    
    First, I've added support for COO sparse matrices, which required
    converting them, as they can't be subsetted, which means they raise a
    lot of errors.
    
    Next, I've added a bunch of features. This includes setting
    `feature_controls` and including log scaling for a few metrics.

--- scanpy/tests/test_qc_metrics.py
+++ scanpy/tests/test_qc_metrics.py
@@ -1,6 +1,7 @@
+import numpy as np
+import pandas as pd
 import scanpy.api as sc
 import scanpy
-import numpy as np
 from scipy import sparse
 
 from scanpy.preprocessing.qc import top_proportions, top_segment_proportions
@@ -60,28 +61,44 @@ def test_top_segments_sparse():
         propfull = top_proportions(a, 100)
         assert (segfull == propfull).all()
 
-
+# While many of these are trivial, they're also just making sure the metrics are there
 def test_qc_metrics():
     adata = sc.AnnData(X=sparse.csr_matrix(
         np.random.binomial(100, .005, (1000, 1000))))
-    sc.pp.calculate_qc_metrics(adata, inplace=True)
+    adata.var["mito"] = np.concatenate(
+        (np.ones(100, dtype=bool), np.zeros(900, dtype=bool)))
+    sc.pp.calculate_qc_metrics(adata, feature_controls=["mito"], inplace=True)
     assert (adata.obs["total_features_by_counts"] > 0).all()
     assert (adata.obs["total_features_by_counts"] < adata.shape[1]).all()
+    assert (adata.obs["total_features_by_counts"] >= adata.obs["log1p_total_features_by_counts"]).all()
     assert (adata.obs["total_counts"] == np.ravel(adata.X.sum(axis=1))).all()
+    assert (adata.obs["total_counts"] >= adata.obs["log1p_total_counts"]).all()
+    assert (adata.obs["total_counts_mito"] > 0).all()
+    assert (adata.obs["total_counts_mito"] >=
+            adata.obs["log1p_total_counts_mito"]).all()
     for col in adata.obs.columns:
+        assert (adata.obs[col] >= 0).all() # Values should be positive or zero
+        assert (adata.obs[col] != 0).any().all() # Nothing should be all zeros
         if col.startswith("pct_counts_in_top"):
             assert (adata.obs[col] <= 100).all()
             assert (adata.obs[col] >= 0).all()
     for col in adata.var.columns:
         assert (adata.var[col] >= 0).all()
     assert (adata.var["mean_counts"] < np.ravel(adata.X.max(axis=0).todense())).all()
+    assert (adata.var["mean_counts"] >= adata.var["log1p_mean_counts"]).all()
+    assert (adata.var["total_counts"] >= adata.var["log1p_total_counts"]).all()
 
 def test_qc_metrics_format():
     a = np.random.binomial(100, .005, (1000, 1000))
-    adata_dense = sc.AnnData(X=a)
-    sc.pp.calculate_qc_metrics(adata_dense, inplace=True)
+    init_var = pd.DataFrame({"mito": np.concatenate(
+        (np.ones(100, dtype=bool), np.zeros(900, dtype=bool)))})
+    adata_dense = sc.AnnData(X=a, var=init_var.copy())
+    sc.pp.calculate_qc_metrics(adata_dense, feature_controls=["mito"], 
+        inplace=True)
     for fmt in [sparse.csr_matrix, sparse.csc_matrix, sparse.coo_matrix]:
-        adata = sc.AnnData(X=fmt(a))
-        sc.pp.calculate_qc_metrics(adata, inplace=True)
+        adata = sc.AnnData(X=fmt(a), var=init_var.copy())
+        sc.pp.calculate_qc_metrics(adata, feature_controls=["mito"], 
+            inplace=True)
         assert np.allclose(adata.obs, adata_dense.obs)
-        assert np.allclose(adata.var, adata_dense.var)
+        for col in adata.var: # np.allclose doesn't like mix of types
+            assert np.allclose(adata.var[col], adata_dense.var[col])
