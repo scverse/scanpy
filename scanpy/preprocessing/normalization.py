86701e263dad350decab60acedcc6b0c8da2bf34
Koncopd <koncopd@gmail.com>
2018-11-12 20:28:23 +0100

    further normalization

--- scanpy/preprocessing/normalization.py
+++ scanpy/preprocessing/normalization.py
@@ -3,7 +3,8 @@ from scipy.sparse import issparse
 from sklearn.utils import sparsefuncs
 
 
-def _normalize_data(X, after=None, counts):
+def _normalize_data(X, after=None, counts, copy=False):
+    X = X.copy() if copy else X
     after = np.median(counts) if after is None else after
     counts += (counts == 0)
     counts /= after
@@ -11,7 +12,7 @@ def _normalize_data(X, after=None, counts):
         X = sparsefuncs.inplace_row_scale(X, 1/counts)
     else:
         X = X/counts
-    return X
+    return X if copy else None
 
 def normalize_quantile(data, counts_per_cell_after=None, quantile=1, min_counts=1,
                        key_n_counts=None, copy=False, layers=[], use_rep=None):
@@ -21,13 +22,30 @@ def normalize_quantile(data, counts_per_cell_after=None, quantile=1, min_counts=
 
     adata = data.copy() if copy else data
 
-    X = adata.X
+    if quantile < 1:
+        X = adata.X
+        counts_per_cell = X.sum(1).A1 if issparse(X) else X.sum(1)
+        gene_subset = (X>counts_per_cell[:, None]*quantile).sum(0).A1 == 0
+        adata._inplace_subset_var(gene_subset)
+        del X
 
+    X = adata.X
     counts_per_cell = X.sum(1).A1 if issparse(X) else X.sum(1)
-    gene_subset = (X>=counts_per_cell[:, None]*quantile).sum(0).A1 == X.shape[0]
+    cell_subset = counts_per_cell >= min_counts
+    adata._inplace_subset_obs(cell_subset)
+    del X
+    counts_per_cell = counts_per_cell[cell_subset]
+    _normalize_data(adata.X, counts_per_cell_after, counts_per_cell)
 
-    adata._inplace_subset_var(gene_subset)
+    layers = adata.layers.keys() if layers == 'all' else layers
+    if use_rep == 'after':
+        after = counts_per_cell_after
+    elif use_rep == 'X':
+        after = np.median(counts_per_cell)
+    elif use_rep is None:
+        after = None
 
-    X = adata.X
-
-    
+    for layer in layers:
+        L = adata.layers[layer]
+        counts = L.sum(1).A1 if issparse(L) else L.sum(1)
+        adata.layers[layer] = _normalize_data(L, after, counts, copy=True)
