fe055d8b279d6316ddb61e82d579b04f74ca1055
Sergei Rybakov <koncopd@gmail.com>
2018-11-14 16:01:34 +0100

    normalize_ total

--- scanpy/preprocessing/normalization.py
+++ scanpy/preprocessing/normalization.py
@@ -3,7 +3,7 @@ from scipy.sparse import issparse
 from sklearn.utils import sparsefuncs
 
 
-def _normalize_data(X, after=None, counts, copy=False):
+def _normalize_data(X, counts, after=None, copy=False):
     X = X.copy() if copy else X
     after = np.median(counts) if after is None else after
     counts += (counts == 0)
@@ -11,7 +11,7 @@ def _normalize_data(X, after=None, counts, copy=False):
     if issparse(X):
         X = sparsefuncs.inplace_row_scale(X, 1/counts)
     else:
-        X /= counts
+        X /= counts[:, None]
     return X if copy else None
 
 def normalize_quantile(data, counts_per_cell_after=None, quantile=1, min_counts=1,
@@ -20,22 +20,25 @@ def normalize_quantile(data, counts_per_cell_after=None, quantile=1, min_counts=
     if quantile < 0 or quantile > 1:
         raise ValueError('Choose quantile between 0 and 1.')
 
+    if key_n_counts is None: key_n_counts = 'n_counts'
+
     adata = data.copy() if copy else data
 
     if quantile < 1:
         X = adata.X
         counts_per_cell = X.sum(1).A1 if issparse(X) else X.sum(1)
-        gene_subset = (X>counts_per_cell[:, None]*quantile).sum(0).A1 == 0
+        gene_subset = (X>counts_per_cell[:, None]*quantile).sum(0)
+        gene_subset = gene_subset.A1 == 0 if issparse(X) else gene_subset == 0
         adata._inplace_subset_var(gene_subset)
-        del X
 
     X = adata.X
     counts_per_cell = X.sum(1).A1 if issparse(X) else X.sum(1)
+    adata.obs[key_n_counts] = counts_per_cell
     cell_subset = counts_per_cell >= min_counts
     adata._inplace_subset_obs(cell_subset)
     del X
     counts_per_cell = counts_per_cell[cell_subset]
-    _normalize_data(adata.X, counts_per_cell_after, counts_per_cell)
+    _normalize_data(adata.X, counts_per_cell, counts_per_cell_after)
 
     layers = adata.layers.keys() if layers == 'all' else layers
     if use_rep == 'after':
@@ -48,4 +51,13 @@ def normalize_quantile(data, counts_per_cell_after=None, quantile=1, min_counts=
     for layer in layers:
         L = adata.layers[layer]
         counts = L.sum(1).A1 if issparse(L) else L.sum(1)
-        _normalize_data(L, after, counts)
+        _normalize_data(L, counts, after)
+
+    return adata if copy else None
+
+def normalize_total(data, counts_per_cell_after=None, counts_per_cell=None,
+                    key_n_counts=None, copy=False, layers=[], use_rep=None,
+                    min_counts=1):
+    return normalize_quantile(data=data, counts_per_cell_after=counts_per_cell_after,
+                              key_n_counts=key_n_counts, copy=copy, layers=layers,
+                              use_rep=use_rep, min_counts=min_counts, quantile=1)
