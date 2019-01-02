c017b2afa903c549ab2d5c221e4ea0e210d6b6fa
falexwolf <f.alex.wolf@gmx.de>
2019-01-02 02:53:09 +0100

    release 1.3.7

--- scanpy/plotting/__init__.py
+++ scanpy/plotting/__init__.py
@@ -1,25 +1,4 @@
-from .anndata import scatter, violin, ranking, clustermap, stacked_violin, heatmap, dotplot, matrixplot, tracksplot
-
-from .preprocessing import filter_genes_dispersion, highly_variable_genes
-
-from .tools.scatterplots import pca, diffmap, draw_graph, tsne, phate, umap
-from .tools import pca_loadings, pca_scatter, pca_overview, pca_variance_ratio
-from .tools.paga import paga, paga_adjacency, paga_compare, paga_path
-from .tools import dpt_timeseries, dpt_groups_pseudotime
-from .tools import rank_genes_groups, rank_genes_groups_violin
-from .tools import rank_genes_groups_dotplot, rank_genes_groups_heatmap, rank_genes_groups_stacked_violin, rank_genes_groups_matrixplot, rank_genes_groups_tracksplot
-from .tools import sim
-
-from .rcmod import set_rcParams_scanpy, set_rcParams_defaults
-from . import palettes
-
-from .utils import matrix
-from .utils import timeseries, timeseries_subplot, timeseries_as_heatmap
-
-from .qc import highest_expr_genes
-
-
-__doc__ = """\
+"""
 Plotting: PL
 ============
 
@@ -119,3 +98,23 @@ Visualize clusters using one of the embedding methods passing ``color='louvain'`
 
    pl.sim
 """
+
+from .anndata import scatter, violin, ranking, clustermap, stacked_violin, heatmap, dotplot, matrixplot, tracksplot
+
+from .preprocessing import filter_genes_dispersion, highly_variable_genes
+
+from .tools.scatterplots import pca, diffmap, draw_graph, tsne, phate, umap
+from .tools import pca_loadings, pca_scatter, pca_overview, pca_variance_ratio
+from .tools.paga import paga, paga_adjacency, paga_compare, paga_path
+from .tools import dpt_timeseries, dpt_groups_pseudotime
+from .tools import rank_genes_groups, rank_genes_groups_violin
+from .tools import rank_genes_groups_dotplot, rank_genes_groups_heatmap, rank_genes_groups_stacked_violin, rank_genes_groups_matrixplot, rank_genes_groups_tracksplot
+from .tools import sim
+
+from .rcmod import set_rcParams_scanpy, set_rcParams_defaults
+from . import palettes
+
+from .utils import matrix
+from .utils import timeseries, timeseries_subplot, timeseries_as_heatmap
+
+from .qc import highest_expr_genes
