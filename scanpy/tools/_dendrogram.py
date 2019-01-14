b542423de09aab638b7414bfef7c3d851fc062df
Fidel Ramírez <fidel.ramirez@gmail.com>
2019-01-14 11:19:08 +0100

    modified default key to 'dendrogram_' + groupby. Added correlation matrix to dendrogram info.

--- scanpy/tools/_dendrogram.py
+++ scanpy/tools/_dendrogram.py
@@ -8,6 +8,7 @@ from anndata import AnnData
 from pandas.api.types import is_categorical_dtype
 
 from .. utils import doc_params
+from .. import logging as logg
 from ..tools._utils import choose_representation, doc_use_rep, doc_n_pcs
 
 
@@ -19,7 +20,7 @@ def dendrogram(adata: AnnData, groupby: str,
                use_raw: Optional[bool]=None,
                cor_method: Optional[str]='pearson',
                linkage_method: Optional[str]='complete',
-               key_added: Optional[str]='dendrogram') -> None:
+               key_added: Optional[str]=None) -> None:
 
     """
     Computes a hierarchical clustering for the given `groupby` categories. Be default the PCA
@@ -27,7 +28,7 @@ def dendrogram(adata: AnnData, groupby: str,
 
     Alternatively, a list of var_names (e.g genes) can be given.
 
-    Average values of either var_names or components are then used to compute a correlation matrix.
+    Average values of either var_names or components are used to compute a correlation matrix.
 
     The hierarchical clustering can be visualized using `sc.pl.dendrogram` or multiple other
     visualizations that can include a dendrogram: `matrixplot`, `heatmap`, `dotplot` and `stacked_violin`
@@ -42,14 +43,15 @@ def dendrogram(adata: AnnData, groupby: str,
         List of var_names to use for computing the hierarchical clustering. If `var_names` is given,
         then `use_rep` and `n_pcs` is ignored.
     use_raw : `bool`, optional (default: None)
-
+        Only when `var_names` is not None. Use `raw` attribute of `adata` if present.
     cor_method : `str`, optional (default: `"pearson"`)
         correlation method to use.
     linkage_method : `str`, optional (default: `"complete"`)
         linkage method to use. See https://docs.scipy.org/doc/scipy/reference/generated/scipy.cluster.hierarchy.linkage.html
         for more information.
-    key_added : : `str`, optional (default: `"dendrogram"`)
-        By default, the dendrogram information is added to the `.uns['dendrogram']`.
+    key_added : : `str`, optional (default: `None`)
+        By default, the dendrogram information is added to `.uns['dendrogram_' + groupby]`. Notice
+        that the `groupby` information is added to the dendrogram.
 
     Returns
     -------
@@ -84,6 +86,10 @@ def dendrogram(adata: AnnData, groupby: str,
         from ..plotting._anndata import _prepare_dataframe
         categories, rep_df = _prepare_dataframe(adata, gene_names, groupby, use_raw)
 
+    if key_added is None:
+        key_added = 'dendrogram_' + groupby
+
+    logg.info('Storing dendrogram info using `.uns[{!r}]`'.format(key_added))
     # aggregate values within categories using 'mean'
     mean_df = rep_df.groupby(level=0).mean()
 
@@ -102,4 +108,5 @@ def dendrogram(adata: AnnData, groupby: str,
                             'cor_method': cor_method,
                             'linkage_method': linkage_method,
                             'categories_idx_ordered': categories_idx_ordered,
-                            'dendrogram_info': dendro_info}
+                            'dendrogram_info': dendro_info,
+                            'correlation_matrix': corr_matrix.values}
