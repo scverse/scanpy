06853c2dfc19ab734eacf447df8b729e0edfd5ca
Philipp A <flying-sheep@web.de>
2019-01-16 10:56:36 +0100

    add py 3.7 classifier

--- setup.py
+++ setup.py
@@ -55,6 +55,7 @@ setup(
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
         'Topic :: Scientific/Engineering :: Bio-Informatics',
         'Topic :: Scientific/Engineering :: Visualization',
     ],
