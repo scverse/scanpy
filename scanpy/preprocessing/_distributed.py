4f62c9e2a5ccc77423972e69c2816b586557871d
Tom White <tom.e.white@gmail.com>
2019-01-22 10:44:23 +0000

    Use np.asarray() rather than Zappy-specific code.
    This uses the __array__ method on ndarray-like classes to convert from
    a distributed array to a regular NumPy ndarray.

--- scanpy/preprocessing/_distributed.py
+++ scanpy/preprocessing/_distributed.py
@@ -1,26 +1,15 @@
+import numpy as np
+
 # install dask if available
 try:
     import dask.array as da
 except ImportError:
     da = None
 
-# install zappy (which wraps numpy), or fall back to plain numpy
-try:
-    import zappy.base as np
-except ImportError:
-    import numpy as np
-
-
 def materialize_as_ndarray(a):
     """Convert distributed arrays to ndarrays."""
     if type(a) in (list, tuple):
         if da is not None and any(isinstance(arr, da.Array) for arr in a):
             return da.compute(*a, sync=True)
-        elif hasattr(np, 'asarray'): # zappy case
-            return tuple(np.asarray(arr) for arr in a)
-    else:
-        if da is not None and isinstance(a, da.Array):
-            return a.compute()
-        elif hasattr(np, 'asarray'): # zappy case
-            return np.asarray(a)
-    return a
+        return tuple(np.asarray(arr) for arr in a)
+    return np.asarray(a)
