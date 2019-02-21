723eb7ecb935e52c9ac403f2cbf74a64ff2fc191
Tom White <tom.e.white@gmail.com>
2019-02-21 09:02:11 +0000

    Install packages necessary to run distributed tests. (#485)

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ setup(
         leiden=['python-igraph', 'leidenalg'],
         bbknn=['bbknn'],
         doc=['sphinx', 'sphinx_rtd_theme', 'sphinx_autodoc_typehints', 'scanpydoc'],
-        test=['pytest>=3.9'],
+        test=['pytest>=3.9', 'dask[array]', 'zappy', 'zarr'],
     ),
     packages=find_packages(),
     # `package_data` does NOT work for source distributions!!!
