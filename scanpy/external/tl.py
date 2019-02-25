9d7e9591e5710cfe780e47f06aa6c1155a4e36b5
Awni Mousa <awnimousa@gmail.com>
2019-02-25 15:26:24 -0500

    Update tl.py

--- scanpy/external/tl.py
+++ scanpy/external/tl.py
@@ -2,3 +2,4 @@ from ..tools._pypairs import cyclone, sandbag
 from ..tools._phate import phate
 from ..tools._phenograph import phenograph
 from ..tools._palantir import palantir
+from ..tools._harmony import harmony
