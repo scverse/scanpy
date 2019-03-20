ce0131b5c1e872f281ff7f913f2fd243c8a88afc
LuckyMD <malte.luecken@icb-lisa.scidom.de>
2019-03-20 14:49:20 +0100

    added density scaling between 0 and 1, and new 'key_added' parameter

--- scanpy/tools/_density.py
+++ scanpy/tools/_density.py
@@ -19,13 +19,20 @@ def _calc_density(
     xy = np.vstack([x,y])
     z = gaussian_kde(xy)(xy)
 
-    return(z)
+    min_z = np.min(z)
+    max_z = np.max(z)
+
+    # Scale between 0 and 1
+    scaled_z = (z-min_z)/(max_z-min_z)
+
+    return(scaled_z)
 
 
 def density(
         adata: AnnData,
         embedding: str,
-        key: str = None):
+        groupby: str = None,
+        key_added: str = None):
     """Calculate the density of cells in an embedding (per condition)
 
     Gaussian kernel density estimation is used to calculate the density of
@@ -52,12 +59,17 @@ def density(
         Keys for categorical observation/cell annotation for which densities
         are calculated per category. Columns with up to ten categories are
         accepted.
+    key_added : `str`, optional (default: `None`)
+        Name of the `.obs` covariate that will be added with the density
+        estimates.
 
     Returns
     -------
-    Updates `adata.obs` with an additional field `[embedding]_density`, where
-    `[embedding]` is one of `umap`, `dm`, `pca`, `tsne`, or `draw_graph_fa`.
-    Updates `adata.uns` with an additional field `[embedding]_density_param`.
+    Updates `adata.obs` with an additional field specified by the `key_added`
+    parameter. This parameter defaults to `[embedding]_density_[groupby]`,
+    where `[embedding]` is one of `umap`, `dm`, `pca`, `tsne`, or `draw_graph_fa`
+    and `[groupby]` denotes the parameter input.
+    Updates `adata.uns` with an additional field `[key_added]_param`.
     """
     logg.info('computing density on \'{}\''.format(embedding), r=True)
 
@@ -85,11 +97,19 @@ def density(
         if len(adata.obs[groupby].cat.categories) > 10:
             raise ValueError('More than 10 categories in {!r} column.'.format(groupby))
 
+    # Define new covariate name
+    if key_added is not None:
+        density_covariate = key_added
+    elif groupby is not None:
+        density_covariate = embedding+'_density_'+groupby
+    else:
+        density_covariate = embedding+'_density'
+
     # Calculate the densities over each category in the groupby column
     if groupby is not None:
         categories = adata.obs[groupby].cat.categories
 
-        adata.obs[embedding+'_density'] = [0 for i in range(adata.n_obs)]
+        adata.obs[density_covariate] = [0 for i in range(adata.n_obs)]
         
         for cat in categories:
             cat_mask = adata.obs[groupby] == cat
@@ -97,20 +117,20 @@ def density(
             embed_y = adata.obsm['X_'+embedding][cat_mask, 1]
 
             dens_embed = _calc_density(embed_x, embed_y)
-            adata.obs[embedding+'_density'][cat_mask] = dens_embed
+            adata.obs[density_covariate][cat_mask] = dens_embed
 
     # Calculate the density over the whole embedding without subsetting
     else: #if groupby is None
         embed_x = adata.obsm['X_'+embedding][:, 0]
         embed_y = adata.obsm['X_'+embedding][:, 1]
 
-        adata.obs[embedding+'_density'] = _calc_density(embed_x, embed_y)
+        adata.obs[density_covariate] = _calc_density(embed_x, embed_y)
 
-    adata.uns[embedding+'_density_param'] = groupby
+    adata.uns[density_covariate+'_param'] = groupby
 
     logg.hint('added\n'
-              '    \'{}_density\', densities (adata.obs)\n'
-              '    \'{}_density_param\', parameter (adata.uns)'.format(embedding, embedding))
+              '    \'{}\', densities (adata.obs)\n'
+              '    \'{}_param\', parameter (adata.uns)'.format(density_covariate, density_covariate))
 
     return None
     
