3e50142566a4c0bb0f3f1e854b016d2ea2c55e92
LuckyMD <malte.luecken@icb-lisa.scidom.de>
2019-04-02 12:18:31 +0200

    add print statements to tests to debug Travis

--- scanpy/tests/test_marker_gene_overlap.py
+++ scanpy/tests/test_marker_gene_overlap.py
@@ -20,6 +20,8 @@ def test_marker_overlap_base():
     test_data,marker_genes = generate_test_data()
 
     t1 = sc.tl.marker_gene_overlap(test_data, marker_genes)
+    
+    print(t1)
 
     assert t1.iloc[1,1] == 3.0
     assert t1.iloc[0,0] == 3.0
@@ -30,6 +32,8 @@ def test_marker_overlap_normalization():
 
     t2 = sc.tl.marker_gene_overlap(test_data, marker_genes, normalize='reference')
     t3 = sc.tl.marker_gene_overlap(test_data, marker_genes, normalize='data')
+    print(t2)
+    print(t3)
 
     assert t2.iloc[0,0] == 1.0
     assert t3.iloc[1,1] == 0.6
@@ -40,7 +44,8 @@ def test_marker_overlap_methods():
 
     t4 = sc.tl.marker_gene_overlap(test_data, marker_genes, method='overlap_coef')
     t5 = sc.tl.marker_gene_overlap(test_data, marker_genes, method='jaccard')
-
+    print(t4)
+    print(t5)
     assert t4.iloc[0,0] == 1.0
     assert t5.iloc[0,0] == 0.6
 
@@ -50,6 +55,8 @@ def test_marker_overlap_subsetting():
 
     t6 = sc.tl.marker_gene_overlap(test_data, marker_genes, top_n_markers=2)
     t7 = sc.tl.marker_gene_overlap(test_data, marker_genes, adj_pval_threshold=0.01)
+    print(t6)
+    print(t7)
 
     assert t6.iloc[0,0] == 2
     assert t7.iloc[0,0] == 1.0
