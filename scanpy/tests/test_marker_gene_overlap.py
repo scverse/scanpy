85b65500855706827682788bf86a7f1b284cb924
LuckyMD <malte.luecken@icb-lisa.scidom.de>
2019-04-02 12:45:23 +0200

    created named recarrays for testing to be more specific

--- scanpy/tests/test_marker_gene_overlap.py
+++ scanpy/tests/test_marker_gene_overlap.py
@@ -6,9 +6,10 @@ def generate_test_data():
     test_data = sc.AnnData(X = np.ones((9,10)))
     test_data.uns['rank_genes_groups'] = dict()
     test_data.uns['rank_genes_groups']['names'] = np.rec.fromarrays(
-        [['a', 'b','c','d','e'], ['a','f','g','h','i']])
+        [['a', 'b','c','d','e'], ['a','f','g','h','i']], names='c0,c1')
     test_data.uns['rank_genes_groups']['pvals_adj'] = np.rec.fromarrays(
-        [[0.001, 0.01, 0.02, 0.05, 0.6], [0.001, 0.01, 0.02, 0.05, 0.6]])
+        [[0.001, 0.01, 0.02, 0.05, 0.6], [0.001, 0.01, 0.02, 0.05, 0.6]], 
+        names='c0,c1')
 
     marker_genes = {'type 1':{'a','b','c'}, 'type 2':{'a','f','g'}}
 
@@ -21,10 +22,8 @@ def test_marker_overlap_base():
 
     t1 = sc.tl.marker_gene_overlap(test_data, marker_genes)
     
-    print(t1)
-
-    assert t1.iloc[1,1] == 3.0
-    assert t1.iloc[0,0] == 3.0
+    assert t1['c0']['type 1'] == 3.0
+    assert t1['c1']['type 2'] == 3.0
 
 
 def test_marker_overlap_normalization():
@@ -32,11 +31,9 @@ def test_marker_overlap_normalization():
 
     t2 = sc.tl.marker_gene_overlap(test_data, marker_genes, normalize='reference')
     t3 = sc.tl.marker_gene_overlap(test_data, marker_genes, normalize='data')
-    print(t2)
-    print(t3)
 
-    assert t2.iloc[0,0] == 1.0
-    assert t3.iloc[1,1] == 0.6
+    assert t2['c0']['type 1'] == 1.0
+    assert t3['c1']['type 2'] == 0.6
 
 
 def test_marker_overlap_methods():
@@ -44,8 +41,7 @@ def test_marker_overlap_methods():
 
     t4 = sc.tl.marker_gene_overlap(test_data, marker_genes, method='overlap_coef')
     t5 = sc.tl.marker_gene_overlap(test_data, marker_genes, method='jaccard')
-    print(t4)
-    print(t5)
+
     assert t4.iloc[0,0] == 1.0
     assert t5.iloc[0,0] == 0.6
 
@@ -55,9 +51,7 @@ def test_marker_overlap_subsetting():
 
     t6 = sc.tl.marker_gene_overlap(test_data, marker_genes, top_n_markers=2)
     t7 = sc.tl.marker_gene_overlap(test_data, marker_genes, adj_pval_threshold=0.01)
-    print(t6)
-    print(t7)
 
-    assert t6.iloc[0,0] == 2
-    assert t7.iloc[0,0] == 1.0
+    assert t6['c0']['type 1'] == 2.0
+    assert t7['c0']['type 1'] == 1.0
 
