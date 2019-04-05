d2cc2ca0380e3d8b25212b245fe1e58a0c32da65
Isaac Virshup <ivirshup@gmail.com>
2019-04-05 17:03:08 +1100

    improve 10x read tests

--- scanpy/tests/test_read_10x.py
+++ scanpy/tests/test_read_10x.py
@@ -8,17 +8,30 @@ import scanpy as sc
 ROOT = os.path.dirname(os.path.abspath(__file__))
 ROOT = os.path.join(ROOT, '_data', '10x_data')
 
+
 def assert_anndata_equal(a1, a2):
     assert a1.shape == a2.shape
     assert (a1.obs == a2.obs).all(axis=None)
     assert (a1.var == a2.var).all(axis=None)
     assert np.allclose(a1.X.todense(), a2.X.todense())
 
-def test_read_10x_mtx():
-    sc.read_10x_mtx(os.path.join(ROOT, '1.2.0', 'filtered_gene_bc_matrices', 'hg19_chr21'),
+
+def test_read_10x_v1():
+    v1_mtx = sc.read_10x_mtx(
+        os.path.join(ROOT, '1.2.0', 'filtered_gene_bc_matrices', 'hg19_chr21'),
+        var_names='gene_symbols'
+    )
+    v1_h5 = sc.read_10x_h5(os.path.join(ROOT, '1.2.0', 'filtered_gene_bc_matrices_h5.h5'))
+    assert_anndata_equal(v1_mtx, v1_h5)
+
+
+def test_read_10x_v3():
+    v3_mtx = sc.read_10x_mtx(os.path.join(ROOT, '3.0.0', 'filtered_feature_bc_matrix'),
                     var_names='gene_symbols')
-    sc.read_10x_mtx(os.path.join(ROOT, '3.0.0', 'filtered_feature_bc_matrix'),
-                    var_names='gene_symbols')
+    v3_h5 = sc.read_10x_h5(os.path.join(ROOT, '3.0.0', 'filtered_feature_bc_matrix.h5'))
+    v3_h5.var.drop(columns="genome", inplace=True)
+    assert_anndata_equal(v3_mtx, v3_h5)
+
 
 def test_read_10x_h5_v1():
     spec_genome_v1 = sc.read_10x_h5(os.path.join(ROOT, '1.2.0', 'filtered_gene_bc_matrices_h5.h5'),
@@ -26,11 +39,13 @@ def test_read_10x_h5_v1():
     nospec_genome_v1 = sc.read_10x_h5(os.path.join(ROOT, '1.2.0', 'filtered_gene_bc_matrices_h5.h5'))
     assert_anndata_equal(spec_genome_v1, nospec_genome_v1)
 
+
 def test_read_10x_h5():
     spec_genome_v3 = sc.read_10x_h5(os.path.join(ROOT, '3.0.0', 'filtered_feature_bc_matrix.h5'), 
                                     genome='GRCh38_chr21')
     nospec_genome_v3 = sc.read_10x_h5(os.path.join(ROOT, '3.0.0', 'filtered_feature_bc_matrix.h5'))
     assert_anndata_equal(spec_genome_v3, nospec_genome_v3)
+
 
 def test_error_10x_h5_legacy(tmp_path):
     onepth = os.path.join(ROOT, '1.2.0', 'filtered_gene_bc_matrices_h5.h5')
@@ -41,6 +56,7 @@ def test_error_10x_h5_legacy(tmp_path):
     with pytest.raises(ValueError):
         sc.read_10x_h5(twopth)
     sc.read_10x_h5(twopth, genome="hg19_chr21_copy")
+
 
 def test_error_missing_genome():
     legacy_pth = os.path.join(ROOT, '1.2.0', 'filtered_gene_bc_matrices_h5.h5')
