05e2eb2df0108947b9f106cde28ba38c55f2d294
Isaac Virshup <ivirshup@gmail.com>
2019-04-05 16:26:15 +1100

    Be a bit more thorough in dataset tests

--- scanpy/tests/test_datasets.py
+++ scanpy/tests/test_datasets.py
@@ -3,8 +3,10 @@ Tests to make sure the example datasets load.
 """
 
 import scanpy as sc
+import numpy as np
 import pytest
 from pathlib import Path
+
 
 @pytest.fixture(scope="module")
 def tmp_dataset_dir(tmpdir_factory):
@@ -23,13 +25,9 @@ def test_burczynski06(tmp_dataset_dir):
 
 
 @pytest.mark.internet
-def test_krumsiek11(tmp_dataset_dir):
-    sc.datasets.krumsiek11()
-
-
-@pytest.mark.internet
 def test_moignard15(tmp_dataset_dir):
-    sc.datasets.moignard15()
+    adata = sc.datasets.moignard15()
+    assert adata.shape == (3934, 42)
 
 
 @pytest.mark.internet
@@ -39,16 +37,31 @@ def test_paul15(tmp_dataset_dir):
 
 @pytest.mark.internet
 def test_pbmc3k(tmp_dataset_dir):
-    sc.datasets.pbmc3k()
+    adata = sc.datasets.pbmc3k()
+    assert adata.shape == (2700, 32738)
+    assert "CD8A" in adata.var_names
 
 
 @pytest.mark.internet
 def test_ebi_expression_atlas(tmp_dataset_dir):
     adata = sc.datasets.ebi_expression_atlas("E-MTAB-4888")
+    assert adata.shape == (4032, 27785)
+
+
+def test_krumsiek11(tmp_dataset_dir):
+    adata = sc.datasets.krumsiek11()
+    assert adata.shape == (640, 11)
+    assert all(
+           np.unique(adata.obs["cell_type"])
+        == np.array(["Ery", "Mk", "Mo", "Neu", "progenitor"])
+    )
 
 
 def test_blobs():
-    sc.datasets.blobs()
+    n_obs = np.random.randint(15, 30)
+    n_var = np.random.randint(500, 600)
+    adata = sc.datasets.blobs(n_variables=n_var, n_observations=n_obs)
+    assert adata.shape == (n_obs, n_var)
 
 
 def test_toggleswitch():
