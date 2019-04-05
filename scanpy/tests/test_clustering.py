65b7c4998d653b22c9f1c6a69c12a454bffb1fa4
Francesco G. Brundu <francesco.brundu@gmail.com>
2019-04-04 20:29:11 -0400

    Improved test

--- scanpy/tests/test_clustering.py
+++ scanpy/tests/test_clustering.py
@@ -20,51 +20,49 @@ def adata_neighbors():
 def test_leiden_basic(adata_neighbors):
     sc.tl.leiden(adata_neighbors)
 
+@pytest.mark.parametrize('clustering,key', [
+    (sc.tl.louvain, 'louvain'),
+    (sc.tl.leiden, 'leiden'),
+])
+def test_clustering_subset(adata_neighbors, clustering, key):
+    clustering(adata_neighbors, key_added=key)
+    print(adata_neighbors)
+    print(adata_neighbors.obs[key].value_counts())
 
-def test_clustering_subset(adata_neighbors):
-    methods = (
-        (sc.tl.louvain, 'louvain'),
-        (sc.tl.leiden, 'leiden'),
-    )
-    for clustering, key in methods:
-        print('Clustering method ' + str(key))
-        clustering(adata_neighbors, key_added=key)
-        print(adata_neighbors)
-        print(adata_neighbors.obs[key].value_counts())
+    for c in np.unique(adata_neighbors.obs[key]):
+        print('Analyzing cluster ', c)
+        cells_in_c = adata_neighbors.obs[key] == c
+        ncells_in_c = adata_neighbors.obs[key].value_counts().loc[c]
+        key_sub = str(key) + '_sub'
+        clustering(
+            adata_neighbors,
+            restrict_to=(key, [c]),
+            key_added=key_sub
+        ) 
+        new_partition = adata_neighbors.obs[key_sub]
 
-        for c in np.unique(adata_neighbors.obs[key]):
-            print('Analyzing cluster ' + str(c))
-            cells_in_c = (adata_neighbors.obs[key] == c)
-            ncells_in_c = adata_neighbors.obs[key].value_counts().loc[c]
-            key_sub = str(key) + '_sub'
-            clustering(adata_neighbors, restrict_to=(key, [c]),
-                key_added=key_sub) 
+        # Old category should not be present
+        categories = new_partition.value_counts().index
+        assert c not in categories
 
-            # Old category should not be present
-            assert (c not in
-                adata_neighbors.obs.loc[:, key_sub].value_counts().index)
+        # Cells in the original category are assigned only to new categories
+        print('Categories')
+        cat_counts = new_partition[cells_in_c].value_counts()
+        print(cat_counts)
 
-            # Cells in the original category are assigned only to new categories
-            print('Categories')
-            cat_counts = adata_neighbors.obs.loc[cells_in_c, key_sub].value_counts()
-            print(cat_counts)
+        ## Only cells of the original cluster have been assigned to
+        ## new categories
+        assert cat_counts.sum() == ncells_in_c
 
-            ## Only cells of the original cluster have been assigned to
-            ## new categories
-            assert cat_counts.sum() == ncells_in_c
+        print('Non-zero categories')
+        nonzero_cat = cat_counts[cat_counts > 0].index
+        print(nonzero_cat)
 
-            print('Non-zero categories')
-            nonzero_cat = (cat_counts > 0)
-            print(nonzero_cat)
-            nonzero_cat = (adata_neighbors.obs[key_sub].cat.categories
-                .to_series()[nonzero_cat].index)
-            print(nonzero_cat)
-
-            common_cat = (nonzero_cat & adata_neighbors.obs[key].cat.categories)
-            print('Common categories')
-            print(common_cat)
-            ## Only new categories
-            assert len(common_cat) == 0
+        common_cat = nonzero_cat & adata_neighbors.obs[key].cat.categories
+        print('Common categories')
+        print(common_cat)
+        ## Only new categories
+        assert len(common_cat) == 0
 
 
 def test_louvain_basic(adata_neighbors):
