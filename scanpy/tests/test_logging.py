a4f4607671e90d809401c37dd2da1f8181669720
Philipp A <flying-sheep@web.de>
2019-06-05 11:34:38 +0200

    Maybe works with capsys?

--- scanpy/tests/test_logging.py
+++ scanpy/tests/test_logging.py
@@ -15,38 +15,23 @@ def logging_state():
     s.verbosity = verbosity_orig
 
 
-class Recorder:
-    def __init__(self):
-        self.records = []
-
-    def write(self, s: str):
-        self.records.append(s)
-
-    def pop(self):
-        return self.records.pop()
-
-
 def test_defaults():
-    # This is only true when not in IPython,
-    # But I hope we’ll never run tests from there
-    assert s.logfile is sys.stderr
     assert s.logpath is None
 
 
-def test_formats(logging_state):
+def test_formats(capsys, logging_state):
+    s.logfile = sys.stderr
     s.verbosity = Verbosity.debug
-    rec = Recorder()
-    s.logfile = rec
     l.error('0')
-    assert rec.pop() == 'ERROR: 0\n'
+    assert capsys.readouterr().err == 'ERROR: 0\n'
     l.warning('1')
-    assert rec.pop() == 'WARNING: 1\n'
+    assert capsys.readouterr().err == 'WARNING: 1\n'
     l.info('2')
-    assert rec.pop().endswith(' | 2\n')
+    assert capsys.readouterr().err.endswith(' | 2\n')
     l.hint('3')
-    assert rec.pop() == '--> 3\n'
+    assert capsys.readouterr().err == '--> 3\n'
     l.debug('4')
-    assert rec.pop() == '    4\n'
+    assert capsys.readouterr().err == '    4\n'
 
 
 def test_logfile(tmp_path, logging_state):
@@ -68,7 +53,8 @@ def test_logfile(tmp_path, logging_state):
     assert s.logpath.read_text() == '--> test2\n'
 
 
-def test_timing(monkeypatch, logging_state):
+def test_timing(monkeypatch, capsys, logging_state):
+    s.logfile = sys.stderr
     counter = 0
 
     class IncTime:
@@ -80,14 +66,12 @@ def test_timing(monkeypatch, logging_state):
 
     monkeypatch.setattr(l, 'datetime', IncTime)
     s.verbosity = Verbosity.debug
-    rec = Recorder()
-    s.logfile = rec
 
     l.hint('1')
-    assert counter == 1 and rec.pop() == '--> 1\n'
+    assert counter == 1 and capsys.readouterr().err == '--> 1\n'
     start = l.info('2')
-    assert counter == 2 and rec.pop().endswith(' | 2\n')
+    assert counter == 2 and capsys.readouterr().err.endswith(' | 2\n')
     l.hint('3')
-    assert counter == 3 and rec.pop() == '--> 3\n'
+    assert counter == 3 and capsys.readouterr().err == '--> 3\n'
     l.info('4', time=start)
-    assert counter == 4 and rec.pop().endswith(' | 4 (0:00:02)\n')
+    assert counter == 4 and capsys.readouterr().err.endswith(' | 4 (0:00:02)\n')
