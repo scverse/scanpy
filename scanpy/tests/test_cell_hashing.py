ee0843c501be405e2962d3d5c762028c5855faf3
njbernstein <njbernstein@gmail.com>
2019-08-22 08:01:51 -0700

    Adding cell hashing tests

---
+++ scanpy/tests/test_cell_hashing.py
@@ -0,0 +1,19 @@
+from anndata import AnnData
+import scanpy as sc
+import pandas as pd
+
+
+def test_cell_hashing():
+    X = np.ones((10, 10))
+    for x in range(10):
+        X[x, x] = 1000
+    X[0, 1] = 1000
+    test_data = AnnData(X)
+    sc.pp.demultiplex_hashing(test_data)
+
+    ground_truth = [["Doublet", "0_1"]]
+    for x in range(1, 10):
+        ground_truth.append([str(x), str(x)])
+    ground_truth_df = pd.DataFrame(ground_truth, columns=["ID", "CLASSIFICATION"])
+    assert all(test_data.obs["ID"].values == ground_truth_df["ID"].values)
+    assert all(test_data.obs["CLASSIFICATION"].values == ground_truth_df["CLASSIFICATION"].values)
