185123239515f4e465b4962e0613ab538f1a761d
Philipp A <flying-sheep@web.de>
2019-09-06 15:12:42 +0200

    Better test IDs

--- scanpy/tests/test_docs.py
+++ scanpy/tests/test_docs.py
@@ -1,12 +1,13 @@
 import inspect
-import traceback
+from types import FunctionType
+from typing import Iterable
 
 import pytest
 
 import scanpy as sc
 
 
-def iterate_over_functions():
+def iterate_over_functions() -> Iterable[FunctionType]:
     functions_all = {}
     for mn in dir(sc):
         # skip privates and duplicates and stuff we haven’t formatted
@@ -19,14 +20,17 @@ def iterate_over_functions():
         module = sc.__dict__[mn]
         if not inspect.ismodule(module):
             continue
-        function_names = [fn for fn in dir(module) if inspect.isfunction(module.__dict__[fn])]
-        functions = {mn + '.' + fn: module.__dict__[fn] for fn in function_names}
+        function_names = [
+            fn for fn in dir(module)
+            if inspect.isfunction(module.__dict__[fn])
+        ]
+        functions = {f"{mn}.{fn}": module.__dict__[fn] for fn in function_names}
         functions_all.update(functions)
-    return functions_all
+    return functions_all.values()
 
 
-@pytest.mark.parametrize("descr,f", iterate_over_functions().items())
-def test_function_headers(descr, f):
+@pytest.mark.parametrize("f", iterate_over_functions())
+def test_function_headers(f):
     assert f.__doc__ is not None, f"{f} has no docstring"
     lines = getattr(f, "__orig_doc__", f.__doc__).split("\n")
     assert lines[0], "Expected single-line summary"
@@ -35,8 +39,9 @@ def test_function_headers(descr, f):
         if l and not l.startswith("    ")
     ]
     if any(broken):
+        name = f"{f.__module__}.{f.__qualname__}"
         msg = f'''\
-Header of function `{descr}`’s docstring should start with one-line description:
+Header of function `{name}`’s docstring should start with one-line description:
 
 ␣␣␣␣"""\\
 ␣␣␣␣My one-line␣description.
@@ -48,3 +53,7 @@ Header of function `{descr}`’s docstring should start with one-line descriptio
         _, lineno = inspect.getsourcelines(f)
         text = f">{lines[broken[0]]}<"
         raise SyntaxError(msg, (filename, lineno, 2, text))
+
+
+def test_plot_doc_signatures():
+    pass
