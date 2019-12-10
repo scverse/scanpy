3bf9a36203fce3830ffa4b5894ab65dc72615065
Philipp A <flying-sheep@web.de>
2019-12-10 19:30:37 +0100

    Ignore scipy.stats – It’s only 85ms

--- scanpy/tests/test_performance.py
+++ scanpy/tests/test_performance.py
@@ -28,7 +28,6 @@ def test_deferred_imports(imported_modules):
         'umap',  # neighbors, tl.umap
         'seaborn',  # plotting
         'sklearn.metrics',  # neighbors
-        'scipy.stats',  # tools._embedding_density
         'networkx',  # diffmap, paga, plotting._utils
         # TODO: 'matplotlib.pyplot',
         # TODO (maybe): 'numba',
