ef664af83bba9eef86e04a174adf4fd6d36cfac4
Awni Mousa <awnimousa@gmail.com>
2020-01-14 16:05:19 -0500

    Update test_harmony_timeseries_ext.py

--- scanpy/tests/test_harmony_timeseries_ext.py
+++ scanpy/tests/test_harmony_timeseries_ext.py
@@ -5,13 +5,25 @@ sample_names = ['sa1_Rep1', 'sa1_Rep2', 'sa2_Rep1', 'sa2_Rep2']
 adata_ref = sc.datasets.pbmc3k()
 n = int(adata_ref.shape[0] / 4)
 datalist = [adata_ref[n * i : n * (i + 1)] for i in range(4)]
+timepoints = [i.split("_")[0] for i in sample_names]
+for ad, sn, tp in zip(datalist, sample_names, timepoints):
+    ad.obs["time_points"] = tp
+    ad.obs["sample_name"] = sn
+adata = datalist[0].concatenate(*datalist[1:], join="outer")
+sc.pp.normalize_total(adata, target_sum=10000)
+sc.pp.log1p(adata)
+sc.pp.highly_variable_genes(adata, n_top_genes=1000, subset=True)
 
 
 def load_timepoints_from_anndata_list():
 
-    d = sce.tl.harmony_timeseries(data=datalist, sample_names=sample_names)
-    d.log_transform = True
-    d.process()
-    assert isinstance(
-        d.harmony_adata, sc.AnnData
+    d = sce.tl.harmony_timeseries(data=adata, tp="time_points")
+    aug_aff, aff = d.harmony_timeseries.core.augmented_affinity_matrix(
+        data_df=adata.to_df(),
+        timepoints=adata.obs["time_points"],
+        timepoint_connections=adata.uns["timepoint_connections"],
+        pc_components=300,
+    )
+    assert all(
+        [aug_aff.shape[0], aff.shape[0],]
     ), "harmony_timeseries augmented affinity matrix Error!"
