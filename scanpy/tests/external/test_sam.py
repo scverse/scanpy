bd692fc62ce9b795da08a66281ecbeb19a80c7ba
atarashansky <atarashansky@gmail.com>
2020-02-12 11:39:19 -0800

    whoops, forgot to black it

--- scanpy/tests/external/test_sam.py
+++ scanpy/tests/external/test_sam.py
@@ -8,12 +8,11 @@ pytest.importorskip("SAM")
 
 def test_sam():
     adata_ref = sc.datasets.pbmc3k()
-    ix = np.random.choice(adata_ref.shape[0],size=200,replace=False)
-    adata = adata_ref[ix,:].copy()
+    ix = np.random.choice(adata_ref.shape[0], size=200, replace=False)
+    adata = adata_ref[ix, :].copy()
     sc.pp.normalize_total(adata, target_sum=10000)
     sc.pp.log1p(adata)
-    sce.tl.sam(adata, inplace=True);
+    sce.tl.sam(adata, inplace=True)
     uns_keys = list(adata.uns.keys())
     obsm_keys = list(adata.obsm.keys())
-    assert all(['sam' in uns_keys, 'X_umap' in obsm_keys,
-                        'neighbors' in uns_keys])
+    assert all(['sam' in uns_keys, 'X_umap' in obsm_keys, 'neighbors' in uns_keys])
