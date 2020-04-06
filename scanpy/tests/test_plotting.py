ec927d6901c22037a9bb96f1b2ac5fe9c49e083d
giovp <giov.pll@gmail.com>
2020-04-06 15:58:31 +0200

    fix codacy and test

--- scanpy/tests/test_plotting.py
+++ scanpy/tests/test_plotting.py
@@ -822,15 +822,6 @@ def test_visium_circles(image_comparer):
 
     save_and_compare_images('master_spatial_visium')
 
-
-def test_visium_empty_img_key(image_comparer):
-    save_and_compare_images = image_comparer(ROOT, FIGS, tol=15)
-    adata = sc.read_visium(HERE / '_data' / 'visium_data' / '1.0.0')
-    adata.obs = adata.obs.astype({'array_row': 'str'})
-
     sc.pl.spatial(adata, img_key=None, color="array_row")
 
-    save_and_compare_images('master_spatial_visium_empty_image')
-
-    sc.pl.embedding(adata, basis="coords", color="array_row")
     save_and_compare_images('master_spatial_visium_empty_image')
