c5bb72b4825b13d2117d3c381acc0d0fccea706c
Fidel Ramírez <fidel.ramirez@gmail.com>
2020-07-09 15:05:06 +0200

    remove problematic docstrings for now to check if rtd pases...

--- scanpy/plotting/_dotplot.py
+++ scanpy/plotting/_dotplot.py
@@ -326,35 +326,35 @@ class DotPlot(BasePlot):
         colorbar_title: Optional[str] = DEFAULT_COLOR_LEGEND_TITLE,
         width: Optional[float] = DEFAULT_LEGENDS_WIDTH,
     ):
-        """
-        Configure legend parameters.
-
-        Parameters
-        ----------
-        show
-            Set to `False` to hide the default plot of the legends.
-        show_size_legend
-            Set to `False` to hide the the size legend
-        show_colorbar
-            Set to `False` to hide the the colorbar
-        size_title
-            Title for the dot size legend. Use "\n" to add line breaks.
-        colorbar_title
-            Title for the color bar. Use "\n" to add line breaks.
-        width
-            Width of the legends.
-
-        Returns
-        -------
-        DotPlot
-
-        Examples
-        --------
-        >>> adata = sc.datasets.pbmc68k_reduced()
-        >>> markers = {{'T-cell': 'CD3D', 'B-cell': 'CD79A', 'myeloid': 'CST3'}}
-        >>> dp = sc.pl.DotPlot(adata, markers, groupby='bulk_labels')
-        >>> dp.legend(colorbar_title='log(UMI counts + 1)').show()
-        """
+        # """\
+        # Configure legend parameters.
+        #
+        # Parameters
+        # ----------
+        # show
+        #     Set to `False` to hide the default plot of the legends.
+        # show_size_legend
+        #     Set to `False` to hide the the size legend
+        # show_colorbar
+        #     Set to `False` to hide the the colorbar
+        # size_title
+        #     Title for the dot size legend. Use "\n" to add line breaks.
+        # colorbar_title
+        #     Title for the color bar. Use "\n" to add line breaks.
+        # width
+        #     Width of the legends.
+        #
+        # Returns
+        # -------
+        # DotPlot
+        #
+        # Examples
+        # --------
+        # >>> adata = sc.datasets.pbmc68k_reduced()
+        # >>> markers = {{'T-cell': 'CD3D', 'B-cell': 'CD79A', 'myeloid': 'CST3'}}
+        # >>> dp = sc.pl.DotPlot(adata, markers, groupby='bulk_labels')
+        # >>> dp.legend(colorbar_title='log(UMI counts + 1)').show()
+        # """
 
         if not show:
             # turn of legends by setting width to 0
