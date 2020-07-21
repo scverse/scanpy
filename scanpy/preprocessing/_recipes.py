7d58f79b8f700265bf7f9735d640ebf65b0a8351
Isaac Virshup <ivirshup@gmail.com>
2020-07-21 19:45:32 +1000

    Format recipes

--- scanpy/preprocessing/_recipes.py
+++ scanpy/preprocessing/_recipes.py
@@ -4,7 +4,10 @@ from typing import Optional
 from anndata import AnnData
 
 from .. import preprocessing as pp
-from ._deprecated.highly_variable_genes import filter_genes_dispersion, filter_genes_cv_deprecated
+from ._deprecated.highly_variable_genes import (
+    filter_genes_dispersion,
+    filter_genes_cv_deprecated,
+)
 from ._normalization import normalize_total
 from .. import logging as logg
 from .._utils import AnyRandom
@@ -37,16 +40,17 @@ def recipe_weinreb17(
     """
     from ._deprecated import normalize_per_cell_weinreb16_deprecated, zscore_deprecated
     from scipy.sparse import issparse
+
     if issparse(adata.X):
         raise ValueError('`recipe_weinreb16 does not support sparse matrices.')
-    if copy: adata = adata.copy()
-    if log: pp.log1p(adata)
+    if copy:
+        adata = adata.copy()
+    if log:
+        pp.log1p(adata)
     adata.X = normalize_per_cell_weinreb16_deprecated(
         adata.X, max_fraction=0.05, mult_with_mean=True
     )
-    gene_subset = filter_genes_cv_deprecated(
-        adata.X, mean_threshold, cv_threshold
-    )
+    gene_subset = filter_genes_cv_deprecated(adata.X, mean_threshold, cv_threshold)
     adata._inplace_subset_var(gene_subset)  # this modifies the object itself
     X_pca = pp.pca(
         zscore_deprecated(adata.X),
@@ -60,10 +64,7 @@ def recipe_weinreb17(
 
 
 def recipe_seurat(
-    adata: AnnData,
-    log: bool = True,
-    plot: bool = False,
-    copy: bool = False,
+    adata: AnnData, log: bool = True, plot: bool = False, copy: bool = False,
 ) -> Optional[AnnData]:
     """\
     Normalization and filtering as of Seurat [Satija15]_.
@@ -73,17 +74,23 @@ def recipe_seurat(
     Expects non-logarithmized data.
     If using logarithmized data, pass `log=False`.
     """
-    if copy: adata = adata.copy()
+    if copy:
+        adata = adata.copy()
     pp.filter_cells(adata, min_genes=200)
     pp.filter_genes(adata, min_cells=3)
     normalize_total(adata, target_sum=1e4)
     filter_result = filter_genes_dispersion(
-        adata.X, min_mean=0.0125, max_mean=3, min_disp=0.5, log=not log)
+        adata.X, min_mean=0.0125, max_mean=3, min_disp=0.5, log=not log
+    )
     if plot:
-        from ..plotting import _preprocessing as ppp  # should not import at the top of the file
+        from ..plotting import (
+            _preprocessing as ppp,
+        )  # should not import at the top of the file
+
         ppp.filter_genes_dispersion(filter_result, log=not log)
     adata._inplace_subset_var(filter_result.gene_subset)  # filter genes
-    if log: pp.log1p(adata)
+    if log:
+        pp.log1p(adata)
     pp.scale(adata, max_value=10)
     return adata if copy else None
 
@@ -139,7 +146,8 @@ def recipe_zheng17(
     Returns or updates `adata` depending on `copy`.
     """
     start = logg.info('running recipe zheng17')
-    if copy: adata = adata.copy()
+    if copy:
+        adata = adata.copy()
     # only consider genes with more than 1 count
     pp.filter_genes(adata, min_counts=1)
     # normalize with total UMI count per cell
@@ -149,12 +157,14 @@ def recipe_zheng17(
     )
     if plot:  # should not import at the top of the file
         from ..plotting import _preprocessing as ppp
+
         ppp.filter_genes_dispersion(filter_result, log=True)
     # actually filter the genes, the following is the inplace version of
     #     adata = adata[:, filter_result.gene_subset]
     adata._inplace_subset_var(filter_result.gene_subset)  # filter genes
     normalize_total(adata)  # renormalize after filtering
-    if log: pp.log1p(adata)  # log transform: X = log(X + 1)
+    if log:
+        pp.log1p(adata)  # log transform: X = log(X + 1)
     pp.scale(adata)
     logg.info('    finished', time=start)
     return adata if copy else None
