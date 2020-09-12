663fbb29cb64604ed02d423de2cd2f502757d9be
dwnorton <64930308+dwnorton@users.noreply.github.com>
2020-09-12 16:40:02 +0100

    Added test parameterisation for dense and sparse AnnData (with currently failing sparse cases)

--- scanpy/tests/test_neighbors_metric.py
+++ scanpy/tests/test_neighbors_metric.py
@@ -1,10 +1,15 @@
 import scanpy as sc
 import pytest
+from scipy.sparse import csr_matrix
 
 
 @pytest.mark.parametrize('metric', ['cosine', 'euclidean', 'hellinger', 'll_dirichlet'])
-def test_neighbors_metric(metric):
+@pytest.mark.parametrize('adata_type', ['dense', 'sparse'])
+def test_neighbors_metric(metric, adata_type):
     adata = sc.datasets.pbmc68k_reduced()
+    if adata_type == 'sparse':
+        adata.X = csr_matrix(adata.X)
+
     sc.pp.neighbors(adata, random_state=0, use_rep="X", metric=metric)
 
     assert 'neighbors' in adata.uns
