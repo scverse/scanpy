f3a2c051e5dea5d667fd132433021760b7f432fa
Koncopd <koncopd@gmail.com>
2020-09-16 14:57:03 +0200

    non-visium data and rings for visium

--- scanpy/preprocessing/_spatial.py
+++ scanpy/preprocessing/_spatial.py
@@ -2,21 +2,37 @@ from ..get import _get_obs_rep
 
 import numpy as np
 from scipy import sparse
+from typing import Optional
 
 
 def spatial_connectivity(
-    adata: "AnnData", obsm: str = "spatial", key_added: str = "spatial_connectivity",
+    adata: "AnnData",
+    obsm: str = "spatial",
+    key_added: str = "spatial_connectivity",
+    n_rings: int = 1,
+    n_neigh: int = 6,
+    radius: Optional[float] = None,
+    coord_type: str = "visium",
 ):
-    """Creates an adjacency matrix of wells for visium data.
+    """
+    Creates graph from spatial coordinates
 
     Params
     ------
     adata
         The AnnData object.
     obsm
-        Key in obsm which stores coordinates of wells.
+        Key to spatial coordinates.
     key_added
-        Key in obsp to add spatial graph as.
+        Key added to connectivity matrix in obsp.
+    n_rings
+        Number of rings of neighbors for Visium data
+    n_neigh
+        Number of neighborhoods to consider for non-Visium data
+    radius
+        Radius of neighbors for non-Visium data
+    coord_type
+        Type of coordinate system (Visium vs. general coordinates)
 
     Usage
     -----
@@ -36,29 +52,49 @@ def spatial_connectivity(
     obsm: 'spatial'
     obsp: 'spatial_connectivity'
     """
-    adata.obsp[key_added] = _spatial_connectivity(_get_obs_rep(adata, obsm=obsm))
+    coords = _get_obs_rep(adata, obsm=obsm)
+    if coord_type == "visium":
+        Adj = _build_connectivity(coords, 6, None, True)
+        if n_rings > 1:
+            # get up to n_rings order connections
+            Adj += Adj ** n_rings
+            Adj.setdiag(0)
+            Adj.eliminate_zeros()
+            Adj.data[:] = 1.0
+        adata.obsp[key_added] = Adj
+    else:
+        adata.obsp[key_added] = _spatial_connectivity(coords, n_neigh, radius, False)
 
 
-def _spatial_connectivity(coords: np.ndarray):
+def _build_connectivity(
+    coords: np.ndarray, n_neigh: int, radius: float, neigh_correct: bool
+):
     """
-    Given the coordinates of hex based cells from a visium experiment, this returns an adjacency matrix for those cells.
-    
-    Usage
-    -----
-    >>> adata.obsp["spatial_connectivity"] = spatial_connectivity(adata.obsm["spatial"])
+    Build connectivity matrix from spatial coordinates
     """
     from sklearn.neighbors import NearestNeighbors
 
     N = coords.shape[0]
-    dists, row_indices = (
-        x.reshape(-1)
-        for x in NearestNeighbors(n_neighbors=6, metric="euclidean")
-        .fit(coords)
-        .kneighbors()
+
+    tree = NearestNeighbors(
+        n_neighbors=n_neigh or 6, radius=radius or 1, metric="euclidean"
     )
-    col_indices = np.repeat(np.arange(N), 6)
-    dist_cutoff = np.median(dists) * 1.3  # There's a small amount of sway
-    mask = dists < dist_cutoff
+    tree.fit(coords)
+
+    if radius is not None:
+        results = tree.radius_neighbors()
+        row_indices = np.concatenate(results[1])
+        lengths = [len(x) for x in results[1]]
+        col_indices = np.repeat(np.arange(N), lengths)
+    else:
+        results = tree.kneighbors()
+        dists, row_indices = (result.reshape(-1) for result in results)
+        col_indices = np.repeat(np.arange(N), n_neigh or 6)
+        if neigh_correct:
+            dist_cutoff = np.median(dists) * 1.3  # There's a small amount of sway
+            mask = dists < dist_cutoff
+            row_indices, col_indices = row_indices[mask], col_indices[mask]
+
     return sparse.csr_matrix(
-        (np.ones(mask.sum()), (row_indices[mask], col_indices[mask])), shape=(N, N)
+        (np.ones(len(row_indices)), (row_indices, col_indices)), shape=(N, N)
     )
