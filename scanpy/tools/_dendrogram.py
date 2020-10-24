57ffcb72108ea614b70a3498d9b4180b53d8e4fc
Mukund Varma <mmv1301@gmail.com>
2020-10-24 13:41:28 -0400

    address PR remarks

--- scanpy/tools/_dendrogram.py
+++ scanpy/tools/_dendrogram.py
@@ -156,10 +156,7 @@ def dendrogram(
 
     if inplace:
         if key_added is None:
-            if len(groupby) == 1:
-                key_added = f'dendrogram_{groupby[0]}'
-            else:
-                key_added = f'dendrogram_{"_".join(groupby)}'
+            key_added = f'dendrogram_{"_".join(groupby)}'
         logg.info(f'Storing dendrogram info using `.uns[{key_added!r}]`')
         adata.uns[key_added] = dat
     else:
