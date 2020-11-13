550b82fdb53f35890e60343b826dd19454600bdb
Rajesh Ilango <rilango@nvidia.com>
2020-11-12 19:21:48 -0800

    Bugfix: Failure due to cugraph api change in v0.16...
    Method cugraph.add_adj_list is replaced in RAPIDS v0.16 with from_cudf_adjlist.
    This patch will check for existence of 'add_adj_list' in the object before
    calling it.

--- scanpy/tools/_louvain.py
+++ scanpy/tools/_louvain.py
@@ -170,7 +170,12 @@ def louvain(
         else:
             weights = None
         g = cugraph.Graph()
-        g.add_adj_list(offsets, indices, weights)
+
+        if hasattr(g, 'add_adj_list'):
+            g.add_adj_list(offsets, indices, weights)
+        else:
+            g.from_cudf_adjlist(offsets, indices, weights)
+
         logg.info('    using the "louvain" package of rapids')
         louvain_parts, _ = cugraph.louvain(g)
         groups = louvain_parts.to_pandas().sort_values('vertex')[['partition']].to_numpy().ravel()
