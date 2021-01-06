f63cb15b6d72de8f8d3512dce4d760f141222f14
giovp <giov.pll@gmail.com>
2021-01-06 10:27:44 +0100

    minor fix docs

--- scanpy/plotting/_docs.py
+++ scanpy/plotting/_docs.py
@@ -74,8 +74,7 @@ legend_fontoutline
 size
     Point size. If `None`, is automatically computed as 120000 / n_cells.
     Can be a sequence containing the size for each cell. The order should be
-    the same as in adata.obs. If `img_key` not `None`, size is the scaling factor
-    for the spot size.
+    the same as in adata.obs.
 color_map
     Color map to use for continous variables. Can be a name or a
     :class:`~matplotlib.colors.Colormap` instance (e.g. `"magma`", `"viridis"`
