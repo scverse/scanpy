5fc12f4a918e21f0c57937b787d52040db046f01
Isaac Virshup <ivirshup@gmail.com>
2021-01-19 22:30:43 +1100

    Attach failing plots to CI results (#1587)
    * Attach failing plots via pytest-nunit
    * Document viewing plots through CI

--- scanpy/tests/conftest.py
+++ scanpy/tests/conftest.py
@@ -5,7 +5,7 @@ import matplotlib as mpl
 
 mpl.use('agg')
 from matplotlib import pyplot
-from matplotlib.testing.compare import compare_images
+from matplotlib.testing.compare import compare_images, make_test_filename
 import pytest
 
 import scanpy
@@ -21,24 +21,42 @@ def imported_modules():
     return IMPORTED
 
 
-def make_comparer(path_expected: Path, path_actual: Path, *, tol: int):
-    def save_and_compare(basename, tolerance=None):
-        path_actual.mkdir(parents=True, exist_ok=True)
-        out_path = path_actual / f'{basename}.png'
-        pyplot.savefig(out_path, dpi=40)
-        pyplot.close()
-        if tolerance is None:
-            tolerance = tol
-        res = compare_images(
-            str(path_expected / f'{basename}.png'), str(out_path), tolerance
-        )
-        assert res is None, res
+@pytest.fixture
+def check_same_image(add_nunit_attachment):
+    def _(pth1, pth2, *, tol: int, basename: str = ""):
+        def fmt_descr(descr):
+            if basename != "":
+                return f"{descr} ({basename})"
+            else:
+                return descr
 
-    return save_and_compare
+        pth1, pth2 = Path(pth1), Path(pth2)
+        try:
+            result = compare_images(str(pth1), str(pth2), tol=tol)
+            assert result is None, result
+        except Exception as e:
+            diff_pth = make_test_filename(pth2, 'failed-diff')
+            add_nunit_attachment(str(pth1), fmt_descr("Expected"))
+            add_nunit_attachment(str(pth2), fmt_descr("Result"))
+            if Path(diff_pth).is_file():
+                add_nunit_attachment(str(diff_pth), fmt_descr("Difference"))
+            raise e
+
+    return _
 
 
 @pytest.fixture
-def image_comparer():
+def image_comparer(check_same_image):
+    def make_comparer(path_expected: Path, path_actual: Path, *, tol: int):
+        def save_and_compare(basename, tol=tol):
+            path_actual.mkdir(parents=True, exist_ok=True)
+            out_path = path_actual / f'{basename}.png'
+            pyplot.savefig(out_path, dpi=40)
+            pyplot.close()
+            check_same_image(path_expected / f'{basename}.png', out_path, tol=tol)
+
+        return save_and_compare
+
     return make_comparer
 
 
