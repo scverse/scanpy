scverse/scanpy

commit 64f04d8441d5cfe61da0338312c2742f4ebbff8b

Use immutable defaults (#908)

scanpy/plotting/_utils.py
@@ -1,6 +1,6 @@
 import warnings
 import collections.abc as cabc
-from typing import Union, List, Sequence, Tuple
+from typing import Union, List, Sequence, Tuple, Collection, Optional
 
 import numpy as np
 from matplotlib import pyplot as pl
@@ -52,8 +52,10 @@ def matrix(matrix, xlabel=None, ylabel=None, xticks=None, yticks=None,
 
 def timeseries(X, **kwargs):
     """Plot X. See timeseries_subplot."""
-    pl.figure(figsize=(2*rcParams['figure.figsize'][0], rcParams['figure.figsize'][1]),
-              subplotpars=sppars(left=0.12, right=0.98, bottom=0.13))
+    pl.figure(
+        figsize=tuple(2*s for s in rcParams['figure.figsize']),
+        subplotpars=sppars(left=0.12, right=0.98, bottom=0.13),
+    )
     timeseries_subplot(X, **kwargs)
 
 
@@ -62,14 +64,15 @@ def timeseries_subplot(
     time=None,
     color=None,
     var_names=(),
-    highlightsX=(),
+    highlights_x=(),
     xlabel='',
     ylabel='gene expression',
     yticks=None,
     xlim=None,
     legend=True,
-    palette=None,
+    palette: Union[Sequence[str], Cycler, None] = None,
     color_map='viridis',
+    ax: Optional[Axes] = None,
 ):
     """\
     Plot X.
@@ -99,8 +102,10 @@ def timeseries_subplot(
         colors = np.array(palette[:len(levels)].by_key()['color'])
         subsets = [(x_range[color == l], X[color == l, :]) for l in levels]
 
+    if ax is None:
+        ax = pl.subplot()
     for i, (x, y) in enumerate(subsets):
-        pl.scatter(
+        ax.scatter(
             x, y,
             marker='.',
             edgecolor='face',
@@ -108,25 +113,26 @@ def timeseries_subplot(
             c=colors[i],
             label=var_names[i] if len(var_names) > 0 else '',
             cmap=color_map,
-            rasterized=settings._vector_friendly)
-    ylim = pl.ylim()
-    for ih, h in enumerate(highlightsX):
-        pl.plot([h, h], [ylim[0], ylim[1]], '--', color='black')
-    pl.ylim(ylim)
+            rasterized=settings._vector_friendly,
+        )
+    ylim = ax.get_ylim()
+    for h in highlights_x:
+        ax.plot([h, h], [ylim[0], ylim[1]], '--', color='black')
+    ax.set_ylim(ylim)
     if xlim is not None:
-        pl.xlim(xlim)
-    pl.xlabel(xlabel)
-    pl.ylabel(ylabel)
+        ax.set_xlim(xlim)
+    ax.set_xlabel(xlabel)
+    ax.set_ylabel(ylabel)
     if yticks is not None:
-        pl.yticks(yticks)
+        ax.set_yticks(yticks)
     if len(var_names) > 0 and legend:
-        pl.legend(frameon=False)
+        ax.legend(frameon=False)
 
 
 def timeseries_as_heatmap(
     X: np.ndarray,
-    var_names: np.ndarray = None,
-    highlightsX=None,
+    var_names: Collection[str] = (),
+    highlights_x=(),
     color_map=None,
 ):
     """\
@@ -139,10 +145,6 @@ def timeseries_as_heatmap(
     var_names
         Array of strings naming variables stored in columns of X.
     """
-    if highlightsX is None:
-        highlightsX = []
-    if var_names is None:
-        var_names = []
     if len(var_names) == 0:
         var_names = np.arange(X.shape[1])
     if var_names.ndim == 2:
@@ -150,32 +152,36 @@ def timeseries_as_heatmap(
 
     # transpose X
     X = X.T
-    minX = np.min(X)
+    min_x = np.min(X)
 
     # insert space into X
     if False:
-        # generate new array with highlightsX
+        # generate new array with highlights_x
         space = 10  # integer
-        Xnew = np.zeros((X.shape[0], X.shape[1] + space*len(highlightsX)))
+        x_new = np.zeros((X.shape[0], X.shape[1] + space*len(highlights_x)))
         hold = 0
         _hold = 0
         space_sum = 0
-        for ih, h in enumerate(highlightsX):
+        for ih, h in enumerate(highlights_x):
             _h = h + space_sum
-            Xnew[:, _hold:_h] = X[:, hold:h]
-            Xnew[:, _h:_h+space] = minX * np.ones((X.shape[0], space))
+            x_new[:, _hold:_h] = X[:, hold:h]
+            x_new[:, _h:_h+space] = min_x * np.ones((X.shape[0], space))
             # update variables
             space_sum += space
             _hold = _h + space
             hold = h
-        Xnew[:, _hold:] = X[:, hold:]
+        x_new[:, _hold:] = X[:, hold:]
 
-    fig = pl.figure(figsize=(1.5*4, 2*4))
-    im = pl.imshow(np.array(X, dtype=np.float_), aspect='auto',
-                   interpolation='nearest', cmap=color_map)
+    _, ax = pl.subplots(figsize=(1.5*4, 2*4))
+    ax.imshow(
+        np.array(X, dtype=np.float_),
+        aspect='auto',
+        interpolation='nearest',
+        cmap=color_map,
+    )
     pl.colorbar(shrink=0.5)
     pl.yticks(range(X.shape[0]), var_names)
-    for ih, h in enumerate(highlightsX):
+    for h in highlights_x:
         pl.plot([h, h], [0, X.shape[0]], '--', color='black')
     pl.xlim([0, X.shape[1]-1])
     pl.ylim([0, X.shape[0]-1])
@@ -254,7 +260,13 @@ def savefig(writekey, dpi=None, ext=None):
     pl.savefig(filename, dpi=dpi, bbox_inches='tight')
 
 
-def savefig_or_show(writekey, show=None, dpi=None, ext=None, save=None):
+def savefig_or_show(
+    writekey: str,
+    show: Optional[bool] = None,
+    dpi: Optional[int] = None,
+    ext: str = None,
+    save: Union[bool, str, None] = None,
+):
     if isinstance(save, str):
         # check whether `save` contains a figure extension
         if ext is None:
@@ -273,7 +285,9 @@ def savefig_or_show(writekey, show=None, dpi=None, ext=None, save=None):
     if save: pl.close()  # clear figure
 
 
-def default_palette(palette=None):
+def default_palette(
+    palette: Union[Sequence[str], Cycler, None] = None
+) -> Cycler:
     if palette is None: return rcParams['axes.prop_cycle']
     elif not isinstance(palette, Cycler): return cycler(color=palette)
     else: return palette
@@ -310,7 +324,11 @@ def _validate_palette(adata, key):
         adata.uns[color_key] = _palette
 
 
-def _set_colors_for_categorical_obs(adata, value_to_plot, palette):
+def _set_colors_for_categorical_obs(
+    adata,
+    value_to_plot,
+    palette: Union[str, Sequence[str], Cycler],
+):
     """
     Sets the adata.uns[value_to_plot + '_colors'] according to the given palette
 
@@ -322,7 +340,7 @@ def _set_colors_for_categorical_obs(adata, value_to_plot, palette):
         name of a valid categorical observation
     palette
         Palette should be either a valid :func:`~matplotlib.pyplot.colormaps` string,
-        a list of colors (in a format that can be understood by matplotlib,
+        a sequence of colors (in a format that can be understood by matplotlib,
         eg. RGB, RGBS, hex, or a cycler object with key='color'
 
     Returns

docs/conf.py
@@ -163,8 +163,11 @@ def insert_function_images(app, what, name, obj, options, lines):
 
 def autolink(url_template, title_template='{}'):
     from docutils import nodes
+    from types import MappingProxyType
 
-    def role(name, rawtext, text, lineno, inliner, options={}, content=[]):
+    mp = MappingProxyType({})
+
+    def role(name, rawtext, text, lineno, inliner, options=mp, content=()):
         url = url_template.format(text)
         title = title_template.format(text)
         node = nodes.reference(rawtext, title, refuri=url, **options)

scanpy/neighbors/__init__.py
@@ -1,3 +1,4 @@
+from types import MappingProxyType
 from typing import Union, Optional, Any, Mapping, Callable, NamedTuple, Generator, Tuple
 
 import numpy as np
@@ -42,7 +43,7 @@ def neighbors(
     random_state: Optional[Union[int, RandomState]] = 0,
     method: Optional[_Method] = 'umap',
     metric: Union[_Metric, _MetricFn] = 'euclidean',
-    metric_kwds: Mapping[str, Any] = {},
+    metric_kwds: Mapping[str, Any] = MappingProxyType({}),
     copy: bool = False,
 ) -> Optional[AnnData]:
     """\
@@ -167,7 +168,7 @@ def compute_neighbors_umap(
     n_neighbors: int,
     random_state: Optional[Union[int, RandomState]] = None,
     metric: Union[_Metric, _MetricFn] = 'euclidean',
-    metric_kwds: Mapping[str, Any] = {},
+    metric_kwds: Mapping[str, Any] = MappingProxyType({}),
     angular: bool = False,
     verbose: bool = False,
 ):
@@ -624,7 +625,7 @@ class Neighbors:
         random_state: Optional[Union[int, RandomState]] = 0,
         write_knn_indices: bool = False,
         metric: _Metric = 'euclidean',
-        metric_kwds: Mapping[str, Any] = {},
+        metric_kwds: Mapping[str, Any] = MappingProxyType({}),
     ) -> None:
         """\
         Compute distances and connectivities of neighbors.

scanpy/plotting/_tools/__init__.py
@@ -1,6 +1,7 @@
 import collections.abc as cabc
 import numpy as np
 import pandas as pd
+from cycler import Cycler
 from matplotlib.axes import Axes
 from matplotlib.figure import Figure
 from scipy.sparse import issparse
@@ -160,7 +161,7 @@ def dpt_timeseries(
         timeseries_as_heatmap(
             adata.X[adata.obs['dpt_order_indices'].values],
             var_names=adata.var_names,
-            highlightsX=adata.uns['dpt_changepoints'],
+            highlights_x=adata.uns['dpt_changepoints'],
             color_map=color_map,
         )
     else:
@@ -168,35 +169,45 @@ def dpt_timeseries(
         timeseries(
             adata.X[adata.obs['dpt_order_indices'].values],
             var_names=adata.var_names,
-            highlightsX=adata.uns['dpt_changepoints'],
+            highlights_x=adata.uns['dpt_changepoints'],
             xlim=[0, 1.3*adata.X.shape[0]],
         )
     pl.xlabel('dpt order')
     savefig_or_show('dpt_timeseries', save=save, show=show)
 
 
-def dpt_groups_pseudotime(adata, color_map=None, palette=None, show=None, save=None):
+def dpt_groups_pseudotime(
+    adata: AnnData,
+    color_map: Union[str, Colormap, None] = None,
+    palette: Union[Sequence[str], Cycler, None] = None,
+    show: Optional[bool] = None,
+    save: Union[bool, str, None] = None,
+):
     """Plot groups and pseudotime."""
-    pl.figure()
-    pl.subplot(211)
-    timeseries_subplot(adata.obs['dpt_groups'].cat.codes,
-                       time=adata.obs['dpt_order'].values,
-                       color=np.asarray(adata.obs['dpt_groups']),
-                       highlightsX=adata.uns['dpt_changepoints'],
-                       ylabel='dpt groups',
-                       yticks=(np.arange(len(adata.obs['dpt_groups'].cat.categories), dtype=int)
-                                     if len(adata.obs['dpt_groups'].cat.categories) < 5 else None),
-                       palette=palette)
-    pl.subplot(212)
+    _, (ax_grp, ax_ord) = pl.subplots(2, 1)
+    timeseries_subplot(
+        adata.obs['dpt_groups'].cat.codes,
+        time=adata.obs['dpt_order'].values,
+        color=np.asarray(adata.obs['dpt_groups']),
+        highlights_x=adata.uns['dpt_changepoints'],
+        ylabel='dpt groups',
+        yticks=(
+            np.arange(len(adata.obs['dpt_groups'].cat.categories), dtype=int)
+            if len(adata.obs['dpt_groups'].cat.categories) < 5 else None
+        ),
+        palette=palette,
+        ax=ax_grp,
+    )
     timeseries_subplot(
         adata.obs['dpt_pseudotime'].values,
         time=adata.obs['dpt_order'].values,
         color=adata.obs['dpt_pseudotime'].values,
         xlabel='dpt order',
-        highlightsX=adata.uns['dpt_changepoints'],
+        highlights_x=adata.uns['dpt_changepoints'],
         ylabel='pseudotime',
         yticks=[0, 1],
         color_map=color_map,
+        ax=ax_ord,
     )
     savefig_or_show('dpt_groups_pseudotime', save=save, show=show)
 
@@ -786,7 +797,7 @@ def sim(
                 adata.X,
                 var_names=adata.var_names,
                 xlim=[0, 1.25*adata.n_obs],
-                highlightsX=np.arange(tmax, n_realizations*tmax, tmax),
+                highlights_x=np.arange(tmax, n_realizations*tmax, tmax),
                 xlabel='realizations',
             )
         else:
@@ -794,7 +805,7 @@ def sim(
             timeseries_as_heatmap(
                 adata.X,
                 var_names=adata.var_names,
-                highlightsX=np.arange(tmax, n_realizations*tmax, tmax),
+                highlights_x=np.arange(tmax, n_realizations*tmax, tmax),
             )
         pl.xticks(
             np.arange(0, n_realizations*tmax, tmax),
@@ -809,7 +820,7 @@ def sim(
             X,
             var_names=adata.var_names,
             xlim=[0, 1.25*adata.n_obs],
-            highlightsX=np.arange(tmax, n_realizations*tmax, tmax),
+            highlights_x=np.arange(tmax, n_realizations*tmax, tmax),
             xlabel='index (arbitrary order)',
         )
         savefig_or_show('sim_shuffled', save=save, show=show)

scanpy/plotting/_tools/paga.py
@@ -1,6 +1,7 @@
 import warnings
 import collections.abc as cabc
 from pathlib import Path
+from types import MappingProxyType
 from typing import Optional, Union, List, Sequence, Mapping, Any, Tuple
 
 import numpy as np
@@ -154,7 +155,7 @@ def _compute_pos(
     init_pos=None,
     adj_tree=None,
     root=0,
-    layout_kwds: Mapping[str, Any] = {},
+    layout_kwds: Mapping[str, Any] = MappingProxyType({}),
 ):
     import networkx as nx
 
@@ -248,7 +249,7 @@ def paga(
     threshold: Optional[float] = None,
     color: Optional[str] = None,
     layout: Optional[_IGraphLayout] = None,
-    layout_kwds: Optional[Mapping[str, Any]] = None,
+    layout_kwds: Mapping[str, Any] = MappingProxyType({}),
     init_pos: Optional[np.ndarray] = None,
     root: Union[int, str, Sequence[int], None] = 0,
     labels: Union[str, Sequence[str], Mapping[str, str], None] = None,
@@ -259,7 +260,7 @@ def paga(
     fontsize: Optional[int] = None,
     fontweight: str = 'bold',
     fontoutline: Optional[int] = None,
-    text_kwds: Optional[Mapping[str, Any]] = None,
+    text_kwds: Mapping[str, Any] = MappingProxyType({}),
     node_size_scale: float = 1.,
     node_size_power: float = 0.5,
     edge_width_scale: float = 1.,
@@ -274,7 +275,7 @@ def paga(
     cmap: Union[str, Colormap]=None,
     cax: Optional[Axes] = None,
     colorbar=None,  # TODO: this seems to be unused
-    cb_kwds: Optional[Mapping[str, Any]] = None,
+    cb_kwds: Mapping[str, Any] = MappingProxyType({}),
     frameon: Optional[bool] = None,
     add_pos: bool = True,
     export_to_gexf: bool = False,
@@ -418,9 +419,6 @@ def paga(
     pl.paga_compare
     pl.paga_path
     """
-    if layout_kwds is None: layout_kwds = {}
-    if text_kwds is None: text_kwds = {}
-    if cb_kwds is None: cb_kwds = {}
 
     if groups is not None:  # backwards compat
         labels = groups
@@ -585,7 +583,7 @@ def _paga_graph(
     fontsize=None,
     fontweight=None,
     fontoutline=None,
-    text_kwds=None,
+    text_kwds: Mapping[str, Any] = MappingProxyType({}),
     node_size_scale=1.,
     node_size_power=0.5,
     edge_width_scale=1.,
@@ -599,13 +597,11 @@ def _paga_graph(
     export_to_gexf=False,
     colorbar=None,
     use_raw=True,
-    cb_kwds=None,
+    cb_kwds: Mapping[str, Any] = MappingProxyType({}),
     single_component=False,
     arrowsize=30,
 ):
     import networkx as nx
-    if text_kwds is None: text_kwds = {}
-    if cb_kwds is None: cb_kwds = {}
 
     node_labels = labels  # rename for clarity
     if (node_labels is not None
@@ -779,10 +775,11 @@ def _paga_graph(
         for count, n in enumerate(nx_g_solid.nodes()):
             nx_g_solid.node[count]['label'] = str(node_labels[count])
             nx_g_solid.node[count]['color'] = str(colors[count])
-            nx_g_solid.node[count]['viz'] = {
-                'position': {'x': 1000*pos[count][0],
-                             'y': 1000*pos[count][1],
-                             'z': 0}}
+            nx_g_solid.node[count]['viz'] = dict(position=dict(
+                x=1000 * pos[count][0],
+                y=1000 * pos[count][1],
+                z=0,
+            ))
         filename = settings.writedir / 'paga_graph.gexf'
         logg.warning(f'exporting to {filename}')
         settings.writedir.mkdir(parents=True, exist_ok=True)
@@ -807,8 +804,10 @@ def _paga_graph(
     if fontsize is None:
         fontsize = rcParams['legend.fontsize']
     if fontoutline is not None:
-        text_kwds['path_effects'] = [patheffects.withStroke(linewidth=fontoutline,
-                                                            foreground='w')]
+        text_kwds = dict(text_kwds)
+        text_kwds['path_effects'] = [
+            patheffects.withStroke(linewidth=fontoutline, foreground='w')
+        ]
     # usual scatter plot
     if not isinstance(colors[0], cabc.Mapping):
         n_groups = len(pos_array)
@@ -877,7 +876,8 @@ def paga_path(
     use_raw: bool = True,
     annotations: Sequence[str] = ('dpt_pseudotime',),
     color_map: Union[str, Colormap, None] = None,
-    color_maps_annotations: Optional[Mapping[str, Union[str, Colormap]]] = {'dpt_pseudotime': 'Greys'},
+    color_maps_annotations: Mapping[str, Union[str, Colormap]] =
+        MappingProxyType(dict(dpt_pseudotime='Greys')),
     palette_groups: Optional[Sequence[str]] = None,
     n_avg: int = 1,
     groups_key: Optional[str] = None,

scanpy/plotting/_tools/scatterplots.py
@@ -812,8 +812,15 @@ def _add_legend_or_colorbar(adata, ax, cax, categorical, value_to_plot, legend_l
         pl.colorbar(cax, ax=ax, pad=0.01, fraction=0.08, aspect=30)
 
 
-def _get_color_values(adata, value_to_plot, groups=None, palette=None, use_raw=False,
-                      gene_symbols=None, layer=None) -> Tuple[Union[np.ndarray, str], bool]:
+def _get_color_values(
+    adata,
+    value_to_plot,
+    groups=None,
+    palette: Union[str, Sequence[str], Cycler, None] = None,
+    use_raw=False,
+    gene_symbols=None,
+    layer=None,
+) -> Tuple[Union[np.ndarray, str], bool]:
     """
     Returns the value or color associated to each data point.
     For categorical data, the return value is list of colors taken

scanpy/queries/_queries.py
@@ -1,6 +1,6 @@
 import collections.abc as cabc
-from copy import copy
 from functools import singledispatch
+from types import MappingProxyType
 from typing import Any, Union, Optional, Iterable, Dict, Mapping
 
 import pandas as pd
@@ -205,7 +205,7 @@ def enrich(
     container: Iterable[str],
     *,
     org: str = "hsapiens",
-    gprofiler_kwargs: Mapping[str, Any] = {},
+    gprofiler_kwargs: Mapping[str, Any] = MappingProxyType({}),
 ) -> pd.DataFrame:
     """\
     Get enrichment for DE results.
@@ -262,7 +262,7 @@ def enrich(
             "This method requires the `gprofiler-official` module to be installed."
         )
     gprofiler = GProfiler(user_agent="scanpy", return_dataframe=True)
-    gprofiler_kwargs = copy(gprofiler_kwargs)
+    gprofiler_kwargs = dict(gprofiler_kwargs)
     for k in ["organism"]:
         if gprofiler_kwargs.get(k) is not None:
             raise ValueError(
@@ -283,7 +283,7 @@ def _enrich_anndata(
     log2fc_min: Optional[float] = None,
     log2fc_max: Optional[float] = None,
     gene_symbols: Optional[str] = None,
-    gprofiler_kwargs: dict = {},
+    gprofiler_kwargs: Mapping[str, Any] = MappingProxyType({}),
 ) -> pd.DataFrame:
     de = rank_genes_groups_df(
         adata,

scanpy/tools/_leiden.py
@@ -98,6 +98,7 @@ def leiden(
         raise ImportError(
             'Please install the leiden algorithm: `pip3 install leidenalg`.'
         )
+    partition_kwargs = dict(partition_kwargs)
 
     start = logg.info('running Leiden clustering')
     adata = adata.copy() if copy else adata
@@ -126,8 +127,6 @@ def leiden(
     # appending to whatever the user provided. It needs to be this way
     # as this allows for the accounting of a None resolution
     # (in the case of a partition variant that doesn't take it on input)
-    if partition_kwargs is None:
-        partition_kwargs = {}
     if use_weights:
         partition_kwargs['weights'] = np.array(g.es['weight']).astype(np.float64)
     partition_kwargs['n_iterations'] = n_iterations

commit 3439c54d28b8cfe651d7efe0050b73e06376aa4c

Pass generated img to plt.coloarbar
This fixes:
```
Traceback (most recent call last):
  File "/private/var/folders/df/6xqpqpcd7h73b6jpx9t6cwhw0000gn/T/tmpn9tl9wf0/job_working_directory/000/2/configs/tmp_r9i0cvx", line 15, in <module>
    sc.pl.dpt_timeseries(
  File "/Users/mvandenb/miniconda3/envs/anndata/lib/python3.8/site-packages/scanpy/plotting/_tools/__init__.py", line 171, in dpt_timeseries
    timeseries_as_heatmap(
  File "/Users/mvandenb/miniconda3/envs/anndata/lib/python3.8/site-packages/scanpy/plotting/_utils.py", line 206, in timeseries_as_heatmap
    pl.colorbar(shrink=0.5)
  File "/Users/mvandenb/miniconda3/envs/anndata/lib/python3.8/site-packages/matplotlib/pyplot.py", line 2188, in colorbar
    raise RuntimeError('No mappable was found to use for colorbar '
RuntimeError: No mappable was found to use for colorbar creation. First define a mappable such as an image (with imshow) or a contour set (with contourf).
```
I see that in other places plt.colobar is used in this module you're
doing the same thing. I believe this broke in
https://github.com/theislab/scanpy/commit/64f04d8441d5cfe61da0338312c2742f4ebbff8b.

scanpy/plotting/_utils.py
@@ -197,13 +197,13 @@ def timeseries_as_heatmap(
         x_new[:, _hold:] = X[:, hold:]
 
     _, ax = pl.subplots(figsize=(1.5 * 4, 2 * 4))
-    ax.imshow(
+    img = ax.imshow(
         np.array(X, dtype=np.float_),
         aspect='auto',
         interpolation='nearest',
         cmap=color_map,
     )
-    pl.colorbar(shrink=0.5)
+    pl.colorbar(img, shrink=0.5)
     pl.yticks(range(X.shape[0]), var_names)
     for h in highlights_x:
         pl.plot([h, h], [0, X.shape[0]], '--', color='black')