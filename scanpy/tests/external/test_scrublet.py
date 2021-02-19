d6adcd133f5c0a4ce5e73bb7ceb005f2abba18e2
Jonathan Manning <jmanning@ebi.ac.uk>
2021-02-19 15:13:32 +0000

    Test with scrublet for differences in response to non-standard parameter settings

--- scanpy/tests/external/test_scrublet.py
+++ scanpy/tests/external/test_scrublet.py
@@ -2,6 +2,8 @@ import pytest
 
 import scanpy as sc
 import scanpy.external as sce
+import io
+from contextlib import redirect_stdout
 
 
 def test_scrublet():
@@ -22,6 +24,48 @@ def test_scrublet():
     assert "doublet_score" in adata.obs.columns
 
     assert adata.obs["predicted_doublet"].any(), "Expect some doublets to be identified"
+
+
+def test_scrublet_params():
+    """
+    Test that Scrublet args are passed.
+
+    Check that changes to parameters change scrublet results.
+    """
+    pytest.importorskip("scrublet")
+
+    adata = sc.datasets.pbmc3k()
+
+    # Get the default output
+
+    f = io.StringIO()
+    with redirect_stdout(f):
+        sce.pp.scrublet(adata)
+
+    default = f.getvalue()
+
+    test_params = {
+        'expected_doublet_rate': 0.1,
+        'synthetic_doublet_umi_subsampling': 0.8,
+        'knn_dist_metric': 'manhattan',
+        'normalize_variance': False,
+        'log_transform': True,
+        'mean_center': False,
+        'n_prin_comps': 10,
+        'n_neighbors': 2,
+        'threshold': 0.1,
+    }
+
+    # Test each parameter and make sure something changes
+
+    for param in test_params.keys():
+        test_args = {'adata': adata, param: test_params[param]}
+        f = io.StringIO()
+        with redirect_stdout(f):
+            sc.external.pp.scrublet(**test_args)
+        diff_result = f.getvalue()
+
+        assert diff_result != default
 
 
 def test_scrublet_simulate_doublets():
