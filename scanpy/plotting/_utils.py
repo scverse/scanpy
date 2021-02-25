a5b12900eeda627cbe06f007ab72a7e91267e59c
Zethson <lukas.heumos@posteo.net>
2021-02-25 13:21:16 +0100

    review round 3
    Signed-off-by: Zethson <lukas.heumos@posteo.net>

--- scanpy/plotting/_utils.py
+++ scanpy/plotting/_utils.py
@@ -937,7 +937,7 @@ def hierarchy_pos(G, root, levels=None, width=1.0, height=1.0):
     if levels is None:
         levels = make_levels({})
     else:
-        levels = {level: {TOTAL: levels[level], CURRENT: 0} for level in levels}
+        levels = {l: {TOTAL: levels[l], CURRENT: 0} for l in levels}
     vert_gap = height / (max([level for level in levels]) + 1)
     return make_pos({})
 
