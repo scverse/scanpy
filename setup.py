d2bb2a9b5ed44f8868951544f250064d7d5c6f20
Philipp A <flying-sheep@web.de>
2021-03-01 11:27:19 +0100

    Restore setup.py

--- setup.py
+++ setup.py
@@ -26,8 +26,7 @@ setup(
     license='BSD',
     python_requires='>=3.6',
     install_requires=[
-        line.strip()
-        for line in Path('requirements.txt').read_text('utf-8').splitlines()
+        l.strip() for l in Path('requirements.txt').read_text('utf-8').splitlines()
     ],
     extras_require=dict(
         louvain=['python-igraph', 'louvain>=0.6,!=0.6.2'],
