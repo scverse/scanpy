d89105f368fe13b36232d1b9a1531052d47df204
Zethson <lukas.heumos@posteo.net>
2021-03-16 14:16:14 +0100

    import Literal
    Signed-off-by: Zethson <lukas.heumos@posteo.net>

--- scanpy/get/get.py
+++ scanpy/get/get.py
@@ -6,7 +6,7 @@ import pandas as pd
 from scipy.sparse import spmatrix
 
 from anndata import AnnData
-import warnings
+from ._compat import Literal
 
 # --------------------------------------------------------------------------------
 # Plotting data helpers
@@ -96,7 +96,7 @@ def rank_genes_groups_df(
 def _check_indices(
     dim_df: pd.DataFrame,
     alt_index: pd.Index,
-    dim: "Literal['obs', 'var']",  # noqa: F821
+    dim: "Literal['obs', 'var']",
     keys: List[str],
     alias_index: Optional[pd.Index] = None,
     use_raw: bool = False,
