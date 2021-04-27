775f56c2f609756dd53e38e456086bd9a2cab68e
mbuttner <maren.buettner@gmail.com>
2021-04-27 11:50:07 +0200

    Inline documentation matrixplot, fix typos

--- scanpy/plotting/_tools/__init__.py
+++ scanpy/plotting/_tools/__init__.py
@@ -820,9 +820,14 @@ def rank_genes_groups_matrixplot(
     Also, only genes with a log fold change of 3 or more are shown.
     .. plot::
         :context: close-figs
-        sc.pl.rank_genes_groups_dotplot(adata,
-    ... n_genes=4, values_to_plot="logfoldchanges", cmap='bwr',
-    ... vmin=-4, vmax=4, min_logfoldchange=3, colorbar_title='log fold change')
+        sc.pl.rank_genes_groups_matrixplot(adata,
+        n_genes=4,
+        values_to_plot="logfoldchanges",
+        cmap='bwr',
+        vmin=-4,
+        vmax=4,
+        min_logfoldchange=3,
+        colorbar_title='log fold change')
     """
 
     return _rank_genes_groups_plot(
