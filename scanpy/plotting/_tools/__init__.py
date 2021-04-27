e10de4e88f78ae3499e265163be80ede229fd82d
biola <fabiola.curion@gmail.com>
2021-04-27 11:10:29 +0100

    added example for PCA loadings plot

--- scanpy/plotting/_tools/__init__.py
+++ scanpy/plotting/_tools/__init__.py
@@ -96,6 +96,24 @@ def pca_loadings(
         If `True` or a `str`, save the figure.
         A string is appended to the default filename.
         Infer the filetype if ending on {`'.pdf'`, `'.png'`, `'.svg'`}.
+
+    Examples
+    --------
+    .. plot::
+        :context: close-figs
+
+        import scanpy as sc
+        import numpy as np
+        import pandas as pd
+        adata = sc.datasets.pbmc68k_reduced()
+        sc.tl.pca(adata, svd_solver='arpack')
+
+    .. plot::
+        Show first 3 components loadings
+        :context: close-figs
+        sc.pl.pca_loadings(adata, components = '1,2,3')
+
+
     """
     if components is None:
         components = [1, 2, 3]
