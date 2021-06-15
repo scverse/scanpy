ecd68cb2e6c4cb3c44589217322bd95ca9ab7d69
ktpolanski <krzysztof_polanski@o2.pl>
2021-06-15 12:19:34 +0200

    revert to single tick

--- scanpy/external/pp/_bbknn.py
+++ scanpy/external/pp/_bbknn.py
@@ -52,18 +52,18 @@ def bbknn(
     Params
     ------
     adata
-        Needs the PCA computed and stored in ``adata.obsm["X_pca"]``.
+        Needs the PCA computed and stored in `adata.obsm["X_pca"]`.
     batch_key
-        ``adata.obs`` column name discriminating between your batches.
+        `adata.obs` column name discriminating between your batches.
     use_rep
-        The dimensionality reduction in ``.obsm`` to use for neighbour detection. Defaults to PCA.
+        The dimensionality reduction in `.obsm` to use for neighbour detection. Defaults to PCA.
     approx
-        If ``True``, use approximate neighbour finding - annoy or pyNNDescent. This results
+        If `True`, use approximate neighbour finding - annoy or pyNNDescent. This results
         in a quicker run time for large datasets while also potentially increasing the degree of
         batch correction.
     use_annoy
-        Only used when ``approx=True``. If ``True``, will use annoy for neighbour finding. If
-        ``False``, will use pyNNDescent instead.
+        Only used when `approx=True`. If `True`, will use annoy for neighbour finding. If
+        `False`, will use pyNNDescent instead.
     metric
         What distance metric to use. The options depend on the choice of neighbour algorithm.
 
@@ -71,7 +71,7 @@ def bbknn(
 
         Annoy supports "angular", "manhattan" and "hamming".
 
-        PyNNDescent supports metrics listed in ``pynndescent.distances.named_distances``
+        PyNNDescent supports metrics listed in `pynndescent.distances.named_distances`
         and custom functions, including compiled Numba code.
 
         >>> pynndescent.distances.named_distances.keys()
@@ -81,14 +81,14 @@ def bbknn(
         'kantorovich', 'wasserstein', 'tsss', 'true_angular', 'hamming', 'jaccard', 'dice', 'matching', 'kulsinski',
         'rogerstanimoto', 'russellrao', 'sokalsneath', 'sokalmichener', 'yule'])
 
-        KDTree supports members of the ``sklearn.neighbors.KDTree.valid_metrics`` list, or parameterised
-        ``sklearn.neighbors.DistanceMetric`` `objects
+        KDTree supports members of the `sklearn.neighbors.KDTree.valid_metrics` list, or parameterised
+        `sklearn.neighbors.DistanceMetric` `objects
         <https://scikit-learn.org/stable/modules/generated/sklearn.neighbors.DistanceMetric.html>`_:
 
         >>> sklearn.neighbors.KDTree.valid_metrics
         ['p', 'chebyshev', 'cityblock', 'minkowski', 'infinity', 'l2', 'euclidean', 'manhattan', 'l1']
     copy
-        If ``True``, return a copy instead of writing to the supplied adata.
+        If `True`, return a copy instead of writing to the supplied adata.
     neighbors_within_batch
         How many top neighbours to report for each batch; total number of neighbours in
         the initial k-nearest-neighbours computation will be this number times the number
@@ -100,7 +100,7 @@ def bbknn(
         Trim the neighbours of each cell to these many top connectivities. May help with
         population independence and improve the tidiness of clustering. The lower the value the
         more independent the individual populations, at the cost of more conserved batch effect.
-        If ``None``, sets the parameter value automatically to 10 times ``neighbors_within_batch``
+        If `None`, sets the parameter value automatically to 10 times `neighbors_within_batch`
         times the number of batches. Set to 0 to skip.
     annoy_n_trees
         Only used with annoy neighbour identification. The number of trees to construct in the
@@ -114,7 +114,7 @@ def bbknn(
         Only used with pyNNDescent neighbour identification. The RNG seed to use when creating
         the graph.
     use_faiss
-        If ``approx=False`` and the metric is "euclidean", use the faiss package to compute
+        If `approx=False` and the metric is "euclidean", use the faiss package to compute
         nearest neighbours if installed. This improves performance at a minor cost to numerical
         precision as faiss operates on float32.
     set_op_mix_ratio
