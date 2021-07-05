8b3e5d4bcd6e164f2862454dd8117720694c88d7
Iwo Kucinski <iwo.kucinski@gmail.com>
2021-07-05 09:20:18 +0100

    Removed leftover line.

--- scanpy/tests/test_score_genes.py
+++ scanpy/tests/test_score_genes.py
@@ -54,7 +54,6 @@ def test_score_with_reference():
     and stored as a pickle object in ./data
     """
 
-    np.random.seed(123)
     adata = sc.datasets.paul15()
     sc.pp.normalize_per_cell(adata, counts_per_cell_after=10000)
     sc.pp.scale(adata)
