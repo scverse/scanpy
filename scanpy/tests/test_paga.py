44d43052b13dd1791e3bc7a966a5827e2b2eded9
MeeseeksMachine <39504233+meeseeksmachine@users.noreply.github.com>
2021-11-03 08:28:32 -0700

    Backport PR #1922: Fix paga exact reproducibility (#2035)
    Co-authored-by: Isaac Virshup <ivirshup@gmail.com>

--- scanpy/tests/test_paga.py
+++ scanpy/tests/test_paga.py
@@ -2,6 +2,7 @@ from functools import partial
 from pathlib import Path
 
 from matplotlib import cm
+import numpy as np
 
 import scanpy as sc
 
@@ -86,3 +87,21 @@ def test_paga_compare(image_comparer):
     sc.pl.paga_compare(pbmc, basis="umap", show=False)
 
     save_and_compare_images('master_paga_compare_pbmc3k')
+
+
+def test_paga_positions_reproducible():
+    """Check exact reproducibility and effect of random_state on paga positions"""
+    # https://github.com/theislab/scanpy/issues/1859
+    pbmc = sc.datasets.pbmc68k_reduced()
+    sc.tl.paga(pbmc, "bulk_labels")
+
+    a = pbmc.copy()
+    b = pbmc.copy()
+    c = pbmc.copy()
+
+    sc.pl.paga(a, show=False, random_state=42)
+    sc.pl.paga(b, show=False, random_state=42)
+    sc.pl.paga(c, show=False, random_state=13)
+
+    np.testing.assert_array_equal(a.uns["paga"]["pos"], b.uns["paga"]["pos"])
+    assert a.uns["paga"]["pos"].tolist() != c.uns["paga"]["pos"].tolist()
