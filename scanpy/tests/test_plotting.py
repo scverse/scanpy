80fcd33bb4240a3a43ad0bd31ca84fb8e6a60da9
Edward S. Rice <edward.rice@missouri.edu>
2021-11-13 12:49:20 +0100

    Fix path typo
    Co-authored-by: Isaac Virshup <ivirshup@gmail.com>

--- scanpy/tests/test_plotting.py
+++ scanpy/tests/test_plotting.py
@@ -1258,7 +1258,7 @@ def test_scatter_no_basis_raw(check_same_image, pbmc_filtered):
     # and also to:
     sc.pl.scatter(pbmc_filtered.raw.to_adata(), x='EGFL7', y='F12', color='FAM185A')
     plt.savefig(path3)
-    check_same_image(path1, path2, tol=15)
+    check_same_image(path1, path3, tol=15)
 
 
 @pytest.mark.parametrize(
