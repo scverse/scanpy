e3be9e5eb04d05f2bc963307b1da4426d435c4a8
MeeseeksMachine <39504233+meeseeksmachine@users.noreply.github.com>
2021-12-01 09:26:27 -0800

    Backport PR #2063: Make louvain optional for tests (#2066)
    Co-authored-by: Isaac Virshup <ivirshup@gmail.com>

--- scanpy/tests/test_clustering.py
+++ scanpy/tests/test_clustering.py
@@ -19,6 +19,9 @@ def test_leiden_basic(adata_neighbors):
     ],
 )
 def test_clustering_subset(adata_neighbors, clustering, key):
+    if clustering == sc.tl.louvain:
+        pytest.importorskip("louvain")
+
     clustering(adata_neighbors, key_added=key)
 
     for c in adata_neighbors.obs[key].unique():
@@ -46,6 +49,8 @@ def test_clustering_subset(adata_neighbors, clustering, key):
 
 
 def test_louvain_basic(adata_neighbors):
+    pytest.importorskip("louvain")
+
     sc.tl.louvain(adata_neighbors)
     sc.tl.louvain(adata_neighbors, use_weights=True)
     sc.tl.louvain(adata_neighbors, use_weights=True, flavor="igraph")
@@ -53,7 +58,7 @@ def test_louvain_basic(adata_neighbors):
 
 
 def test_partition_type(adata_neighbors):
-    import louvain
+    louvain = pytest.importorskip("louvain")
 
     sc.tl.louvain(adata_neighbors, partition_type=louvain.RBERVertexPartition)
     sc.tl.louvain(adata_neighbors, partition_type=louvain.SurpriseVertexPartition)
