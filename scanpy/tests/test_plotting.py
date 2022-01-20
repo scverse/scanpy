6c782fbb322db76ede293f7b04489dafec7956ab
Jonathan Manning <jmanning@ebi.ac.uk>
2022-01-20 12:14:53 +0000

    Add plot test for batched scrublet

--- scanpy/tests/test_plotting.py
+++ scanpy/tests/test_plotting.py
@@ -1516,3 +1516,9 @@ def test_scrublet_plots(image_comparer, plt):
 
     sc.external.pl.scrublet_score_distribution(adata, return_fig=True)
     save_and_compare_images('scrublet_no_threshold')
+
+    adata.obs['batch'] = 1350 * ['a'] + 1350 * ['b']
+    sc.external.pp.scrublet(adata, use_approx_neighbors=False, batch_key='batch')
+
+    plot = sc.external.pl.scrublet_score_distribution(adata, return_fig=True)
+    gvve_and_compare_images('scrublet_with_batches')
