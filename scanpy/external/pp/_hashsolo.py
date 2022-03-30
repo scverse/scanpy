scverse/scanpy

commit 8eeb3fc229d5daee8961c32756becae4b1504a36

Addresses hashsolo pre_existing_cluster bugs (#1483)
Fixed pre_existing_clusters argument

Fixed hashsolo example to include columns

* spelling

* adding hashsolo to docs

* fixing docs

* getting rid of slash

* removed typing from docstring

* docs

* spacing

scanpy/external/pp/_hashsolo.py
@@ -26,12 +26,14 @@ hypotheses in a bayesian fashion, and select the most probable hypothesis.
 
 def _calculate_log_likelihoods(data, number_of_noise_barcodes):
     """Calculate log likelihoods for each hypothesis, negative, singlet, doublet
+
     Parameters
     ----------
     data : np.ndarray
         cells by hashing counts matrix
     number_of_noise_barcodes : int,
         number of barcodes to used to calculated noise distribution
+
     Returns
     -------
     log_likelihoods_for_each_hypothesis : np.ndarray
@@ -43,6 +45,7 @@ def _calculate_log_likelihoods(data, number_of_noise_barcodes):
     def gaussian_updates(data, mu_o, std_o):
         """Update parameters of your gaussian
         https://www.cs.ubc.ca/~murphyk/Papers/bayesGauss.pdf
+
         Parameters
         ----------
         data : np.array
@@ -51,6 +54,7 @@ def _calculate_log_likelihoods(data, number_of_noise_barcodes):
             global mean for hashing count distribution
         std_o : float,
             global std for hashing count distribution
+
         Returns
         -------
         float
@@ -73,11 +77,8 @@ def _calculate_log_likelihoods(data, number_of_noise_barcodes):
 
     all_indices = np.empty(data.shape[0])
     num_of_barcodes = data.shape[1]
-    number_of_non_noise_barcodes = (
-        num_of_barcodes - number_of_noise_barcodes
-        if number_of_noise_barcodes is not None
-        else 2
-    )
+    number_of_non_noise_barcodes = num_of_barcodes - number_of_noise_barcodes
+
     num_of_noise_barcodes = num_of_barcodes - number_of_non_noise_barcodes
 
     # assume log normal
@@ -201,6 +202,7 @@ def _calculate_log_likelihoods(data, number_of_noise_barcodes):
 def _calculate_bayes_rule(data, priors, number_of_noise_barcodes):
     """
     Calculate bayes rule from log likelihoods
+
     Parameters
     ----------
     data : np.array
@@ -215,6 +217,7 @@ def _calculate_bayes_rule(data, priors, number_of_noise_barcodes):
         in the transcriptome space, e.g. UMI counts, pct mito reads, etc.
     number_of_noise_barcodes : int
         number of barcodes to used to calculated noise distribution
+
     Returns
     -------
     bayes_dict_results : dict
@@ -246,21 +249,22 @@ def hashsolo(
     cell_hashing_columns: list,
     priors: list = [0.01, 0.8, 0.19],
     pre_existing_clusters: str = None,
-    number_of_noise_barcodes: int = None,
+    number_of_noise_barcodes: int = 2,
     inplace: bool = True,
 ):
     """Probabilistic demultiplexing of cell hashing data using HashSolo [Bernstein20]_.
 
     .. note::
         More information and bug reports `here <https://github.com/calico/solo>`__.
+
     Parameters
     ----------
-    adata : anndata.AnnData
+    adata
         Anndata object with cell hashes in .obs columns
-    cell_hashing_columns : list,
+    cell_hashing_columns
         list specifying which columns in adata.obs
         are cell hashing counts
-    priors : list,
+    priors
         a list of your prior for each hypothesis
         first element is your prior for the negative hypothesis
         second element is your prior for the singlet hypothesis
@@ -268,17 +272,18 @@ def hashsolo(
         We use [0.01, 0.8, 0.19] by default because we assume the barcodes
         in your cell hashing matrix are those cells which have passed QC
         in the transcriptome space, e.g. UMI counts, pct mito reads, etc.
-    pre_existing_clusters : str
+    pre_existing_clusters
         column in adata.obs for how to break up demultiplexing
         for example leiden or cell types, not batches though
-    number_of_noise_barcodes : int,
+    number_of_noise_barcodes
         Use this if you wish change the number of barcodes used to create the
         noise distribution. The default is number of cell hashes - 2.
-    inplace : bool
+    inplace
         To do operation in place
+
     Returns
     -------
-    adata : AnnData
+    adata
         if inplace is False returns AnnData with demultiplexing results
         in .obs attribute otherwise does is in place
 
@@ -287,16 +292,22 @@ def hashsolo(
     >>> import anndata
     >>> import scanpy.external as sce
     >>> data = anndata.read("data.h5ad")
-    >>> sce.pp.hashsolo(data)
+    >>> sce.pp.hashsolo(data, ['Hash1', 'Hash2', 'Hash3'])
     >>> data.obs.head()
     """
     print(
-        "Please cite HashSolo paper: \nhttps://www.cell.com/cell-systems/fulltext/S2405-4712(20)30195-2"
+        "Please cite HashSolo paper:\nhttps://www.cell.com/cell-systems/fulltext/S2405-4712(20)30195-2"
     )
 
     data = adata.obs[cell_hashing_columns].values
     if not check_nonnegative_integers(data):
         raise ValueError("Cell hashing counts must be non-negative")
+    if number_of_noise_barcodes >= len(cell_hashing_columns):
+        raise ValueError(
+            "number_of_noise_barcodes must be at least one less \
+        than the number of samples you have as determined by the number of \
+        cell_hashing_columns you've given as input  "
+        )
     num_of_cells = adata.shape[0]
     results = pd.DataFrame(
         np.zeros((num_of_cells, 6)),
@@ -314,7 +325,7 @@ def hashsolo(
         cluster_features = pre_existing_clusters
         unique_cluster_features = np.unique(adata.obs[cluster_features])
         for cluster_feature in unique_cluster_features:
-            cluster_feature_bool_vector = adata.obs[cluster_features == cluster_feature]
+            cluster_feature_bool_vector = adata.obs[cluster_features] == cluster_feature
             posterior_dict = _calculate_bayes_rule(
                 data[cluster_feature_bool_vector], priors, number_of_noise_barcodes
             )

scanpy/external/__init__.py
@@ -34,6 +34,14 @@ Data integration
    pp.harmony_integrate
    pp.mnn_correct
 
+Sample demultiplexing, Doublet detection
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+.. autosummary::
+   :toctree: .
+
+   pp.hashsolo
+
 Imputation
 ~~~~~~~~~~
 

commit b8c2cf56d645071153e1f46e100176de9a3a1e74

Fix default `number_of_noise_barcodes` for hashsolo (#2190)
* apply black

* fix number of noise barcodes

commit #8eeb3fc229d5daee8961c32756becae4b1504a36 broke correct calculation of `number_of_noise_barcodes`

* fix precommit hook

* Added more noise to test data

With the given test data the signal was to clear for an acutal test case.

scanpy/external/pp/_hashsolo.py
@@ -77,7 +77,11 @@ def _calculate_log_likelihoods(data, number_of_noise_barcodes):
 
     all_indices = np.empty(data.shape[0])
     num_of_barcodes = data.shape[1]
-    number_of_non_noise_barcodes = num_of_barcodes - number_of_noise_barcodes
+    number_of_non_noise_barcodes = (
+        num_of_barcodes - number_of_noise_barcodes
+        if number_of_noise_barcodes is not None
+        else 2
+    )
 
     num_of_noise_barcodes = num_of_barcodes - number_of_non_noise_barcodes
 
@@ -91,11 +95,13 @@ def _calculate_log_likelihoods(data, number_of_noise_barcodes):
     # barcodes with rank < k are considered to be noise
     global_signal_counts = np.ravel(data_sort[:, -1])
     global_noise_counts = np.ravel(data_sort[:, :-number_of_non_noise_barcodes])
-    global_mu_signal_o, global_sigma_signal_o = np.mean(global_signal_counts), np.std(
-        global_signal_counts
+    global_mu_signal_o, global_sigma_signal_o = (
+        np.mean(global_signal_counts),
+        np.std(global_signal_counts),
     )
-    global_mu_noise_o, global_sigma_noise_o = np.mean(global_noise_counts), np.std(
-        global_noise_counts
+    global_mu_noise_o, global_sigma_noise_o = (
+        np.mean(global_noise_counts),
+        np.std(global_noise_counts),
     )
 
     noise_params_dict = {}
@@ -196,7 +202,11 @@ def _calculate_log_likelihoods(data, number_of_noise_barcodes):
         # each cell and each hypothesis probability
         for prob_idx, log_prob in enumerate(log_probs_list):
             log_likelihoods_for_each_hypothesis[indices, prob_idx] = log_prob
-    return log_likelihoods_for_each_hypothesis, all_indices, counter_to_barcode_combo
+    return (
+        log_likelihoods_for_each_hypothesis,
+        all_indices,
+        counter_to_barcode_combo,
+    )
 
 
 def _calculate_bayes_rule(data, priors, number_of_noise_barcodes):
@@ -233,7 +243,8 @@ def _calculate_bayes_rule(data, priors, number_of_noise_barcodes):
         np.exp(log_likelihoods_for_each_hypothesis)
         * priors
         / np.sum(
-            np.multiply(np.exp(log_likelihoods_for_each_hypothesis), priors), axis=1
+            np.multiply(np.exp(log_likelihoods_for_each_hypothesis), priors),
+            axis=1,
         )[:, None]
     )
     most_likely_hypothesis = np.argmax(probs_hypotheses, axis=1)
@@ -249,7 +260,7 @@ def hashsolo(
     cell_hashing_columns: list,
     priors: list = [0.01, 0.8, 0.19],
     pre_existing_clusters: str = None,
-    number_of_noise_barcodes: int = 2,
+    number_of_noise_barcodes: int = None,
     inplace: bool = True,
 ):
     """Probabilistic demultiplexing of cell hashing data using HashSolo [Bernstein20]_.
@@ -302,7 +313,9 @@ def hashsolo(
     data = adata.obs[cell_hashing_columns].values
     if not check_nonnegative_integers(data):
         raise ValueError("Cell hashing counts must be non-negative")
-    if number_of_noise_barcodes >= len(cell_hashing_columns):
+    if (number_of_noise_barcodes is not None) and (
+        number_of_noise_barcodes >= len(cell_hashing_columns)
+    ):
         raise ValueError(
             "number_of_noise_barcodes must be at least one less \
         than the number of samples you have as determined by the number of \
@@ -327,7 +340,9 @@ def hashsolo(
         for cluster_feature in unique_cluster_features:
             cluster_feature_bool_vector = adata.obs[cluster_features] == cluster_feature
             posterior_dict = _calculate_bayes_rule(
-                data[cluster_feature_bool_vector], priors, number_of_noise_barcodes
+                data[cluster_feature_bool_vector],
+                priors,
+                number_of_noise_barcodes,
             )
             results.loc[
                 cluster_feature_bool_vector, "most_likely_hypothesis"

scanpy/tests/external/test_hashsolo.py
@@ -10,7 +10,7 @@ def test_cell_demultiplexing():
     random.seed(52)
     signal = stats.poisson.rvs(1000, 1, 990)
     doublet_signal = stats.poisson.rvs(1000, 1, 10)
-    x = np.reshape(stats.poisson.rvs(5, 1, 10000), (1000, 10))
+    x = np.reshape(stats.poisson.rvs(500, 1, 10000), (1000, 10))
     for idx, signal_count in enumerate(signal):
         col_pos = idx % 10
         x[idx, col_pos] = signal_count