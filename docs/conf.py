c25df19a20aeec5effabf1dc9eae476acaa93d61
Adam Gayoso <adamgayoso@users.noreply.github.com>
2023-01-07 16:29:19 -0800

    Update docs/conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -142,8 +142,8 @@ html_logo = '_static/img/Scanpy_Logo_BrightFG.svg'
 html_title = "scanpy"
 
 
-# def setup(app):
-#     app.warningiserror = on_rtd
+def setup(app):
+    app.warningiserror = on_rtd
 
 
 nbsphinx_prolog = r"""
