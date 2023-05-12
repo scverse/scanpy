36b380678bdbacd7d49d13b9cb695485831686ec
Rahul Shrestha <rahulshrestha0101@gmail.com>
2023-05-12 10:06:18 +0200

    Added preprocessing

---
+++ benchmarks/benchmarks/preprocessing.py
@@ -0,0 +1,80 @@
+"""
+This module will benchmark preprocessing operations in ScanPy
+API documentation: https://scanpy.readthedocs.io/en/stable/api.html#module-scanpy.pp
+
+
+"""
+import tempfile
+from pathlib import Path
+import sys
+
+from memory_profiler import memory_usage
+import numpy as np
+import scanpy as sc
+
+from .utils import get_anndata_memsize, sedate, get_peak_mem, get_actualsize
+
+
+class PreprocessingSuite:
+
+    params =  [sc.datasets.pbmc68k_reduced()]
+    param_names = ['input_data']
+
+    def setup(self, input_data):
+        self.adata = input_data
+
+    def time_calculate_qc_metrics(self, input_data):
+        self.adata.var['mt'] = self.adata.var_names.str.startswith('MT-')
+        sc.pp.calculate_qc_metrics(self.adata, qc_vars=['mt'], percent_top=None, log1p=False, inplace=True)
+
+    def peakmem_calculate_qc_metrics(self, input_data):
+        self.adata.var['mt'] = self.adata.var_names.str.startswith('MT-')
+        sc.pp.calculate_qc_metrics(self.adata, qc_vars=['mt'], percent_top=None, log1p=False, inplace=True)
+    
+    def time_filter_cells(self, input_data):
+        sc.pp.filter_cells(self.adata, min_genes=200)
+
+    def peakmem_filter_cells(self, input_data):
+        sc.pp.filter_cells(self.adata, min_genes=200)
+
+    def time_filter_genes(self, input_data):
+        sc.pp.filter_genes(self.adata, min_cells=3) 
+
+    def peakmem_filter_genes(self, input_data):
+        sc.pp.filter_genes(self.adata, min_cells=3)
+
+    def time_normalize_total(self, input_data):
+        sc.pp.normalize_total(self.adata, target_sum=1e4)
+
+    def peakmem_normalize_total(self, input_data):  
+        sc.pp.normalize_total(self.adata, target_sum=1e4)
+
+    def time_log1p(self, input_data):
+        sc.pp.log1p(self.adata)
+
+    def peakmem_time_log1p(self, input_data):
+        sc.pp.log1p(self.adata)
+
+    def time_pca(self, input_data):
+        sc.pp.pca(self.adata, svd_solver='arpack')
+
+    def peakmem_pca(self, input_data):
+        sc.pp.pca(self.adata, svd_solver='arpack')
+
+    def time_highly_variable_genes(self, input_data):
+        sc.pp.highly_variable_genes(self.adata, min_mean=0.0125, max_mean=3, min_disp=0.5)
+
+    def peakmem_highly_variable_genes(self, input_data):
+        sc.pp.highly_variable_genes(self.adata, min_mean=0.0125, max_mean=3, min_disp=0.5)
+
+    def time_regress_out(self, input_data):
+        sc.pp.regress_out(self.adata, ['n_counts', 'percent_mito'])
+
+    def peakmem_regress_out(self, input_data):
+        sc.pp.regress_out(self.adata, ['n_counts', 'percent_mito'])
+
+    def time_scale(self, input_data):    
+        sc.pp.scale(self.adata, max_value=10)   
+
+    def peakmem_scale(self, input_data):
+        sc.pp.scale(self.adata, max_value=10)
--- benchmarks/benchmarks/readwrite.py
+++ benchmarks/benchmarks/readwrite.py
@@ -1,82 +1,84 @@
-"""
-This module will benchmark io of AnnData objects
+# """
+# This module will benchmark io of ScanPy readwrite operations
 
-Things to test:
+# Things to test:
 
-* Read time, write time
-* Peak memory during io
-* File sizes
+# * Read time, write time
+# * Peak memory during io
+# * File sizes
 
-Parameterized by:
+# Parameterized by:
 
-* What method is being used
-* What data is being included
-* Size of data being used
+# * What method is being used
+# * What data is being included
+# * Size of data being used
 
-Also interesting:
+# Also interesting:
 
-* io for views
-* io for backed objects
-* Reading dense as sparse, writing sparse as dense
-"""
-import tempfile
-from pathlib import Path
-import sys
+# * io for views
+# * io for backed objects
+# * Reading dense as sparse, writing sparse as dense
+# """
+# import tempfile
+# from pathlib import Path
+# import sys
 
-from memory_profiler import memory_usage
-import numpy as np
-import pooch
-import scanpy as sc
+# from memory_profiler import memory_usage
+# import numpy as np
+# import pooch
+# import scanpy as sc
+# import anndata
 
-from .utils import get_anndata_memsize, sedate, get_peak_mem, get_actualsize
-
-
-PBMC_3K_URL = "http://cf.10xgenomics.com/samples/cell-exp/1.1.0/pbmc3k/pbmc3k_filtered_gene_bc_matrices.tar.gz"
+# from .utils import get_anndata_memsize, sedate, get_peak_mem, get_actualsize
 
 
-class H5ADInMemorySizeSuite:
-    params = [PBMC_3K_URL]
-    param_names = ["input_url"]
+# #PBMC_3K_URL = "http://cf.10xgenomics.com/samples/cell-exp/1.1.0/pbmc3k/pbmc3k_filtered_gene_bc_matrices.tar.gz"
+# PBMC_3K_URL = "http://falexwolf.de/data/pbmc3k_raw.h5ad"
 
-    def setup(self, input_url):
-        self.filepath = pooch.retrieve(url=input_url, known_hash=None)
+# class H5ADInMemorySizeSuite:
+#     params = [PBMC_3K_URL]
+#     param_names = ["input_url"]
 
-    def track_in_memory_size(self, input_url):
-        adata = sc.read_10x_mtx(self.filepath)
-        adata_size = sys.getsizeof(adata)
+#     def setup(self, input_url):
+#         #self.filepath = pooch.unzip(pooch.retrieve(url=input_url, known_hash=None))
+#         self.filepath = pooch.retrieve(url=input_url, known_hash=None)
 
-        return adata_size
+#     def track_in_memory_size(self, input_url):
+#         adata = anndata.read_h5ad(self.filepath)
+#         adata_size = sys.getsizeof(adata)
 
-    def track_actual_in_memory_size(self, input_url):
-        adata = sc.read_10x_mtx(self.filepath)
-        adata_size = get_actualsize(adata)
+#         return adata_size
 
-        return adata_size
+#     def track_actual_in_memory_size(self, input_url):
+#         adata = sc.read_10x_mtx(self.filepath)
+#         adata_size = get_actualsize(adata)
+
+#         return adata_size
 
 
-class H5ADReadSuite:
-    # params = [PBMC_REDUCED_PATH, PBMC_3K_PATH, BM_43K_CSR_PATH]
-    params = [PBMC_3K_URL]
-    param_names = ["input_url"]
+# class H5ADReadSuite:
+#     # params = [PBMC_REDUCED_PATH, PBMC_3K_PATH, BM_43K_CSR_PATH]
+#     params = [PBMC_3K_URL]
+#     param_names = ["input_url"]
 
-    def setup(self, input_url):
-        self.filepath = pooch.retrieve(url=input_url, known_hash=None)
+#     def setup(self, input_url):
+#         self.filepath = pooch.retrieve(url=input_url, known_hash=None)
 
-    def time_read_full(self, input_url):
-        sc.read_10x_mtx(self.filepath)
+#     def time_read_full(self, input_url):
+#         sc.read_10x_mtx(self.filepath)
 
-    def peakmem_read_full(self, input_url):
-        sc.read_10x_mtx(self.filepath)
+#     def peakmem_read_full(self, input_url):
+#         sc.read_10x_mtx(self.filepath)
 
-    def mem_readfull_object(self, input_url):
-        return sc.read_10x_mtx(self.filepath)
+#     def mem_readfull_object(self, input_url):
+#         return sc.read_10x_mtx(self.filepath)
 
-    def track_read_full_memratio(self, input_url):
-        mem_recording = memory_usage(
-            (sedate(sc.read_10x_mtx, 0.005), (self.filepath,)), interval=0.001
-        )
+    # def track_read_full_memratio(self, input_url):
+    #     mem_recording = memory_usage(
+    #         (sedate(sc.read_10x_mtx, 0.005), (self.filepath,)), interval=0.001
+    #     )
 
-        base_size = mem_recording[-1] - mem_recording[0]
-        print(np.max(mem_recording) - np.min(mem_recording))
-        print(base_size)
-        return (np.max(mem_recording) - np.min(mem_recording)) / base_size
+    #     base_size = mem_recording[-1] - mem_recording[0]
+    #     print(np.max(mem_recording) - np.min(mem_recording))
+    #     print(base_size)
+    #     return (np.max(mem_recording) - np.min(mem_recording)) / base_size
