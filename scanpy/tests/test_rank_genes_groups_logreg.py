33464f3756ceae126fe936e105af66a3bf891d00
Philipp A <flying-sheep@web.de>
2023-08-07 17:36:29 +0200

    quotes

--- scanpy/tests/test_rank_genes_groups_logreg.py
+++ scanpy/tests/test_rank_genes_groups_logreg.py
@@ -5,10 +5,7 @@ import scanpy as sc
 import pandas as pd
 
 
-@pytest.mark.parametrize(
-    "method",
-    ["t-test", "logreg"],
-)
+@pytest.mark.parametrize('method', ['t-test', 'logreg'])
 def test_rank_genes_groups_with_renamed_categories(method):
     adata = sc.datasets.blobs(n_variables=4, n_centers=3, n_observations=200)
     assert np.allclose(adata.X[1], [9.214668, -2.6487126, 4.2020774, 0.51076424])
@@ -31,34 +28,34 @@ def test_rank_genes_groups_with_renamed_categories_use_rep():
     adata = sc.datasets.blobs(n_variables=4, n_centers=3, n_observations=200)
     assert np.allclose(adata.X[1], [9.214668, -2.6487126, 4.2020774, 0.51076424])
 
-    adata.layers["to_test"] = adata.X.copy()
+    adata.layers['to_test'] = adata.X.copy()
     adata.X = adata.X[::-1, :]
 
     sc.tl.rank_genes_groups(
-        adata, 'blobs', method='logreg', layer="to_test", use_raw=False
+        adata, 'blobs', method='logreg', layer='to_test', use_raw=False
     )
     assert adata.uns['rank_genes_groups']['names'].dtype.names == ('0', '1', '2')
     assert adata.uns['rank_genes_groups']['names'][0].tolist() == ('1', '3', '0')
 
-    sc.tl.rank_genes_groups(adata, 'blobs', method="logreg")
+    sc.tl.rank_genes_groups(adata, 'blobs', method='logreg')
     assert not adata.uns['rank_genes_groups']['names'][0].tolist() == ('3', '1', '0')
 
 
 def test_rank_genes_groups_with_unsorted_groups():
     adata = sc.datasets.blobs(n_variables=10, n_centers=5, n_observations=200)
     adata._sanitize()
-    adata.rename_categories('blobs', ['Zero', 'One', 'Two', "Three", "Four"])
+    adata.rename_categories('blobs', ['Zero', 'One', 'Two', 'Three', 'Four'])
     bdata = adata.copy()
     sc.tl.rank_genes_groups(
-        adata, 'blobs', groups=['Zero', 'One', 'Three'], method="logreg"
+        adata, 'blobs', groups=['Zero', 'One', 'Three'], method='logreg'
     )
     sc.tl.rank_genes_groups(
-        bdata, 'blobs', groups=['One', 'Three', 'Zero'], method="logreg"
+        bdata, 'blobs', groups=['One', 'Three', 'Zero'], method='logreg'
     )
     array_ad = pd.DataFrame(
-        adata.uns["rank_genes_groups"]["scores"]['Three']
+        adata.uns['rank_genes_groups']['scores']['Three']
     ).to_numpy()
     array_bd = pd.DataFrame(
-        bdata.uns["rank_genes_groups"]["scores"]['Three']
+        bdata.uns['rank_genes_groups']['scores']['Three']
     ).to_numpy()
     np.testing.assert_equal(array_ad, array_bd)
