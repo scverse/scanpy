a2c08ab1878b961eba8010748e7d5bc552fbf6bc
ilan-gold <ilanbassgold@gmail.com>
2023-08-14 17:51:17 +0200

    (style): refactor to use fixtures for backing data

--- scanpy/tests/test_aggregated.py
+++ scanpy/tests/test_aggregated.py
@@ -6,21 +6,14 @@ from scipy.sparse import csr_matrix
 import pytest
 
 
-@pytest.mark.parametrize(
-    'data_key',
-    ['layers', 'obsm', 'varm'],
-)
-@pytest.mark.parametrize(
-    'dim',
-    [
-        'obs',
-        'var',
-    ],
-)
-def test_groupby_different_data_locations(data_key, dim):
-    if (data_key == 'varm' and dim == 'obs') or (data_key == 'obsm' and dim == 'var'):
-        pytest.skip("invalid parameter combination")
+@pytest.fixture
+def df_base():
     ax_base = ["A", "B"]
+    return pd.DataFrame(index=ax_base)
+
+
+@pytest.fixture
+def df_groupby():
     ax_groupby = [
         *["v0", "v1", "v2"],
         *["w0", "w1"],
@@ -37,10 +30,12 @@ def test_groupby_different_data_locations(data_key, dim):
     )
     df_groupby["key_subset"] = pd.Categorical([c[1] for c in ax_groupby])
     df_groupby["weight"] = 2.0
+    return df_groupby
 
-    df_base = pd.DataFrame(index=ax_base)
 
-    X = np.array(
+@pytest.fixture
+def X():
+    return np.array(
         [
             *[[0, -2], [1, 13], [2, 1]],  # v
             *[[3, 12], [4, 2]],  # w
@@ -51,7 +46,22 @@ def test_groupby_different_data_locations(data_key, dim):
         ],
         dtype=np.float32,
     )
-    data_dense = X
+
+
+@pytest.mark.parametrize(
+    'data_key',
+    ['layers', 'obsm', 'varm'],
+)
+@pytest.mark.parametrize(
+    'dim',
+    [
+        'obs',
+        'var',
+    ],
+)
+def test_groupby_different_data_locations(data_key, dim, df_base, df_groupby, X):
+    if (data_key == 'varm' and dim == 'obs') or (data_key == 'obsm' and dim == 'var'):
+        pytest.skip("invalid parameter combination")
     if dim == 'obs':
         data_sparse_mat_dict = {data_key: {'test': csr_matrix(X)}}
         adata_sparse = ad.AnnData(
@@ -63,12 +73,12 @@ def test_groupby_different_data_locations(data_key, dim):
         )
     else:
         if data_key != 'varm':
-            data_dense = X.T
-        data_sparse_mat_dict = {data_key: {'test': csr_matrix(data_dense)}}
+            X = X.T
+        data_sparse_mat_dict = {data_key: {'test': csr_matrix(X)}}
         adata_sparse = ad.AnnData(
             **{'obs': df_base, 'var': df_groupby, **data_sparse_mat_dict}
         )
-        data_dense_mat_dict = {data_key: {'test': data_dense}}
+        data_dense_mat_dict = {data_key: {'test': X}}
         adata_dense = ad.AnnData(
             **{'obs': df_base, 'var': df_groupby, **data_dense_mat_dict}
         )
@@ -162,7 +172,7 @@ def test_groupby_different_data_locations(data_key, dim):
     df = pd.DataFrame(
         index=getattr(adata_dense, dim)["key"],
         columns=getattr(adata_dense, f"{'var' if dim == 'obs' else 'obs'}_names"),
-        data=data_dense.T if dim == 'var' and data_key != 'varm' else data_dense,
+        data=X.T if dim == 'var' and data_key != 'varm' else X,
     )
     grouped_agg_df = (
         df.groupby('key')
@@ -188,44 +198,12 @@ def test_groupby_different_data_locations(data_key, dim):
     'dim',
     ['obs', 'var'],
 )
-def test_groupby_X(dim):
-    ax_base = ["A", "B"]
-    ax_groupby = [
-        *["v0", "v1", "v2"],
-        *["w0", "w1"],
-        *["a1", "a2", "a3"],
-        *["b1", "b2"],
-        *["c1", "c2"],
-        "d0",
-    ]
-
-    df_groupby = pd.DataFrame(index=pd.Index(ax_groupby, name="cell"))
-    df_groupby["key"] = pd.Categorical([c[0] for c in ax_groupby])
-    df_groupby["key_superset"] = pd.Categorical([c[0] for c in ax_groupby]).map(
-        {'v': 'v', 'w': 'v', 'a': 'a', 'b': 'a', 'c': 'a', 'd': 'a'}
-    )
-    df_groupby["key_subset"] = pd.Categorical([c[1] for c in ax_groupby])
-    df_groupby["weight"] = 2.0
-
-    df_base = pd.DataFrame(index=ax_base)
-
-    X = np.array(
-        [
-            *[[0, -2], [1, 13], [2, 1]],  # v
-            *[[3, 12], [4, 2]],  # w
-            *[[5, 11], [6, 3], [7, 10]],  # a
-            *[[8, 4], [9, 9]],  # b
-            *[[10, 5], [11, 8]],  # c
-            [12, 6],  # d
-        ],
-        dtype=np.float32,
-    )
-    data_dense = X
+def test_groupby_X(dim, df_base, df_groupby, X):
     if dim == 'obs':
         adata_sparse = ad.AnnData(obs=df_groupby, var=df_base, X=csr_matrix(X))
         adata_dense = ad.AnnData(obs=df_groupby, var=df_base, X=X)
     else:
-        adata_sparse = ad.AnnData(obs=df_base, var=df_groupby, X=data_dense.T)
+        adata_sparse = ad.AnnData(obs=df_base, var=df_groupby, X=X.T)
         adata_dense = ad.AnnData(obs=df_base, var=df_groupby, X=csr_matrix(X).T)
 
     stats_sparse = sc.get.aggregated(
@@ -295,7 +273,7 @@ def test_groupby_X(dim):
     df = pd.DataFrame(
         index=getattr(adata_dense, dim)["key"],
         columns=getattr(adata_dense, f"{'var' if dim == 'obs' else 'obs'}_names"),
-        data=data_dense,
+        data=X,
     )
     grouped_agg_df = (
         df.groupby('key')
