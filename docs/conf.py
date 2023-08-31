e643925038abc81258680cfb7a77819b0311e935
Phil Schaf <flying-sheep@web.de>
2023-08-31 10:17:46 +0200

    fix docs

--- docs/conf.py
+++ docs/conf.py
@@ -197,6 +197,7 @@ nitpick_ignore = [
     ('py:class', 'scanpy.plotting._utils._AxesSubplot'),
     ('py:class', 'scanpy._utils.Empty'),
     ('py:class', 'numpy.random.mtrand.RandomState'),
+    ('py:class', 'scanpy.neighbors._types.KnnTransformerLike'),
     # Will work once scipy 1.8 is released
     ('py:class', 'scipy.sparse.base.spmatrix'),
     ('py:class', 'scipy.sparse.csr.csr_matrix'),
