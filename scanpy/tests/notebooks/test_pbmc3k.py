c87f1d689f2c04973bd65c9d81b9842ca1376717
Philipp A <flying-sheep@web.de>
2023-09-04 16:21:54 +0200

    Enforce stable cluster order in notebook test (#2650)

--- scanpy/tests/notebooks/test_pbmc3k.py
+++ scanpy/tests/notebooks/test_pbmc3k.py
@@ -129,6 +129,11 @@ def test_pbmc3k(image_comparer):
     # sc.pl.rank_genes_groups_violin(adata, groups='0', n_genes=8)
     # save_and_compare_images('rank_genes_groups_4')
 
+    if adata[adata.obs['leiden'] == '4', 'CST3'].X.mean() < 1:
+        (  # switch clusters
+            adata.obs['leiden'][adata.obs['leiden'] == '4'],
+            adata.obs['leiden'][adata.obs['leiden'] == '5'],
+        ) = ('5', '4')
     new_cluster_names = [
         'CD4 T cells',
         'CD14+ Monocytes',
