06df09371a55db53aea05f67dd72a95fbb022662
Phil Schaf <flying-sheep@web.de>
2023-09-04 10:46:02 +0200

    simplify types

--- scanpy/_utils/_dask.py
+++ scanpy/_utils/_dask.py
@@ -1,4 +1,5 @@
 from __future__ import annotations
+from functools import partial
 
 from typing import TYPE_CHECKING
 
@@ -11,24 +12,16 @@ if TYPE_CHECKING:
 
     _EagerBool: TypeAlias = Union[bool, np.bool_]
     _BoolScalar: TypeAlias = Union[_EagerBool, DaskArray]
-    _Left: TypeAlias = Union[_BoolScalar, Callable[[], _BoolScalar]]
-    _Right: TypeAlias = Union[_BoolScalar, Callable[[], _EagerBool]]
 
 
-def _call_or_return(maybe_cb: Any, validate: bool = False):
-    if not callable(maybe_cb):
-        return maybe_cb
-    rv = maybe_cb()
-    if validate and isinstance(rv, DaskArray):
-        msg = (
-            "Use lazy_*(_, da.array(...)) instead of lazy_*(_, lambda: da.array(...))."
-        )
-        raise AssertionError(msg)
-    return rv
+def _lazy_bool_op(
+    left: _BoolScalar, right: Callable[[], _BoolScalar], /, *, cmp
+) -> _BoolScalar:
+    if callable(left):
+        raise TypeError('only right operand should be callable')
+    if not callable(right):
+        raise TypeError('right argument must be callable')
 
-
-def _lazy_bool_op(left: _Left, right: _Right, /, *, cmp) -> _BoolScalar:
-    left = _call_or_return(left)
     if not isinstance(left, DaskArray):
         return cmp(left, right)
 
@@ -39,18 +32,8 @@ def _lazy_bool_op(left: _Left, right: _Right, /, *, cmp) -> _BoolScalar:
     return left.map_blocks(chain, meta=np.bool_(True))
 
 
-def lazy_and(left: _Left, right: _Right, /) -> _BoolScalar:
-    def cmp(l, r):
-        return l and _call_or_return(r, validate=True)
-
-    return _lazy_bool_op(left, right, cmp=cmp)
-
-
-def lazy_or(left: _Left, right: _Right, /) -> _BoolScalar:
-    def cmp(l, r):
-        return l or _call_or_return(r, validate=True)
-
-    return _lazy_bool_op(left, right, cmp=cmp)
+lazy_and = partial(_lazy_bool_op, cmp=lambda l, r: l and r())
+lazy_or = partial(_lazy_bool_op, cmp=lambda l, r: l or r())
 
 
 def get_ufuncs(data: np.ndarray | DaskArray):
