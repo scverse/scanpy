f2e296c7a39f75aec4746ed0747f774f0bc06068
Phil Schaf <flying-sheep@web.de>
2023-09-07 14:43:02 +0200

    some fixes

--- scanpy/neighbors/__init__.py
+++ scanpy/neighbors/__init__.py
@@ -169,7 +169,7 @@ def neighbors(
     >>> sc.pp.neighbors(adata, 20, metric='cosine')
     >>> # Provide your own transformer for more control and flexibility
     >>> from sklearn.neighbors import KNeighborsTransformer
-    >>> transformer = KNeighborsTransformer(n_neighbors=10, metric='cosine', algorithm='ball_tree')
+    >>> transformer = KNeighborsTransformer(n_neighbors=10, metric='manhattan', algorithm='kd_tree')
     >>> sc.pp.neighbors(adata, transformer=transformer)
     >>> # now you can e.g. access the index: `transformer._tree`
     """
