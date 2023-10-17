c7255f3a6ee8b054c4a3704457f1a7a15b5f2731
Philipp A. <flying-sheep@web.de>
2023-10-17 10:42:05 +0200

    Switch to hatch and Python 3.9 minimum

--- scanpy/__init__.py
+++ scanpy/__init__.py
@@ -1,42 +1,52 @@
 """Single-Cell Analysis in Python."""
 
-from ._metadata import __version__, within_flit
+try:  # See https://github.com/maresb/hatch-vcs-footgun-example
+    from setuptools_scm import get_version
 
-if not within_flit():  # see function docstring on why this is there
-    from ._utils import check_versions
+    __version__ = get_version(root="..", relative_to=__file__)
+    del get_version
+except (ImportError, LookupError):
+    try:
+        from ._version import __version__
+    except ModuleNotFoundError:
+        raise RuntimeError(
+            "anndata is not correctly installed. Please install it, e.g. with pip."
+        )
 
-    check_versions()
-    del check_versions, within_flit
+from ._utils import check_versions
 
-    # the actual API
-    # (start with settings as several tools are using it)
-    from ._settings import settings, Verbosity
-    from . import tools as tl
-    from . import preprocessing as pp
-    from . import plotting as pl
-    from . import datasets, logging, queries, external, get, metrics, experimental
+check_versions()
+del check_versions
 
-    from anndata import AnnData, concat
-    from anndata import (
-        read_h5ad,
-        read_csv,
-        read_excel,
-        read_hdf,
-        read_loom,
-        read_mtx,
-        read_text,
-        read_umi_tools,
-    )
-    from .readwrite import read, read_10x_h5, read_10x_mtx, write, read_visium
-    from .neighbors import Neighbors
+# the actual API
+# (start with settings as several tools are using it)
+from ._settings import settings, Verbosity
+from . import tools as tl
+from . import preprocessing as pp
+from . import plotting as pl
+from . import datasets, logging, queries, external, get, metrics, experimental
 
-    set_figure_params = settings.set_figure_params
+from anndata import AnnData, concat
+from anndata import (
+    read_h5ad,
+    read_csv,
+    read_excel,
+    read_hdf,
+    read_loom,
+    read_mtx,
+    read_text,
+    read_umi_tools,
+)
+from .readwrite import read, read_10x_h5, read_10x_mtx, write, read_visium
+from .neighbors import Neighbors
 
-    # has to be done at the end, after everything has been imported
-    import sys
+set_figure_params = settings.set_figure_params
 
-    sys.modules.update({f'{__name__}.{m}': globals()[m] for m in ['tl', 'pp', 'pl']})
-    from ._utils import annotate_doc_types
+# has to be done at the end, after everything has been imported
+import sys
 
-    annotate_doc_types(sys.modules[__name__], 'scanpy')
-    del sys, annotate_doc_types
+sys.modules.update({f'{__name__}.{m}': globals()[m] for m in ['tl', 'pp', 'pl']})
+from ._utils import annotate_doc_types
+
+annotate_doc_types(sys.modules[__name__], 'scanpy')
+del sys, annotate_doc_types
