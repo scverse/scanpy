121def059c2c1046626c65042871962c60747745
Philipp A <flying-sheep@web.de>
2023-10-17 10:12:33 +0200

    Backport PR #2678: Fix pandas function deprecation warnings (#2692)

--- scanpy/tests/test_embedding_plots.py
+++ scanpy/tests/test_embedding_plots.py
@@ -48,7 +48,7 @@ def adata():
     order = np.argsort(labels)
     adata = sc.AnnData(
         make_blobs(
-            pd.value_counts(labels[order]).values,
+            pd.Series(labels[order]).value_counts().values,
             n_features=20,
             shuffle=False,
             random_state=42,
