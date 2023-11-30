a0670c7d3ba4db77d4016365484b79b9a6a2d522
Geert-Jan Huizing <huizing@ens.fr>
2023-11-30 15:55:22 +0100

    fix output shape

--- scanpy/preprocessing/_simple.py
+++ scanpy/preprocessing/_simple.py
@@ -150,12 +150,14 @@ def filter_cells(
     max_number = max_counts if max_genes is None else max_genes
     if min_genes is None and max_genes is None:
         number_per_cell = np.sum(X, axis=1)
+        if issparse(X):
+            number_per_cell = number_per_cell.A1
     elif isspmatrix_csr(X):
         number_per_cell = np.diff(X.indptr)
     else:
         number_per_cell = np.sum(X > 0, axis=1)
-    if issparse(X) and not isspmatrix_csr(X):
-        number_per_cell = number_per_cell.A1
+        if issparse(X):
+            number_per_cell = number_per_cell.A1
     if min_number is not None:
         cell_subset = number_per_cell >= min_number
     if max_number is not None:
@@ -265,12 +267,14 @@ def filter_genes(
     max_number = max_counts if max_cells is None else max_cells
     if min_cells is None and max_cells is None:
         number_per_gene = np.sum(X, axis=0)
+        if issparse(X):
+            number_per_gene = number_per_gene.A1
     elif isspmatrix_csr(X):
         number_per_gene = np.bincount(X.indices, minlength=X.shape[1])
     else:
         number_per_gene = np.sum(X > 0, axis=0)
-    if issparse(X) and not isspmatrix_csr(X):
-        number_per_gene = number_per_gene.A1
+        if issparse(X):
+            number_per_gene = number_per_gene.A1
     if min_number is not None:
         gene_subset = number_per_gene >= min_number
     if max_number is not None:
