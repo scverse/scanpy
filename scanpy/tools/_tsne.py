573a78fbe58e3d309bac1c396214fffd449b289a
Chiru <48340051+ch1ru@users.noreply.github.com>
2024-01-11 01:04:52 +0000

    Update _tsne.py

--- scanpy/tools/_tsne.py
+++ scanpy/tools/_tsne.py
@@ -24,7 +24,7 @@ if TYPE_CHECKING:
     "random_state",
     "use_fast_tsne",
     "n_jobs",
-    "copy",
+    "copy"
 )
 @_doc_params(doc_n_pcs=doc_n_pcs, use_rep=doc_use_rep)
 def tsne(
@@ -178,6 +178,7 @@ def tsne(
                 "n_jobs": n_jobs,
                 "metric": metric,
                 "use_rep": use_rep,
+                "n_components": n_components,
             }.items()
             if v is not None
         }
