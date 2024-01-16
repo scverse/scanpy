99d3acddbd9b7cf2cd4d1b66af93dabefd14bb23
Philipp A. <flying-sheep@web.de>
2024-01-16 10:02:21 +0100

    fix syntax

--- scanpy/readwrite.py
+++ scanpy/readwrite.py
@@ -1,5 +1,7 @@
 """Reading and Writing
 """
+from __future__ import annotations
+
 import json
 from pathlib import Path, PurePath
 from typing import BinaryIO, Dict, Literal, Optional, Tuple, Union
