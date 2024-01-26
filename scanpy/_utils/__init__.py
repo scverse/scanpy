5c49c56965d188fd9769620d7796f4223aab81d5
Ilan Gold <ilanbassgold@gmail.com>
2024-01-26 13:20:21 +0100

    Update scanpy/_utils/__init__.py
    Co-authored-by: Philipp A. <flying-sheep@web.de>

--- scanpy/_utils/__init__.py
+++ scanpy/_utils/__init__.py
@@ -25,7 +25,7 @@ from anndata import __version__ as anndata_version
 from numpy.typing import NDArray
 from packaging import version
 from scipy import sparse
-from sklearn.dummy import check_random_state
+from sklearn.utils import check_random_state
 
 from .. import logging as logg
 from .._compat import DaskArray
