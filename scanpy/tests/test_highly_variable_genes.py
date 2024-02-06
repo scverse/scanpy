222f340e3f498e4ee456219b24208160018ac637
Philipp A. <flying-sheep@web.de>
2024-02-06 17:58:09 +0100

    fmt

--- scanpy/tests/test_highly_variable_genes.py
+++ scanpy/tests/test_highly_variable_genes.py
@@ -566,8 +566,6 @@ def test_cellranger_n_top_genes_warning():
         sc.pp.highly_variable_genes(adata, n_top_genes=1000, flavor="cell_ranger")
 
 
-
-
 @pytest.mark.parametrize("flavor", ["seurat", "cell_ranger"])
 @pytest.mark.parametrize("array_type", ARRAY_TYPES_SUPPORTED)
 @pytest.mark.parametrize("subset", [True, False], ids=["subset", "full"])
