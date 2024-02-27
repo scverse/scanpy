8dd9a7ab7037fed58d7ae096833fbf0908a2a831
ilan-gold <ilanbassgold@gmail.com>
2024-02-27 13:40:29 +0100

    (refactor): `materialize_as_ndarray` needs to operate on indidiual dask arrays

--- scanpy/preprocessing/_distributed.py
+++ scanpy/preprocessing/_distributed.py
@@ -35,15 +35,12 @@ def materialize_as_ndarray(
 
 
 def materialize_as_ndarray(
-    a: ArrayLike | tuple[ArrayLike | ZappyArray | DaskArray, ...],
+    a: DaskArray | ArrayLike | tuple[ArrayLike | ZappyArray | DaskArray, ...],
 ) -> tuple[np.ndarray] | np.ndarray:
     """Compute distributed arrays and convert them to numpy ndarrays."""
+    if isinstance(a, DaskArray):
+        return a.compute()
     if not isinstance(a, tuple):
         return np.asarray(a)
 
-    if not any(isinstance(arr, DaskArray) for arr in a):
-        return tuple(np.asarray(arr) for arr in a)
-
-    import dask.array as da
-
-    return da.compute(*a, sync=True)
+    return tuple(materialize_as_ndarray(arr) for arr in a)
