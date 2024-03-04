35385725a6e33f2af8789b76e147c8dfa96657ef
ilan-gold <ilanbassgold@gmail.com>
2024-03-04 10:14:15 +0100

    (chore): remove non-public API

--- scanpy/_utils/__init__.py
+++ scanpy/_utils/__init__.py
@@ -602,7 +602,7 @@ def axis_scale(
             new_data = X.data * np.repeat(scaling_array, np.diff(X.indptr))
         elif column_scale:
             new_data = X.data * scaling_array.take(X.indices, mode="clip")
-        return X._with_data(new_data)
+        return sparse.csr_matrix((new_data, X.inidices, X.indptr), shape=X.shape)
     return axis_scale(X.T, scaling_array, (axis + 1) % 2).T
 
 
