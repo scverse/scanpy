6897a2a42847fd13179fd487d002209a59d5053d
Philipp A <flying-sheep@web.de>
2024-05-17 11:44:20 +0200

    Backport PR #3065: Upper-bound numpy below 2 for now

--- scanpy/plotting/_tools/__init__.py
+++ scanpy/plotting/_tools/__init__.py
@@ -407,8 +407,8 @@ def rank_genes_groups(
     gs = gridspec.GridSpec(nrows=n_panels_y, ncols=n_panels_x, wspace=0.22, hspace=0.3)
 
     ax0 = None
-    ymin = np.Inf
-    ymax = -np.Inf
+    ymin = np.inf
+    ymax = -np.inf
     for count, group_name in enumerate(group_names):
         gene_names = adata.uns[key]["names"][group_name][:n_genes]
         scores = adata.uns[key]["scores"][group_name][:n_genes]
