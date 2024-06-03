876bf5dd862022fc6ab30cc35d6b840a82536cb0
Philipp A. <flying-sheep@web.de>
2024-06-03 12:29:48 +0200

    dedupe

--- scanpy/plotting/_tools/__init__.py
+++ scanpy/plotting/_tools/__init__.py
@@ -19,10 +19,12 @@ from ..._utils import _doc_params, sanitize_anndata, subsample
 from ...get import rank_genes_groups_df
 from .._anndata import ranking
 from .._docs import (
+    doc_cm_palette,
     doc_panels,
     doc_rank_genes_groups_plot_args,
     doc_rank_genes_groups_values_to_plot,
     doc_scatter_embedding,
+    doc_show_save,
     doc_show_save_ax,
     doc_vbound_percentile,
 )
@@ -265,6 +267,7 @@ def dpt_timeseries(
 
 
 @old_positionals("color_map", "palette", "show", "save", "marker")
+@_doc_params(cm_palette=doc_cm_palette, show_save=doc_show_save)
 def dpt_groups_pseudotime(
     adata: AnnData,
     *,
@@ -280,29 +283,10 @@ def dpt_groups_pseudotime(
     ----------
     adata
         Annotated data matrix.
-    color_map
-        Color map to use for continous variables. Can be a name or a
-        :class:`~matplotlib.colors.Colormap` instance (e.g. `"magma`", `"viridis"`
-        or `mpl.cm.cividis`), see :func:`~matplotlib.cm.get_cmap`.
-        If `None`, the value of `mpl.rcParams["image.cmap"]` is used.
-        The default `color_map` can be set using :func:`~scanpy.set_figure_params`."
-    palette
-        Colors to use for plotting categorical annotation groups.
-        The palette can be a valid :class:`~matplotlib.colors.ListedColormap` name
-        (`'Set2'`, `'tab20'`, …), a :class:`~cycler.Cycler` object, a dict mapping
-        categories to colors, or a sequence of colors. Colors must be valid to
-        matplotlib. (see :func:`~matplotlib.colors.is_color_like`).
-        If `None`, `mpl.rcParams["axes.prop_cycle"]` is used unless the categorical
-        variable already has colors stored in `adata.uns["{var}_colors"]`.
-        If provided, values of `adata.uns["{var}_colors"]` will be set.
-    show
-        Show the plot, do not return axis.
-    save
-        If `True` or a `str`, save the figure.
-        A string is appended to the default filename.
-        Infer the filetype if ending on {`'.pdf'`, `'.png'`, `'.svg'`}.
+    {cm_palette}
+    {show_save}
     marker
-        (uknown)
+        Marker style. See :mod:`~matplotlib.markers` for details.
     """
     _, (ax_grp, ax_ord) = plt.subplots(2, 1)
     timeseries_subplot(
