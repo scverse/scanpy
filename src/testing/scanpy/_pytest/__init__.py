316869258dfa793142fc0c818df9148afaab909f
Philipp A <flying-sheep@web.de>
2024-06-04 15:36:11 +0200

    Backport PR #3060: Document datasets

--- src/testing/scanpy/_pytest/__init__.py
+++ src/testing/scanpy/_pytest/__init__.py
@@ -17,7 +17,10 @@ if TYPE_CHECKING:
 
 # Defining it here because it’s autouse.
 @pytest.fixture(autouse=True)
-def _global_test_context(request: pytest.FixtureRequest) -> Generator[None, None, None]:
+def _global_test_context(
+    request: pytest.FixtureRequest,
+    tmp_path_factory: pytest.TempPathFactory,
+) -> Generator[None, None, None]:
     """Switch to agg backend, reset settings, and close all figures at teardown."""
     # make sure seaborn is imported and did its thing
     import seaborn as sns  # noqa: F401
@@ -30,6 +33,8 @@ def _global_test_context(request: pytest.FixtureRequest) -> Generator[None, None
     sc.settings.logfile = sys.stderr
     sc.settings.verbosity = "hint"
     sc.settings.autoshow = True
+    sc.settings.datasetdir = tmp_path_factory.mktemp("scanpy_data")
+    sc.settings.writedir = tmp_path_factory.mktemp("scanpy_write")
 
     if isinstance(request.node, pytest.DoctestItem):
         _modify_doctests(request)
@@ -101,6 +106,10 @@ def _modify_doctests(request: pytest.FixtureRequest) -> None:
     skip_reason: str | None
     if skip_reason := getattr(func, "_doctest_skip_reason", None):
         pytest.skip(reason=skip_reason)
+    if getattr(func, "_doctest_internet", False) and not request.config.getoption(
+        "--internet-tests"
+    ):
+        pytest.skip(reason="need --internet-tests option to run")
 
 
 def pytest_itemcollected(item: pytest.Item) -> None:
