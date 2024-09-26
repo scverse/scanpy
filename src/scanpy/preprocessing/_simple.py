fb2d8d5668570684da67feacd976ba14e1f1a675
Philipp A. <flying-sheep@web.de>
2024-09-26 16:13:50 +0200

    Fix compat typing and old_positionals usage (#3264)

--- src/scanpy/preprocessing/_simple.py
+++ src/scanpy/preprocessing/_simple.py
@@ -13,7 +13,6 @@ import numba
 import numpy as np
 import scipy as sp
 from anndata import AnnData
-from legacy_api_wrap import legacy_api
 from pandas.api.types import CategoricalDtype
 from scipy.sparse import csr_matrix, issparse, isspmatrix_csr, spmatrix
 from sklearn.utils import check_array, sparsefuncs
@@ -544,7 +543,7 @@ def sqrt(
         return X.sqrt()
 
 
-@legacy_api(
+@old_positionals(
     "counts_per_cell_after",
     "counts_per_cell",
     "key_n_counts",
@@ -647,7 +646,11 @@ def normalize_per_cell(
             adata.obs[key_n_counts] = counts_per_cell
             adata._inplace_subset_obs(cell_subset)
             counts_per_cell = counts_per_cell[cell_subset]
-        normalize_per_cell(adata.X, counts_per_cell_after, counts_per_cell)
+        normalize_per_cell(
+            adata.X,
+            counts_per_cell_after=counts_per_cell_after,
+            counts_per_cell=counts_per_cell,
+        )
 
         layers = adata.layers.keys() if layers == "all" else layers
         if use_rep == "after":
