5d9505da2f9e379f0889a30a2f913dae5d931be9
Ryan Williams <nobigdealstyle@gmail.com>
2024-10-23 14:24:36 -0400

    Fix some `Returns` docstrs re: `inplace` semantics (#3311)
    * fix comment typo
    
    * fix docstrs re: `Returns`/`inplace` semantics

--- src/scanpy/preprocessing/_combat.py
+++ src/scanpy/preprocessing/_combat.py
@@ -171,7 +171,7 @@ def combat(
 
     Returns
     -------
-    Returns :class:`numpy.ndarray` if `inplace=True`, else returns `None` and sets the following field in the `adata` object:
+    Returns :class:`numpy.ndarray` if `inplace=False`, else returns `None` and sets the following field in the `adata` object:
 
     `adata.X` : :class:`numpy.ndarray` (dtype `float`)
         Corrected data matrix.
@@ -261,7 +261,7 @@ def combat(
     # we now apply the parametric adjustment to the standardized data from above
     # loop over all batches in the data
     for j, batch_idxs in enumerate(batch_info):
-        # we basically substract the additive batch effect, rescale by the ratio
+        # we basically subtract the additive batch effect, rescale by the ratio
         # of multiplicative batch effect to pooled variance and add the overall gene
         # wise mean
         dsq = np.sqrt(delta_star[j, :])
