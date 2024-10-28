9cd667d7a99c9d4554b88edc258127bc043ccc83
Intron7 <severin.dicks@icloud.com>
2024-10-28 15:22:21 +0100

    add note

--- src/scanpy/preprocessing/_simple.py
+++ src/scanpy/preprocessing/_simple.py
@@ -724,6 +724,7 @@ def regress_out(
         logg.debug("... regressing on per-gene means within categories")
         regressors = np.zeros(X.shape, dtype="float32")
         X = _to_dense(X, order="F") if issparse(X) else X
+        # TODO figure out if we should use a numba kernel for this
         for category in adata.obs[keys[0]].cat.categories:
             mask = (category == adata.obs[keys[0]]).values
             for ix, x in enumerate(X.T):
