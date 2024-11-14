1696b4ef77003e117ecd4578b214b8b7ecfcac07
Philipp A. <flying-sheep@web.de>
2024-11-14 10:24:43 +0100

    Fix towncrier PR automation

--- ci/scripts/towncrier_automation.py
+++ ci/scripts/towncrier_automation.py
@@ -62,9 +62,7 @@ def main(argv: Sequence[str] | None = None) -> None:
         text=True,
         check=True,
     ).stdout.strip()
-    pr_description = (
-        "" if base_branch == "main" else "@meeseeksmachine backport to main"
-    )
+    pr_description = "" if base_branch == "main" else "@meeseeksdev backport to main"
     branch_name = f"release_notes_{args.version}"
 
     # Create a new branch + commit
