5335af386ba425ffcff98e3d8a41059d11503e0e
Philipp A. <flying-sheep@web.de>
2025-03-18 14:40:36 +0100

    fix rest

--- tests/test_preprocessing_distributed.py
+++ tests/test_preprocessing_distributed.py
@@ -151,7 +151,7 @@ def test_write_zarr(adata: AnnData, adata_dist: AnnData):
         chunks = (chunks[0][0],) + chunks[1]
 
     # write metadata using regular anndata
-    adata.write_zarr(temp_store, chunks)
+    adata.write_zarr(temp_store, chunks=chunks)
     if adata_dist.uns["dist-mode"] == "dask":
         adata_dist.X.to_zarr(temp_store.dir_path("X"), overwrite=True)
     elif adata_dist.uns["dist-mode"] == "direct":
