54b6c37e1682a8e420ee64e98d012d8a898d689d
ilan-gold <ilanbassgold@gmail.com>
2025-05-20 12:18:57 +0200

    (fix): sparse bug + tests

--- tests/test_preprocessing.py
+++ tests/test_preprocessing.py
@@ -407,13 +407,20 @@ def test_regress_out_ordinal():
     np.testing.assert_array_equal(single.X, multi.X)
 
 
-def test_regress_out_layer():
+@pytest.mark.parametrize("dtype", [np.int64, np.float64, np.int32])
+def test_regress_out_layer(dtype):
     from scipy.sparse import random
 
-    adata = AnnData(random(1000, 100, density=0.6, format="csr"))
+    adata = AnnData(random(1000, 100, density=0.6, format="csr", dtype=dtype))
     adata.obs["percent_mito"] = np.random.rand(adata.X.shape[0])
     adata.obs["n_counts"] = adata.X.sum(axis=1)
-    adata.layers["counts"] = adata.X.copy()
+    if dtype == np.float64:
+        dtype_cast = dtype
+    if dtype == np.int64:
+        dtype_cast = np.float64
+    if dtype == np.int32:
+        dtype_cast = np.float32
+    adata.layers["counts"] = adata.X.copy().astype(dtype_cast)
 
     single = sc.pp.regress_out(
         adata, keys=["n_counts", "percent_mito"], n_jobs=1, copy=True
