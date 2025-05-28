1e5b1bc9325f07eacc559e31bb3dbb17d7db711c
Ilan Gold <ilanbassgold@gmail.com>
2025-05-28 12:36:17 +0200

    (fix): `sc.read` with extension does not warn unnecessarily (#3643)
    * (fix): `sc.read` with extension does not warn unnecessarily
    
    * (fix): obey `return_ext`
    
    * (fix): add extension check internally
    
    * (chore): relnote
    
    (cherry picked from commit 350c3424d2f96c4a3a7bb3b7d0428d38d842ebe8)

--- tests/test_readwrite.py
+++ tests/test_readwrite.py
@@ -2,8 +2,11 @@ from __future__ import annotations
 
 from pathlib import PurePosixPath, PureWindowsPath
 
+import numpy as np
 import pytest
+from anndata import AnnData
 
+import scanpy as sc
 from scanpy.readwrite import _slugify
 
 
@@ -20,3 +23,12 @@ from scanpy.readwrite import _slugify
 )
 def test_slugify(path):
     assert _slugify(path) == "C-foo-bar"
+
+
+def test_read_ext_match(tmp_path):
+    adata_path = tmp_path / "foo.bar.anndata.h5ad"
+    AnnData(np.array([[1, 2], [3, 4]])).write_h5ad(adata_path)
+    with pytest.raises(ValueError, match="does not end in expected extension"):
+        sc.read(adata_path, ext="zarr")
+    # should not warn: https://github.com/scverse/scanpy/issues/2288
+    sc.read(adata_path, ext="h5ad")
