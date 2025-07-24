85c156511ed124c0224da3309815527780921e1f
ilan-gold <ilanbassgold@gmail.com>
2025-07-24 16:30:03 +0200

    fix: dont recalculate when there are fewer categories

--- src/scanpy/plotting/_tools/scatterplots.py
+++ src/scanpy/plotting/_tools/scatterplots.py
@@ -1225,14 +1225,20 @@ def _get_palette(adata, values_key: str, palette=None):
         values = pd.Categorical(adata.obs[values_key])
     if palette:
         _utils._set_colors_for_categorical_obs(adata, values_key, palette)
-    elif color_key not in adata.uns or len(adata.uns[color_key]) != len(
+    elif color_key not in adata.uns or len(adata.uns[color_key]) < len(
         values.categories
     ):
-        #  set a default palette in case that no colors or too few/too many colors are found
+        #  set a default palette in case that no colors or too few colors are found
         _utils._set_default_colors_for_categorical_obs(adata, values_key)
     else:
         _utils._validate_palette(adata, values_key)
-    return dict(zip(values.categories, adata.uns[color_key], strict=True))
+    return dict(
+        zip(
+            values.categories,
+            adata.uns[color_key][: len(values.categories)],
+            strict=True,
+        )
+    )
 
 
 def _color_vector(
