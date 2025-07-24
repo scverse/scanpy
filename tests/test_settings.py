6f98f3bcf3532dd90403b07646fcbd86e3689902
Lumberbot (aka Jack) <39504233+meeseeksmachine@users.noreply.github.com>
2025-07-24 04:04:35 -0700

    Backport PR #3731: (fix): settings.set_figure_params (#3734)
    Co-authored-by: Philipp A <flying-sheep@web.de>

---
+++ tests/test_settings.py
@@ -0,0 +1,14 @@
+from __future__ import annotations
+
+import pytest
+
+import scanpy as sc
+
+
+# TODO: reset everything
+@pytest.mark.parametrize("_attempt", range(3))
+def test_resets(_attempt: int) -> None:
+    """Test that changes made reset."""
+    assert sc.settings.autoshow
+    sc.settings.autoshow = False
+    assert not sc.settings.autoshow
