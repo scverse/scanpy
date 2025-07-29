57780ecc42b46c77e792c3d029d8c85635ba67d2
Lumberbot (aka Jack) <39504233+meeseeksmachine@users.noreply.github.com>
2025-07-29 02:43:31 -0700

    Backport PR #3725: fix: recalculate colors when color keys length does not match number of categories (#3744)
    Co-authored-by: Ilan Gold <ilanbassgold@gmail.com>

--- src/scanpy/plotting/_tools/scatterplots.py
+++ src/scanpy/plotting/_tools/scatterplots.py
@@ -1217,11 +1217,17 @@ def _get_palette(adata, values_key: str, palette=None):
     elif color_key not in adata.uns or len(adata.uns[color_key]) < len(
         values.categories
     ):
-        #  set a default palette in case that no colors or few colors are found
+        #  set a default palette in case that no colors or too few colors are found
         _utils._set_default_colors_for_categorical_obs(adata, values_key)
     else:
         _utils._validate_palette(adata, values_key)
-    return dict(zip(values.categories, adata.uns[color_key], strict=True))
+    return dict(
+        zip(
+            values.categories,
+            adata.uns[color_key][: len(values.categories)],
+            strict=True,
+        )
+    )
 
 
 def _color_vector(
