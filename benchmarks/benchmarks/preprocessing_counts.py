d31397bee217fa9d771cc9f9e2a8e2c633728434
ilan-gold <ilanbassgold@gmail.com>
2025-10-29 17:27:12 +0100

    fix: try `setup_cache`

--- benchmarks/benchmarks/preprocessing_counts.py
+++ benchmarks/benchmarks/preprocessing_counts.py
@@ -7,69 +7,63 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING
 
+import anndata as ad
+
 import scanpy as sc
 
 from ._utils import get_count_dataset
 
 if TYPE_CHECKING:
-    from anndata import AnnData
-
     from ._utils import Dataset, KeyCount
-
-# setup variables
-
-adata: AnnData
-batch_key: str | None
-
-
-def setup(dataset: Dataset, layer: KeyCount, *_):
-    """Set up global variables before each benchmark."""
-    global adata, batch_key
-    adata, batch_key = get_count_dataset(dataset, layer=layer)
-    assert "log1p" not in adata.uns
 
 
 # ASV suite
+class PreprocessingCountsSuite:  # noqa: D101
+    params: tuple[list[Dataset], list[KeyCount]] = (
+        ["pbmc68k_reduced", "pbmc3k"],
+        ["counts", "counts-off-axis"],
+    )
+    param_names = ("dataset", "layer")
 
-params: tuple[list[Dataset], list[KeyCount]] = (
-    ["pbmc68k_reduced", "pbmc3k"],
-    ["counts", "counts-off-axis"],
-)
-param_names = ["dataset", "layer"]
+    def setup_cache(self):
+        """Without this caching, asv was running several processes which meant the data was repeatedly downloaded."""
+        for dataset in self.params[0]:
+            for layer in self.params[1]:
+                adata, batch_key = get_count_dataset(dataset, layer=layer)
+                assert "lop1p" not in adata.uns
+                adata.uns["batch_key"] = batch_key
+                adata.write_h5ad(f"{dataset}_{layer}.h5ad")
 
+    def setup(self, dataset, layer):
+        self.adata = ad.read_h5ad(f"{dataset}_{layer}.h5ad")
 
-def time_filter_cells(*_):
-    sc.pp.filter_cells(adata, min_genes=100)
+    def time_filter_cells(self, *_):
+        sc.pp.filter_cells(self.adata, min_genes=100)
 
+    def peakmem_filter_cells(self, *_):
+        sc.pp.filter_cells(self.adata, min_genes=100)
 
-def peakmem_filter_cells(*_):
-    sc.pp.filter_cells(adata, min_genes=100)
+    def time_filter_genes(self, *_):
+        sc.pp.filter_genes(self.adata, min_cells=3)
 
+    def peakmem_filter_genes(self, *_):
+        sc.pp.filter_genes(self.adata, min_cells=3)
 
-def time_filter_genes(*_):
-    sc.pp.filter_genes(adata, min_cells=3)
+    def time_scrublet(self, *_):
+        sc.pp.scrublet(self.adata, batch_key=self.adata.uns["batch_key"])
 
-
-def peakmem_filter_genes(*_):
-    sc.pp.filter_genes(adata, min_cells=3)
-
-
-def time_scrublet(*_):
-    sc.pp.scrublet(adata, batch_key=batch_key)
-
-
-def peakmem_scrublet(*_):
-    sc.pp.scrublet(adata, batch_key=batch_key)
+    def peakmem_scrublet(self, *_):
+        sc.pp.scrublet(self.adata, batch_key=self.adata.uns["batch_key"])
 
 
 # Can’t do seurat v3 yet: https://github.com/conda-forge/scikit-misc-feedstock/issues/17
 """
-def time_hvg_seurat_v3(*_):
+def time_hvg_seurat_v3(self):
     # seurat v3 runs on counts
     sc.pp.highly_variable_genes(adata, flavor="seurat_v3_paper")
 
 
-def peakmem_hvg_seurat_v3(*_):
+def peakmem_hvg_seurat_v3(self):
     sc.pp.highly_variable_genes(adata, flavor="seurat_v3_paper")
 """
 
@@ -83,28 +77,39 @@ class FastSuite:
     )
     param_names = ("dataset", "layer")
 
+    def setup_cache(self):
+        """Without this caching, asv was running several processes which meant the data was repeatedly downloaded."""
+        for dataset in self.params[0]:
+            for layer in self.params[1]:
+                adata, _ = get_count_dataset(dataset, layer=layer)
+                assert "lop1p" not in adata.uns
+                adata.write_h5ad(f"{dataset}_{layer}.h5ad")
+
+    def setup(self, dataset, layer):
+        self.adata = ad.read_h5ad(f"{dataset}_{layer}.h5ad")
+
     def time_calculate_qc_metrics(self, *_):
         sc.pp.calculate_qc_metrics(
-            adata, qc_vars=["mt"], percent_top=None, log1p=False, inplace=True
+            self.adata, qc_vars=["mt"], percent_top=None, log1p=False, inplace=True
         )
 
     def peakmem_calculate_qc_metrics(self, *_):
         sc.pp.calculate_qc_metrics(
-            adata, qc_vars=["mt"], percent_top=None, log1p=False, inplace=True
+            self.adata, qc_vars=["mt"], percent_top=None, log1p=False, inplace=True
         )
 
     def time_normalize_total(self, *_):
-        sc.pp.normalize_total(adata, target_sum=1e4)
+        sc.pp.normalize_total(self.adata, target_sum=1e4)
 
     def peakmem_normalize_total(self, *_):
-        sc.pp.normalize_total(adata, target_sum=1e4)
+        sc.pp.normalize_total(self.adata, target_sum=1e4)
 
     def time_log1p(self, *_):
-        # TODO: This would fail: assert "log1p" not in adata.uns, "ASV bug?"
+        # TODO: This would fail: assert "log1p" not in self.adata.uns, "ASV bug?"
         # https://github.com/scverse/scanpy/issues/3052
-        adata.uns.pop("log1p", None)
-        sc.pp.log1p(adata)
+        self.adata.uns.pop("log1p", None)
+        sc.pp.log1p(self.adata)
 
     def peakmem_log1p(self, *_):
-        adata.uns.pop("log1p", None)
-        sc.pp.log1p(adata)
+        self.adata.uns.pop("log1p", None)
+        sc.pp.log1p(self.adata)
