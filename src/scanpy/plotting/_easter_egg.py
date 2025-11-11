3fafabc00988905a1bfc81b17259f3c0b7bd929f
Vladimir Shitov <vladimir.shitov@helmholtz-muenchen.de>
2025-11-11 22:19:45 +0100

    Add dogplot

---
+++ src/scanpy/plotting/_easter_egg.py
@@ -0,0 +1,18 @@
+from __future__ import annotations
+
+from pathlib import Path
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+HERE = Path(__file__).parent
+
+def dogplot(*_, **__) -> None:
+    """Shows who's a good boy"""
+    pic = np.random.randint(1, 4)
+    pic_path = HERE / "dogplot_images" /f"doggo_{pic}.jpg"
+    
+    img = plt.imread(pic_path)
+    plt.imshow(img)
+    plt.axis("off")
+    
