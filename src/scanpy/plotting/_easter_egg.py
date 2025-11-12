c2e0720f42070f7e60a1df7a1c137ec2f1e16019
Vladimir Shitov <vladimir.shitov@helmholtz-muenchen.de>
2025-11-12 13:31:57 +0100

    Use importlib to access images

--- src/scanpy/plotting/_easter_egg.py
+++ src/scanpy/plotting/_easter_egg.py
@@ -1,18 +1,18 @@
-from __future__ import annotations
-
-from pathlib import Path
+from importlib.resources import files
 
 import matplotlib.pyplot as plt
 import numpy as np
-
-HERE = Path(__file__).parent
+from PIL import Image
 
 
 def dogplot(*_, **__) -> None:
     """Shows who's a good boy"""
     pic = np.random.randint(1, 4)
-    pic_path = HERE / "dogplot_images" / f"doggo_{pic}.jpg"
+    img_path = files("scanpy.plotting").joinpath(f"dogplot_images/doggo_{pic}.webp")
+    
+    plt.figure(figsize=(3, 3))
+    
+    with Image.open(img_path) as img:
+        plt.imshow(img)
 
-    img = plt.imread(pic_path)
-    plt.imshow(img)
     plt.axis("off")
