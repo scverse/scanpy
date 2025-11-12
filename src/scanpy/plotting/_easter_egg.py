76f0cc047d293e98bee04ad179638ce4d69212d3
Phil Schaf <flying-sheep@web.de>
2025-11-12 15:53:10 +0100

    best practice lol

--- src/scanpy/plotting/_easter_egg.py
+++ src/scanpy/plotting/_easter_egg.py
@@ -2,19 +2,19 @@ from __future__ import annotations
 
 from importlib.resources import files
 
-import matplotlib.pyplot as plt
 import numpy as np
+from matplotlib import pyplot as plt
+from matplotlib.image import imread
 
 
-def dogplot(*_, **__) -> None:
-    """Show who's a good boy."""
-    pic = np.random.randint(1, 4)
-    img_path = files("scanpy.plotting") / f"dogplot_images/doggo_{pic}.webp"
-
-    plt.figure(figsize=(3, 3))
-
+def dogplot(n: int | None = None, *, show: bool = False) -> None:
+    """Show who’s a good boy."""
+    rng = np.random.default_rng()
+    n = int(rng.integers(1, 4))
+    img_path = files("scanpy.plotting") / f"dogplot_images/doggo_{n}.webp"
     with img_path.open("rb") as f:
-        img = plt.imread(f)
-    plt.imshow(img)
+        img = imread(f)
 
-    plt.axis("off")
+    _, ax = plt.subplots(figsize=(3, 3))
+    ax.imshow(img)
+    ax.set_axis_off()
