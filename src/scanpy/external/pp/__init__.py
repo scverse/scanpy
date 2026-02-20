b5527796ffe054a6078f7bd8267b56b5dae2da64
Philipp A. <flying-sheep@web.de>
2026-02-20 10:38:24 +0100

    improve deprecation

--- src/scanpy/external/pp/__init__.py
+++ src/scanpy/external/pp/__init__.py
@@ -3,8 +3,6 @@
 from __future__ import annotations
 
 from ..._compat import deprecated
-from ...preprocessing import _scrublet
-from ...preprocessing._harmony_integrate import harmony_integrate
 from ._bbknn import bbknn
 from ._dca import dca
 from ._hashsolo import hashsolo
@@ -12,17 +10,32 @@ from ._magic import magic
 from ._mnn_correct import mnn_correct
 from ._scanorama_integrate import scanorama_integrate
 
-scrublet = deprecated("Import from sc.pp instead")(_scrublet.scrublet)
-scrublet_simulate_doublets = deprecated("Import from sc.pp instead")(
-    _scrublet.scrublet_simulate_doublets
-)
-
 __all__ = [
     "bbknn",
     "dca",
-    "harmony_integrate",
     "hashsolo",
     "magic",
     "mnn_correct",
     "scanorama_integrate",
 ]
+
+
+@deprecated("Import from sc.pp instead")
+def harmony_integrate(*args, **kwargs):
+    from ...preprocessing import harmony_integrate
+
+    return harmony_integrate(*args, **kwargs)
+
+
+@deprecated("Import from sc.pp instead")
+def scrublet(*args, **kwargs):
+    from ...preprocessing import scrublet
+
+    return scrublet(*args, **kwargs)
+
+
+@deprecated("Import from sc.pp instead")
+def scrublet_simulate_doublets(*args, **kwargs):
+    from ...preprocessing import scrublet_simulate_doublets
+
+    return scrublet_simulate_doublets(*args, **kwargs)
