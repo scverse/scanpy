7e2fab542f4d34568624f640b1051cc4916b06d2
Philipp A. <flying-sheep@web.de>
2026-02-27 12:23:32 +0100

    paga

--- src/scanpy/preprocessing/_utils.py
+++ src/scanpy/preprocessing/_utils.py
@@ -3,28 +3,21 @@ from __future__ import annotations
 from typing import TYPE_CHECKING
 
 import numpy as np
-from sklearn.random_projection import sample_without_replacement
 
-from .._utils.random import _legacy_random_state
+from .._utils.random import _FakeRandomGen
 
 if TYPE_CHECKING:
-    from typing import Literal
-
     from numpy.typing import NDArray
 
 
 def sample_comb(
-    dims: tuple[int, ...],
-    nsamp: int,
-    *,
-    rng: np.random.Generator,
-    method: Literal[
-        "auto", "tracking_selection", "reservoir_sampling", "pool"
-    ] = "auto",
+    dims: tuple[int, ...], nsamp: int, *, rng: np.random.Generator
 ) -> NDArray[np.int64]:
     """Randomly sample indices from a grid, without repeating the same tuple."""
-    random_state = _legacy_random_state(rng)
-    idx = sample_without_replacement(
-        np.prod(dims), nsamp, random_state=random_state, method=method
-    )
+    if isinstance(rng, _FakeRandomGen):
+        from sklearn.random_projection import sample_without_replacement
+
+        idx = sample_without_replacement(np.prod(dims), nsamp, random_state=rng._arg)
+    else:
+        idx = rng.choice(np.prod(dims), size=nsamp, replace=False)
     return np.vstack(np.unravel_index(idx, dims)).T
