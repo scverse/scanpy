Backport PR #4148: fix: moran's calculation parallelization

tests/test_metrics.py
@@ -69,7 +69,7 @@ def test_consistency(metric) -> None:
         pbmc, vals=pbmc[:, pbmc.var_names[0]].layers["raw"].toarray().ravel()
     )
 
-    np.testing.assert_allclose(all_genes[0], first_gene, rtol=1e-9)
+    np.testing.assert_allclose(all_genes[0], first_gene, rtol=1e-6)
 
     # Test that results are similar for sparse and dense reps of same data
     equality_check(