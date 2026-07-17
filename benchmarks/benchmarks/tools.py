1a3cab76a30026dd9fb47daaf80356c8150d3107
Zach Boldyga <zboldyga@users.noreply.github.com>
2026-07-17 04:29:36 -0700

    Backport PR #4204: perf: optimization of rank_genes_groups

--- benchmarks/benchmarks/tools.py
+++ benchmarks/benchmarks/tools.py
@@ -39,12 +39,6 @@ class ToolsSuite:  # noqa: D101
     def peakmem_leiden(self) -> None:
         sc.tl.leiden(self.adata, flavor="igraph")
 
-    def time_rank_genes_groups(self) -> None:
-        sc.tl.rank_genes_groups(self.adata, "bulk_labels", method="wilcoxon")
-
-    def peakmem_rank_genes_groups(self) -> None:
-        sc.tl.rank_genes_groups(self.adata, "bulk_labels", method="wilcoxon")
-
     def time_combat(self) -> None:
         sc.pp.combat(self.adata, key="bulk_labels")
 
