9d5e670547e2746a0a40926fa5a3ffbe85b394a5
Philipp A. <flying-sheep@web.de>
2026-07-28 13:10:31 +0200

    Backport PR #4263 on branch 1.12.x (test: fix sim test) (#4268)

--- tests/test_sim.py
+++ tests/test_sim.py
@@ -6,9 +6,9 @@ import pytest
 import scanpy as sc
 
 
+@pytest.mark.filterwarnings("ignore:.*Observation names are not unique:UserWarning")
 def test_sim_toggleswitch() -> None:
-    with pytest.warns(UserWarning, match=r"Observation names are not unique"):
-        adata_sim = sc.tl.sim("toggleswitch")
-    with pytest.warns(UserWarning, match=r"Observation names are not unique"):
-        adata_ds = sc.datasets.toggleswitch()
-    np.allclose(adata_sim.X, adata_ds.X, np.finfo(np.float32).eps)
+    # the reference data was generated using the legacy global RNG seeded with 0
+    adata_sim = sc.tl.sim("toggleswitch", seed=0)
+    adata_ds = sc.datasets.toggleswitch()
+    np.testing.assert_allclose(adata_sim.X, adata_ds.X, atol=np.finfo(np.float32).eps)
