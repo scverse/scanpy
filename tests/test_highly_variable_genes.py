30a4ec0cd7b28280cdcaf6144ddc9740d12cc03b
amalia-k510 <kareshamal@gmail.com>
2026-09-21 14:29:01 +0200

    xfail jax tests

--- tests/test_highly_variable_genes.py
+++ tests/test_highly_variable_genes.py
@@ -19,7 +19,7 @@ from scanpy._compat import CSRBase
 from testing.scanpy._helpers import _check_check_values_warnings
 from testing.scanpy._helpers.data import pbmc3k, pbmc68k_reduced
 from testing.scanpy._pytest.marks import needs
-from testing.scanpy._pytest.params import ARRAY_TYPES
+from testing.scanpy._pytest.params import ARRAY_TYPES, as_dense_jax_array
 
 if TYPE_CHECKING:
     from collections.abc import Callable
@@ -649,6 +649,7 @@ def test_seurat_v3_bad_chunking(adata, array_type, flavor):
 )
 @pytest.mark.parametrize("batch_key", [None, "batch"])
 def test_subset_inplace_consistency(
+    request,
     subtests: pytest.Subtests,
     flavor: Literal["seurat", "cell_ranger", "seurat_v3", "seurat_v3_paper"],
     array_type,
@@ -661,6 +662,13 @@ def test_subset_inplace_consistency(
     - for dask arrays and non-dask arrays
     - for both with and without batch_key
     """
+    if array_type is as_dense_jax_array and flavor in {"seurat_v3", "seurat_v3_paper"}:
+        request.applymarker(
+            pytest.mark.xfail(
+                reason="seurat_v3 not jax compatible (numba aggregation + DLPack readonly limitation)",
+                strict=False,
+            )
+        )
     rng = np.random.default_rng(0)
     adata = (
         sc.datasets.blobs(n_observations=20, n_variables=80, rng=rng)
